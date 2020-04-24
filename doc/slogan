b0365f9b228b90c3777c4702b7f03603a3851c5f
Jon Pretty <jon.pretty@propensive.com>
2020-04-24 11:03:38 +0200

    Documentation

---
+++ doc/slogan
@@ -0,0 +1,1 @@
+To probe what we can't prove, so what isn't provable may be probable
--- src/core/probably.scala
+++ src/core/probably.scala
@@ -130,9 +130,9 @@ class Runner() extends Dynamic {
 
 case class Seed(value: Long) {
   def apply(): Long = value
-  def stream: Stream[Seed] = {
+  def stream(count: Int): Stream[Seed] = {
     val rnd = new java.util.Random(value)
-    Stream.continually(Seed(rnd.nextLong))
+    Stream.continually(Seed(rnd.nextLong)).take(count)
   }
 }
 
@@ -140,7 +140,7 @@ object Arbitrary {
   type Typeclass[T] = Arbitrary[T]
 
   def combine[T](ctx: CaseClass[Arbitrary, T]): Arbitrary[T] = (seed, n) => ctx.rawConstruct {
-    ctx.parameters.zip(spread(seed, n, ctx.parameters.size)).zip(seed.stream.take(ctx.parameters.size)).map {
+    ctx.parameters.zip(spread(seed, n, ctx.parameters.size)).zip(seed.stream(ctx.parameters.size)).map {
       case ((param, i), s) => param.typeclass(s, i)
     } }
 
@@ -148,19 +148,19 @@ object Arbitrary {
   val interestingInts = Vector(0, 1, -1, 2, -2, 42, Int.MaxValue, Int.MinValue, Int.MaxValue - 1,
       Int.MinValue + 1)
 
-  implicit val int: Arbitrary[Int] = (seed, n) => interestingInts.lift(n).getOrElse(seed.stream(n).value.toInt)
-
+  implicit val int: Arbitrary[Int] =
+    (seed, n) => interestingInts.lift(n).getOrElse(seed.stream(n).last.value.toInt)
 
   val interestingStrings = Vector("", "a", "z", "\n", "0", "_", "\"", "\'", " ", "abcdefghijklmnopqrstuvwxyz")
   implicit def string: Arbitrary[String] = (seed, n) => interestingStrings.lift(n).getOrElse {
-    val chars = seed.stream(n).stream.map(_()).map(_.toByte).filter(_ > 31).filter(_ < 128).take(8)
+    val chars = seed.stream(n).last.stream(10).map(_()).map(_.toByte).filter { c => c > 31 && c < 128 }
     new String(chars.to[Array], "UTF-8")
   }
 
   implicit def gen[T]: Arbitrary[T] = macro Magnolia.gen[T]
 
   private def spread(seed: Seed, total: Int, count: Int): List[Int] = {
-    val sample = seed.stream.map(_.value.toDouble).map(math.abs(_)).take(count).to[List]
+    val sample = seed.stream(count).map(_.value.toDouble).map(math.abs(_)).to[List]
     sample.tails.foldLeft(List[Int]()) { case (acc, tail) => tail.headOption.fold(acc) { v =>
       (((v/(tail.sum))*(total - acc.sum)) + 0.5).toInt :: acc
     } }
