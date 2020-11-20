938b3c1af54018cd150c44981e70e5f41a2163bb
Jon Pretty <jon.pretty@propensive.com>
2020-11-20 14:32:08 +0100

    Version 0.19.0

--- src/core/adversaria.scala
+++ src/core/adversaria.scala
@@ -1,6 +1,6 @@
 /*
 
-    Adversaria, version 0.3.0. Copyright 2017-20 Jon Pretty, Propensive OÜ.
+    Adversaria, version 0.19.0. Copyright 2017-20 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
