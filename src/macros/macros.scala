90813af94974af236010becce67a3e5cf038bbb0
Jon Pretty <jon.pretty@propensive.com>
2020-11-23 13:21:04 +0100

    Refresh everything

--- src/macros/macros.scala
+++ src/macros/macros.scala
@@ -1,20 +1,17 @@
 /*
-  
-  Guillotine, version 0.2.0. Copyright 2018 Jon Pretty, Propensive Ltd.
 
-  The primary distribution site is: https://propensive.com/
+    Guillotine, version 0.5.0. Copyright 2017-20 Jon Pretty, Propensive OÜ.
 
-  Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-  this file except in compliance with the License. You may obtain a copy of the
-  License at
-  
-      http://www.apache.org/licenses/LICENSE-2.0
- 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-  License for the specific language governing permissions and limitations under
-  the License.
+    The primary distribution site is: https://propensive.com/
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+    compliance with the License. You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the License is
+    distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and limitations under the License.
 
 */
 package guillotine
@@ -22,6 +19,7 @@ package guillotine
 import contextual._
 import concurrent._, ExecutionContext.Implicits.global
 import language.higherKinds
+import language.experimental.macros
 import java.io.File
 import scala.io.Source
 
@@ -299,7 +297,7 @@ object `package` {
   )
 
   implicit class ShellStringContext(sc: StringContext) {
-    val sh = Prefix(ShellInterpolator, sc)
+    def sh(expressions: String*): Command = macro Macros.contextual[ShellInterpolator.type]
   }
 
 }
