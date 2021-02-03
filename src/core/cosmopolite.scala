d9f25bbe1ac2baa60963a6d6b89e7f25c4858499
Lander Lopez <lander.lopez@gmail.com>
2021-02-03 13:27:45 +0000

    ReifyToList macro

--- src/core/cosmopolite.scala
+++ src/core/cosmopolite.scala
@@ -11,18 +11,20 @@ object Language:
    @targetName("make")
    def apply[L <: String: ValueOf]: Language[L] = new Language(summon[ValueOf[L]].value)
    
-   inline def parse[L <: String](str: String): Option[Language[L]] = ${doParse[L]('str)}
+   inline def parse[L <: String](str: String): Option[Language[L]] =
+      Option.when(reifyToList[L].contains(str))(Language(str))
 
-   private def doParse[L <: String: Type](str: Expr[String])(using quotes: Quotes): Expr[Option[Language[L]]] =
+   inline def reifyToList[L <: String]: List[String] = ${reifyToListMacro[L]}
+
+   private def reifyToListMacro[L <: String: Type](using quotes: Quotes): Expr[List[String]] =
       import quotes.reflect._
 
       def langs(t: TypeRepr): List[String] = t.dealias match
          case OrType(left, right) => langs(left) ++ langs(right)
+         case AndType(left, right) => langs(left) ++ langs(right)
          case ConstantType(StringConstant(lang)) => List(lang)
 
-      langs(TypeRepr.of[L]).foldLeft('{ None: Option[Language[L]] }) { (agg, lang) =>
-         '{ if $str == ${Expr(lang)} then Some(Language[L](${Expr(lang)})) else $agg }
-      }
+      Expr(langs(TypeRepr.of[L]))
 
 object Messages:
    def apply[L <: String: ValueOf](seq: Seq[String], parts: Seq[Messages[? >: L]]): Messages[L] =
