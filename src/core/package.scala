ada0d768ff43e03c822f913b4998c0eabb4a43c0
Jon Pretty <jon.pretty@propensive.com>
2021-04-14 18:12:30 +0200

    Updates for Scala 3

--- src/core/package.scala
+++ src/core/package.scala
@@ -17,65 +17,55 @@
 
 package rudiments
 
-import scala.collection._, mutable.{Builder, ArrayBuffer}
+extension [T](value: T)
+  def only[S](pf: PartialFunction[T, S]): Option[S] = Some(value).collect(pf)
+  def unit: Unit = ()
 
-import language.implicitConversions
+extension (value: String)
+  def populated: Option[String] = if(value.isEmpty) None else Some(value)
+  def cut(delimiter: String): IArray[String] = IArray.from(value.split(delimiter))
+  def bytes: IArray[Byte] = IArray.from(value.getBytes("UTF-8"))
+  def chars: IArray[Char] = IArray.from(value.toCharArray)
 
-object `package` {
-  @inline implicit def traversableStringExtensions(strings: Traversable[String]): TraversableStringExtensions =
-    TraversableStringExtensions(strings)
-  
-  @inline implicit def globalExtensions[T](value: T): GlobalExtensions[T] = GlobalExtensions(value)
-  @inline implicit def stringExtensions(string: String): StringExtensions = StringExtensions(string)
-}
-
-
-case class GlobalExtensions[T](value: T) extends AnyVal {
-  @inline final def only[S](pf: PartialFunction[T, S]): Option[S] = Some(value).collect(pf)
-  @inline final def unit: Unit = ()
-  @inline final def swallow[S]: S => T = { _ => value }
-  @inline final def pass(fn: T => Any): T = { fn(value); value }
-}
-
-case class StringExtensions(value: String) extends AnyVal {
-  @inline final def populated: Option[String] = if(value.isEmpty) None else Some(value)
-}
-
-object Bytes {
-  def apply(array: Array[Byte]): Bytes = new Bytes(array.clone)
-}
-
-final case class Bytes private[rudiments] (private val array: Array[Byte])
-    extends IndexedSeqOptimized[Byte, Bytes] {
-  override def equals(that: Any): Boolean = that match {
-    case that: Bytes => java.util.Arrays.equals(array, that.array)
-    case _           => false
-  }
-
-  override protected[this] def thisCollection: IndexedSeq[Byte] = array
-  override protected[this] def toCollection(repr: Bytes): IndexedSeq[Byte] = repr.array
-
-  override def hashCode: Int = java.util.Arrays.hashCode(array)
-  def seq: IndexedSeq[Byte] = array
-  def length: Int = array.length
-  override def toString: String = s"[${size}B]"
-  def apply(index: Int): Byte = array(index)
-
-  def newBuilder: Builder[Byte, Bytes] = new Builder[Byte, Bytes] {
-    private val underlying = ArrayBuffer.empty[Byte]
-    
-    def +=(elem: Byte): this.type = {
-      underlying += elem
-      this
-    }
-
-    def clear(): Unit = underlying.clear()
-    def result(): Bytes = Bytes(underlying.toArray)
-  }
-}
-
-case class TraversableStringExtensions(values: Traversable[String]) extends AnyVal {
+extension (values: Traversable[String])
   def join: String = values.mkString
   def join(separator: String): String = values.mkString(separator)
   def join(left: String, separator: String, right: String): String = values.mkString(left, separator, right)
-}
+  
+  def join(separator: String, last: String): String = values.size match
+    case 0 => ""
+    case 1 => values.head
+    case _ => values.init.mkString(separator)+last+values.last
+
+class Recur[T](fn: => T => T):
+  def apply(value: T): T = fn(value)
+
+def fix[T](func: Recur[T] ?=> (T => T)): (T => T) = func(using Recur(fix(func)))
+def recur[T: Recur](value: T): T = summon[Recur[T]](value)
+
+extension (obj: Boolean.type)
+  def unapply(str: String): Option[Boolean] = try Some(str.toBoolean) catch case e: Exception => None
+
+extension (obj: Byte.type)
+  def unapply(str: String): Option[Byte] = try Some(str.toByte) catch case e: Exception => None
+
+extension (obj: Short.type)
+  def unapply(str: String): Option[Short] = try Some(str.toShort) catch case e: Exception => None
+
+extension (obj: Int.type)
+  def unapply(str: String): Option[Int] = try Some(str.toInt) catch case e: Exception => None
+
+extension (obj: Long.type)
+  def unapply(str: String): Option[Long] = try Some(str.toLong) catch case e: Exception => None
+
+extension (obj: Float.type)
+  def unapply(str: String): Option[Float] = try Some(str.toFloat) catch case e: Exception => None
+
+extension (obj: Char.type)
+  def unapply(str: String): Option[Char] = if str.length == 1 then Some(str.head) else None
+
+extension (obj: Double.type)
+  def unapply(str: String): Option[Double] = try Some(str.toDouble) catch case e: Exception => None
+
+extension (ctx: StringContext)
+  def str(strings: String*): String = ctx.parts.head + strings.zip(ctx.parts.tail).map(_+_).mkString
