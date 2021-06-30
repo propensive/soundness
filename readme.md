68086ea2fb6610c27258114dbfa03ad3aeb7182c
Jon Pretty <jon.pretty@propensive.com>
2021-06-30 16:01:00 +0200

    Updated docs

--- readme.md
+++ readme.md
@@ -1,69 +1,121 @@
-[<img alt="GitHub Workflow" src="https://img.shields.io/github/workflow/status/propensive//Build/main?style=for-the-badge" height="24">](https://github.com/propensive//actions)
+[<img alt="GitHub Workflow" src="https://img.shields.io/github/workflow/status/propensive/slalom/Build/main?style=for-the-badge" height="24">](https://github.com/propensive/slalom/actions)
 [<img src="https://img.shields.io/discord/633198088311537684?color=8899f7&label=DISCORD&style=for-the-badge" height="24">](https://discord.gg/CHCPjERybv)
-[<img src="https://img.shields.io/maven-central/v/com.propensive/_2.12?color=2465cd&style=for-the-badge" height="24">](https://search.maven.org/artifact/com.propensive/_2.12)
-[<img src="https://vent.dev/badge/propensive/" height="24">](https://vent.dev/)
+[<img src="https://img.shields.io/maven-central/v/com.propensive/slalom-core_2.12?color=2465cd&style=for-the-badge" height="24">](https://search.maven.org/artifact/com.propensive/slalom-core_2.12)
+[<img src="https://vent.dev/badge/propensive/slalom" height="24">](https://vent.dev/)
 
 <img src="/doc/images/github.png" valign="middle">
 
-# 
+# Slalom
 
-
+__Slalom__ is a small library for handling the abstract notion of paths, distinguishing between relative and absolute forms, rooted on some value. This may be useful for representing paths on a filesystem, resources on an HTTP server, descendants in a family tree, or many other concepts which follow a hierarchical pattern.
 
 ## Features
 
-
+- representations of hierarchical paths
+- designed for extension and use in many concrete contexts
+- distinguishes between absolute and relative paths
 
 ## Getting Started
 
+The type `Root` defines a "root", beneath which any number of possible `Path` instances may exist in
+a hierarchy, and the methods `parent`, `ancestor` and `/` may be used to navigate between them. For
+abstract paths, the value `Base` can serve as a root node for a path hierarchy.
 
+Here are some examples:
+```scala
+val crustaceans = Base / "eukaryota" / "animalia" / "arthropods" / "crustaceans"
+val arthropods = crustaceans.parent
+val animalia = crustaceans.ancestor(2)
+```
+
+`Path`s may be further distinguished as `Path.Relative` or `Path.Absolute`, where a `Relative` may
+be converted into an `Absolute` by calling the `Path#absolute` method, and passing an absolute path
+to which the relative path should be considered relative to. The result is typed as `Path.Absolute`.
+
+`Path` objects, whether absolute or relative, serialize with `toString` according to the delimiters
+in their root, which are defined in terms of a base name (for example, `/` or `classpath:`) and a
+separator (for example, `\`, `/` or `.`).
+
+Any implementation of `Root` should define these values, `prefix` and `separator`, respectively. For
+example, the definition,
+```scala
+object Domain extends Root(prefix = "", separator = ".")
+```
+would ensure that,
+```scala
+Domain / "www" / "example" / "com"
+```
+would serialize to the string `"www.example.com"`.
+
+Note that the `separator` is not included between the `prefix` and the first path element when
+serializing, so _may_ need to be included in the `prefix` value itself.
+
+## Other Methods
+
+The method `++` can add a `Relative` path to an `Absolute` path, and return a new `Absolute` path.
+
+Similarly, `Absolute#relativeTo` takes another `Absolute` path and returns a `Relative` instance
+that, when applied with `++` to the first path, produces the second path.
+
+The `Absolute#conjunction` method will find the closest common parent of the path and its parameter.
+
+Note that `Path`s are not aware of their children, so there is no `children` method, but this may be
+provided by individual implementations.
+
+## Exceptions
+
+Many operations on `Path`s may attempt (directly or indirectly) to access the parent of the root.
+This is not possible, and if this happens, a `RootBoundaryExceeded` exception will be thrown.
+
+## Generic relative paths
+
+Given that a relative path is (by definition) not attached to any particular root, all instances of
+`Root#Path.Relative` inherit from `GenericRelative` which gives users the choice, when implementing
+APIs that accept relative paths, between accepting _any_ kind of relative path (regardless of its
+origin) and accepting just those originating from a particular root.
 
 ## Status
 
- is classified as ____. Propensive defines the following five stability levels for open-source projects:
+Slalom is classified as __fledgling__. Propensive defines the following five stability levels for open-source projects:
 
-- _embryonic_: for experimental or demonstrative purposes only, without guarantee of longevity
+- _embryonic_: for experimental or demonstrative purposes only, without any guarantees of longevity
 - _fledgling_: of proven utility, seeking contributions, but liable to significant redesigns
-- _maturescent_: major design decisions broady settled, seeking probatory adoption and refinement of designs
+- _maturescent_: major design decisions broady settled, seeking probatory adoption and refinement
 - _dependable_: production-ready, subject to controlled ongoing maintenance and enhancement; tagged as version `1.0` or later
 - _adamantine_: proven, reliable and production-ready, with no further breaking changes ever anticipated
 
 ## Availability
 
-&rsquo;s source is available on GitHub, and may be built with [Fury](https://github.com/propensive/fury) by
-cloning the layer `propensive/`.
+Slalom&rsquo;s source is available on GitHub, and may be built with [Fury](https://github.com/propensive/fury) by
+cloning the layer `propensive/slalom`.
 ```
-fury layer clone -i propensive/
+fury layer clone -i propensive/slalom
 ```
 or imported into an existing layer with,
 ```
-fury layer import -i propensive/
-```
-A binary is available on Maven Central as `com.propensive:_<scala-version>:`. This may be added
-to an [sbt](https://www.scala-sbt.org/) build with:
-```
-libraryDependencies += "com.propensive" %% "" % ""
+fury layer import -i propensive/slalom
 ```
 
 ## Contributing
 
-Contributors to  are welcome and encouraged. New contributors may like to look for issues marked
-<a href="https://github.com/propensive//labels/good%20first%20issue"><img alt="label: good first issue"
+Contributors to Slalom are welcome and encouraged. New contributors may like to look for issues marked
+<a href="https://github.com/propensive/slalom/labels/good%20first%20issue"><img alt="label: good first issue"
 src="https://img.shields.io/badge/-good%20first%20issue-67b6d0.svg" valign="middle"></a>.
 
 We suggest that all contributors read the [Contributing Guide](/contributing.md) to make the process of
-contributing to  easier.
+contributing to Slalom easier.
 
 Please __do not__ contact project maintainers privately with questions, as other users cannot then benefit from
 the answers.
 
 ## Author
 
- was designed and developed by [Jon Pretty](https://twitter.com/propensive), and commercial support and
+Slalom was designed and developed by [Jon Pretty](https://twitter.com/propensive), and commercial support and
 training is available from [Propensive O&Uuml;](https://propensive.com/).
 
 
 
 ## License
 
- is copyright &copy; -21 Jon Pretty & Propensive O&Uuml;, and is made available under the
+Slalom is copyright &copy; 2020-21 Jon Pretty & Propensive O&Uuml;, and is made available under the
 [Apache 2.0 License](/license.md).
--- src/core/path.scala
+++ src/core/path.scala
@@ -1,19 +1,19 @@
 /*
-
     Slalom, version 0.1.0. Copyright 2020-21 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
-    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
-    compliance with the License. You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+    file except in compliance with the License. You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software distributed under the License is
-    distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and limitations under the License.
-
+    Unless required by applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+    either express or implied. See the License for the specific language governing permissions
+    and limitations under the License.
 */
+
 package slalom
 
 import rudiments.*
@@ -23,65 +23,101 @@ class SlalomException(message: String) extends Exception(str"slalom: $message")
 case class RootBoundaryExceeded(root: Root)
 extends SlalomException(str"attempted to exceed the root boundary")
 
-trait GenericRelative:
+transparent trait GenericRelative:
   def ascent: Int
   def path: Vector[String]
 
 trait Root(val separator: String, val prefix: String):
-  def root: Root = this
-  sealed trait Path:
-    def parent: Path raises RootBoundaryExceeded
-    def ancestor(ascent: Int): Path raises RootBoundaryExceeded
-    def absolute(pwd: Path.Absolute): Path.Absolute raises RootBoundaryExceeded
-    def /(filename: String): Path raises RootBoundaryExceeded
+  def thisRoot: this.type = this
+
+  type AbsolutePath <: Path.Absolute
+  type RelativePath <: Path.Relative
+
+  protected def makeAbsolute(path: Vector[String]): AbsolutePath
+  protected def makeRelative(ascent: Int, path: Vector[String]): RelativePath
+
+  trait Path:
+    def root: thisRoot.type = thisRoot
+    def parent: Path exposes RootBoundaryExceeded
+    def ancestor(ascent: Int): Path exposes RootBoundaryExceeded
+    def absolute(pwd: AbsolutePath): AbsolutePath exposes RootBoundaryExceeded
+    def /(filename: String): Path exposes RootBoundaryExceeded
     def ++(path: GenericRelative): Path
 
   object Path:
-    case class Absolute(path: Vector[String]) extends Path:
+    class Absolute(val path: Vector[String]) extends Path:
       override def toString(): String = path.join(prefix, separator, "")
       
-      def parent: Absolute raises RootBoundaryExceeded = path match
-        case init :+ last => Absolute(init)
+      def parent: AbsolutePath exposes RootBoundaryExceeded = path match
+        case init :+ last => makeAbsolute(init)
         case _            => throw RootBoundaryExceeded(root)
 
-      def ancestor(ascent: Int): Absolute raises RootBoundaryExceeded =
-        if path.length == 0 then this else parent.ancestor(ascent - 1)
+      def ancestor(ascent: Int): AbsolutePath exposes RootBoundaryExceeded =
+        if path.length == 0 then makeAbsolute(path) else parent.ancestor(ascent - 1)
       
-      def absolute(pwd: Absolute): Absolute raises RootBoundaryExceeded = this
-      
-      def /(filename: String): Absolute raises RootBoundaryExceeded = filename match
-        case ".."     => path match
-          case init :+ last => Absolute(init)
-          case _            => throw RootBoundaryExceeded(root)
-        case "."      => this
-        case filename => Absolute(path :+ filename)
-      
-      def ++(relative: GenericRelative): Absolute raises RootBoundaryExceeded =
-        if relative.ascent > 0 then ancestor(relative.ascent) ++ Relative(0, relative.path)
-        else Absolute(path ++ relative.path)
+      def absolute(pwd: AbsolutePath): AbsolutePath exposes RootBoundaryExceeded =
+        makeAbsolute(path)
 
-    case class Relative(ascent: Int, path: Vector[String]) extends Path, GenericRelative:
+      def conjunction(other: AbsolutePath): AbsolutePath =
+        makeAbsolute(path.zip(other.path).takeWhile(_ == _).map(_.at[0]))
+
+      def relativeTo(base: AbsolutePath): RelativePath =
+        val common = conjunction(base)
+        makeRelative(path.length - common.path.length, base.path.drop(common.path.length))
+
+      def /(filename: String): AbsolutePath exposes RootBoundaryExceeded = filename match
+        case ".."     => path match
+          case init :+ last => makeAbsolute(init)
+          case _            => throw RootBoundaryExceeded(root)
+        case "."      => makeAbsolute(path)
+        case filename => makeAbsolute(path :+ filename)
+      
+      def ++(relative: GenericRelative): AbsolutePath exposes RootBoundaryExceeded =
+        if relative.ascent > 0 then ancestor(relative.ascent) ++ makeRelative(0, relative.path)
+        else makeAbsolute(path ++ relative.path)
+      
+      override def equals(that: Any): Boolean = that match
+        case that: Absolute => path == that.path
+        case _              => false
+
+      override def hashCode: Int = path.hashCode
+
+    case class Relative(val ascent: Int, val path: Vector[String]) extends Path, GenericRelative:
       override def toString(): String = path.join("../"*ascent, "/", "")
 
-      def parent: Relative raises RootBoundaryExceeded = path match
-        case init :+ last => Relative(ascent, init)
-        case empty        => Relative(ascent + 1, Vector())
+      def parent: Relative exposes RootBoundaryExceeded = path match
+        case init :+ last => makeRelative(ascent, init)
+        case empty        => makeRelative(ascent + 1, Vector())
 
-      def ancestor(ascent: Int): Relative raises RootBoundaryExceeded =
-        if ascent == 0 then this else parent.ancestor(ascent - 1)
+      def ancestor(ascent: Int): RelativePath exposes RootBoundaryExceeded =
+        if ascent == 0 then makeRelative(ascent, path) else parent.ancestor(ascent - 1)
 
-      def absolute(pwd: Absolute): Absolute raises RootBoundaryExceeded =
-        if ascent == 0 then pwd.copy(path = pwd.path ++ path) else Relative(ascent - 1, path).absolute(pwd.parent)
+      def absolute(pwd: AbsolutePath): AbsolutePath exposes RootBoundaryExceeded =
+        if ascent == 0 then makeAbsolute(pwd.path ++ path)
+        else makeRelative(ascent - 1, path).absolute(pwd.parent)
       
-      def /(filename: String): Relative raises RootBoundaryExceeded = filename match
+      def /(filename: String): RelativePath exposes RootBoundaryExceeded = filename match
         case ".."     => path match
-          case init :+ last => Relative(ascent, init)
-          case empty        => Relative(ascent + 1, Vector())
-        case "."      => this
-        case filename => Relative(ascent, path :+ filename)
+          case init :+ last => makeRelative(ascent, init)
+          case empty        => makeRelative(ascent + 1, Vector())
+        case "."      => makeRelative(ascent, path)
+        case filename => makeRelative(ascent, path :+ filename)
       
-      def ++(relative: GenericRelative): Relative raises RootBoundaryExceeded =
-        if relative.ascent == 0 then copy(path = path ++ relative.path)
-        else ancestor(relative.ascent) ++ Relative(ascent, relative.path)
+      def ++(relative: GenericRelative): RelativePath exposes RootBoundaryExceeded =
+        if relative.ascent == 0 then makeRelative(ascent, path ++ relative.path)
+        else ancestor(relative.ascent) ++ makeRelative(ascent, relative.path)
 
-object Classpath extends Root("/", "classpath:")
+      override def equals(that: Any): Boolean = that match
+        case that: Relative => ascent == that.ascent && path == that.path
+        case _              => false
+
+      override def hashCode: Int = path.hashCode ^ ascent
+
+object Base extends Root("/", "/"):
+  type RelativePath = Path.Relative
+  type AbsolutePath = Path.Absolute
+  
+  protected def makeAbsolute(path: Vector[String]): AbsolutePath = Path.Absolute(path)
+  
+  protected def makeRelative(ascent: Int, path: Vector[String]): RelativePath =
+    Path.Relative(ascent, path)
