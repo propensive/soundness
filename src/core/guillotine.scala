eca18ac2c7a87565017a42df9031cb576409d7a6
Jon Pretty <jon.pretty@propensive.com>
2021-06-30 18:38:44 +0200

    Code cleanup

--- src/core/guillotine.scala
+++ src/core/guillotine.scala
@@ -16,7 +16,7 @@ object envs:
   val empty: Env = Env(Map(), None)
 
 enum Context:
-  case Awaiting, Unquoted, DoubleQuotes, SingleQuotes
+  case Awaiting, Unquoted, Quotes2, Quotes1
 
 case class State(current: Context, esc: Boolean, args: List[String])
 
@@ -40,12 +40,6 @@ case class Command(args: String*):
 
 case class Env(vars: Map[String, String], workDir: Option[String]):
   private[guillotine] lazy val envArray: Array[String] = vars.map { (k, v) => s"$k=$v" }.to(Array)
-  
-  private[guillotine] lazy val javaMap: JMap[String, String] = vars.foldLeft(JMap()) { case (m, (k, v)) =>
-    m.put(k, v)
-    m
-  }
-
   private[guillotine] lazy val workDirFile: File = File(workDir.getOrElse(System.getenv("PWD")))
   
 case class ExecError(command: Command, stdout: Stream, stderr: Stream) extends Exception
@@ -55,10 +49,10 @@ object Sh extends Interpolator[List[String], State, Command]:
   
   def complete(state: State): Command =
     val args = state.current match
-      case DoubleQuotes       => throw ParseError("the double quotes have not been closed")
-      case SingleQuotes       => throw ParseError("the single quotes have not been closed")
-      case other if state.esc => throw ParseError("An escape character is not permitted at the end")
-      case _                  => state.args
+      case Quotes2        => throw ParseError("the double quotes have not been closed")
+      case Quotes1        => throw ParseError("the single quotes have not been closed")
+      case _ if state.esc => throw ParseError("an escape character is not permitted at the end")
+      case _              => state.args
     
     Command(args*)
 
@@ -67,32 +61,38 @@ object Sh extends Interpolator[List[String], State, Command]:
   def insert(state: State, value: Option[List[String]]): State = value.getOrElse(List("x")) match
     case Nil =>
       state
+
     case h :: t =>
-      if state.esc then throw ParseError("escaping with '\\' is not allowed immediately before a substitution")
+      if state.esc
+      then throw ParseError("escaping with '\\' is not allowed immediately before a substitution")
       
       state match
         case State(Awaiting, false, args) =>
           State(Unquoted, false, args ++ (h :: t))
+
         case State(Unquoted, false, args :+ last) =>
           State(Unquoted, false, args ++ (s"$last$h" :: t))
-        case State(SingleQuotes, false, args :+ last) =>
-          State(SingleQuotes, false, args :+ (s"$last$h" :: t).join(" "))
-        case State(DoubleQuotes, false, args :+ last) =>
-          State(DoubleQuotes, false, args :+ (s"$last$h" :: t).join(" "))
+        
+        case State(Quotes1, false, args :+ last) =>
+          State(Quotes1, false, args :+ (s"$last$h" :: t).join(" "))
+        
+        case State(Quotes2, false, args :+ last) =>
+          State(Quotes2, false, args :+ (s"$last$h" :: t).join(" "))
 
   def parse(state: State, next: String): State = next.foldLeft(state) {
-    case (State(Awaiting, esc, args), ' ')                      => State(Awaiting, false, args)
-    case (State(ctx, false, args), '\\') if ctx != SingleQuotes => State(ctx, true, args)
-    case (State(Unquoted, esc, args), ' ')                      => State(Awaiting, false, args)
-    case (State(SingleQuotes, esc, args), '\'')                 => State(Unquoted, false, args)
-    case (State(DoubleQuotes, false, args), '"')                => State(Unquoted, false, args)
-    case (State(Unquoted, false, args), '"')                    => State(DoubleQuotes, false, args)
-    case (State(Unquoted, false, args), '\'')                   => State(SingleQuotes, false, args)
-    case (State(Awaiting, false, args), '"')                    => State(DoubleQuotes, false, args :+ "")
-    case (State(Awaiting, false, args), '\'')                   => State(SingleQuotes, false, args :+ "")
-    case (State(Awaiting, esc, args), ch)                       => State(Unquoted, false, args :+ s"$ch")
-    case (State(ctx, esc, Nil), ch)                             => State(ctx, false, List(s"$ch"))
-    case (State(ctx, esc, rest :+ cur), ch)                     => State(ctx, false, rest :+ s"$cur$ch")
+    case (State(Awaiting, esc, args), ' ')          => State(Awaiting, false, args)
+    case (State(Quotes1, false, rest :+ cur), '\\') => State(Quotes1, false, rest :+ s"$cur\\")
+    case (State(ctx, false, args), '\\')            => State(ctx, true, args)
+    case (State(Unquoted, esc, args), ' ')          => State(Awaiting, false, args)
+    case (State(Quotes1, esc, args), '\'')          => State(Unquoted, false, args)
+    case (State(Quotes2, false, args), '"')         => State(Unquoted, false, args)
+    case (State(Unquoted, false, args), '"')        => State(Quotes2, false, args)
+    case (State(Unquoted, false, args), '\'')       => State(Quotes1, false, args)
+    case (State(Awaiting, false, args), '"')        => State(Quotes2, false, args :+ "")
+    case (State(Awaiting, false, args), '\'')       => State(Quotes1, false, args :+ "")
+    case (State(Awaiting, esc, args), char)         => State(Unquoted, false, args :+ s"$char")
+    case (State(ctx, esc, Nil), char)               => State(ctx, false, List(s"$char"))
+    case (State(ctx, esc, rest :+ cur), char)       => State(ctx, false, rest :+ s"$cur$char")
   }
 
 given Insertion[List[String], String] = value => List(value)
