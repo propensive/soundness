a712a71676842c2a3834b779bc6c86357e3d3f3a
Jon Pretty <jon.pretty@propensive.com>
2021-08-10 08:07:16 +0200

    Rewrite

---
+++ src/core/xylophone.scala
@@ -0,0 +1,319 @@
+package xylophone
+
+import wisteria.*
+import rudiments.*
+import contextual.*
+
+import scala.annotation.StaticAnnotation
+
+import language.dynamics
+
+case class Namespace(uri: String)
+case class Alias(id: String)
+
+case class TagName(namespace: Namespace | DefaultNamespace.type, name: String)
+
+object DefaultNamespace
+
+enum Ast:
+  case Element(name: TagName, children: Seq[Ast], attributes: Map[String, String], namespaces: Map[Alias, Namespace])
+  case Comment(content: String)
+  case ProcessingInstruction(target: String, content: String)
+  case Text(content: String)
+  case CData(content: String)
+  //case Attribute(namespace: Namespace | DefaultNamespace.type, key: String, value: String)
+  case Root(content: Ast*)
+
+  override def toString(): String = this match
+    case Element(name, children, attributes, namespaces) => str"<${name.name}${attributes.map(_+"="+_).join(" ", " ", "")}>${children.map(_.toString).join}</${name.name}>"
+    case Comment(content)                                => str"<!--$content-->"
+    case ProcessingInstruction(target, content)          => str"<?$target $content?>"
+    case Text(content)                                   => content
+    case CData(content)                                  => str"<![CDATA[${content.toString}]]>"
+    //case Attribute(namespace, key, value)                => str"$key=$value"
+    case Root(content*)                                  => str"<?xml?>${content.map(_.toString).join}"
+
+sealed trait Xml:
+  def pointer: List[Int | String | Unit]
+  def root: Ast.Root
+  def +(other: Xml): Doc
+
+  def string(using XmlPrinter[String]): String =
+    summon[XmlPrinter[String]].write(Doc(Ast.Root(Xml.normalize(this)*)))
+  
+  override def toString(): String =
+    printers.compact.write(Doc(Ast.Root(Xml.normalize(this)*)))
+
+case class Fragment(head: String | Unit, path: List[Int | String | Unit], root: Ast.Root)
+extends Xml, Dynamic:
+  def apply(idx: Int = 0): Node = Node(idx, head :: path, root)
+  def attribute(key: String): Attribute = apply(0).attribute(key)
+  def pointer: List[Int | String | Unit] = (head :: path).reverse
+  def selectDynamic(tagName: String): Fragment = Fragment(tagName, head :: path, root)
+  def applyDynamic(tagName: String)(idx: Int = 0): Node = selectDynamic(tagName).apply(idx)
+  def * : Fragment = Fragment((), head :: path, root)
+  def +(other: Xml): Doc = Doc(Ast.Root(Xml.normalize(this) ++ Xml.normalize(other)*))
+  def as[T: Xml.Reader]: T exposes AccessError = apply().as[T]
+  
+case class Node(head: Int, path: List[Int | String | Unit], root: Ast.Root) extends Xml, Dynamic:
+  def selectDynamic(tagName: String): Fragment = Fragment(tagName, head :: path, root)
+  def applyDynamic(tagName: String)(idx: Int = 0): Node = selectDynamic(tagName).apply(idx)
+  def attribute(attribute: String): Attribute = Attribute(this, attribute)
+  def pointer: List[Int | String | Unit] = (head :: path).reverse
+  def * : Fragment = Fragment((), head :: path, root)
+  def +(other: Xml): Doc = Doc(Ast.Root(Xml.normalize(this) ++ Xml.normalize(other)*))
+  
+  def as[T: Xml.Reader]: T =
+    summon[Xml.Reader[T]].read(Xml.normalize(this)).getOrElse(throw ReadError())
+
+case class Doc(root: Ast.Root) extends Xml, Dynamic:
+  def pointer: List[Int | String | Unit] = Nil
+  def selectDynamic(tagName: String): Fragment = Fragment(tagName, Nil, root)
+  def applyDynamic(tagName: String)(idx: Int = 0): Node = selectDynamic(tagName).apply(idx)
+  def * : Fragment = Fragment((), Nil, root)
+  def +(other: Xml): Doc = Doc(Ast.Root(Xml.normalize(this) ++ Xml.normalize(other)*))
+  
+  def as[T: Xml.Reader]: T =
+    summon[Xml.Reader[T]].read(Xml.normalize(this)).getOrElse(throw ReadError())
+
+case class Attribute(node: Node, attribute: String):
+  def as[T: Xml.Reader]: T =
+    val attributes = Xml.normalize(node).headOption match
+      case Some(Ast.Element(_, _, attributes, _)) => attributes
+      case _                                      => throw ReadError()
+
+    summon[Xml.Reader[T]]
+      .read(Seq(Ast.Element(TagName(DefaultNamespace, "empty"), Seq(Ast.Text(attributes(attribute))), Map(), Map())))
+      .getOrElse(throw ReadError())
+
+class XylophoneException(msg: String) extends Exception(str"xylophone: $msg")
+case class ParseError() extends Exception("could not parse XML")
+case class ReadError() extends XylophoneException("could not read value")
+case class AccessError() extends XylophoneException("could not access value")
+
+case class xmlAttribute() extends StaticAnnotation
+
+case class PartialXml(text: String)
+case class XmlInput(string: String)
+
+object XmlInterpolator extends Interpolator[XmlInput, PartialXml, Xml]:
+  def complete(state: PartialXml): Xml exposes ParseError = ???
+  def initial: PartialXml = ???
+  def insert(state: PartialXml, value: Option[XmlInput]): PartialXml exposes ParseError = ???
+  def parse(state: PartialXml, next: String): PartialXml = ???
+
+object Xml:
+
+  def text(value: Ast.Element): String = value.children.collect { case Ast.Text(txt) => txt }.join
+
+  object Writer extends Derivation[Writer]:
+    given Writer[String] = str =>
+      Ast.Element(TagName(DefaultNamespace, "String"), List(Ast.Text(str)), Map(), Map())
+    
+    given Writer[Int] = int =>
+      Ast.Element(TagName(DefaultNamespace, "Int"), List(Ast.Text(int.toString)), Map(), Map())
+
+    private val attributeAttribute = xmlAttribute()
+
+    def join[T](caseClass: CaseClass[Writer, T]): Writer[T] = value =>
+      val elements = caseClass.params.filter(!_.annotations.contains(attributeAttribute))
+      val attributes = caseClass.params.filter(_.annotations.contains(attributeAttribute))
+
+      Ast.Element(TagName(DefaultNamespace, caseClass.typeInfo.short), elements.map { param =>
+        param.typeclass.write(param.deref(value)).copy(name = TagName(DefaultNamespace, param.label))
+      }, attributes.map { param =>
+        param.label -> text(param.typeclass.write(param.deref(value)))
+      }.to(Map), Map())
+     
+    def split[T](sealedTrait: SealedTrait[Writer, T]): Writer[T] = value =>
+      sealedTrait.choose(value) { subtype =>
+        val xml = subtype.typeclass.write(subtype.cast(value))
+        Ast.Element(
+          TagName(DefaultNamespace, sealedTrait.typeInfo.short),
+          xml.children,
+          xml.attributes.updated("type", xml.name.name),
+          xml.namespaces
+        )
+      }
+
+  trait Writer[T]:
+    def write(value: T): Ast.Element
+    def contraMap[S](fn: S => T): Writer[S] = value => write(fn(value))
+
+  private def childElements(seq: Seq[Ast]): Seq[Ast] =
+    seq.collect { case e@Ast.Element(_, children, _, _) => children }.flatten
+
+  object Reader extends Derivation[Reader]:
+    given string: Reader[String] =
+      childElements(_).collect { case Ast.Text(txt) => txt }.headOption
+
+    given Reader[Int] = string.map(Int.unapply(_))
+    
+    def join[T](caseClass: CaseClass[Reader, T]): Reader[T] = seq =>
+      val elems = childElements(seq)
+      caseClass.constructMonadic { param =>
+        elems
+          .collect { case e: Ast.Element => e }
+          .find(_.name.name == param.label)
+          .flatMap { e => param.typeclass.read(Seq(e)) }
+      }
+    
+    def split[T](sealedTrait: SealedTrait[Reader, T]): Reader[T] = seq =>
+      seq.headOption match
+        case Some(Ast.Element(_, children, attributes, _)) =>
+          attributes
+            .get("type")
+            .flatMap { t => sealedTrait.subtypes.find(_.typeInfo.short == t) }
+            .flatMap(_.typeclass.read(seq))
+        case _ =>
+          None
+
+
+  trait Reader[T]:
+    def read(xml: Seq[Ast]): Option[T]
+    def map[S](fn: T => Option[S]): Reader[S] = read(_).flatMap(fn(_))
+
+  def normalize(xml: Xml): Seq[Ast] =
+    def recur(path: List[Int | String | Unit], current: Seq[Ast]): Seq[Ast] = path match
+      case Nil =>
+        current
+
+      case (idx: Int) :: tail =>
+        recur(tail, Seq(current(idx)))
+
+      case (unit: Unit) :: tail =>
+        recur(tail, current.collect { case e@Ast.Element(_, children, _, _) => children }.flatten.collect { case e: Ast.Element => e })
+
+      case (label: String) :: tail =>
+        val next = current
+          .collect { case e@Ast.Element(_, children, _, _) => children }
+          .flatten.collect { case e: Ast.Element if e.name.name == label => e }
+
+        recur(tail, next)
+    
+    recur(xml.pointer, xml.root.content)
+
+  def parse(content: String): Doc =
+    import org.w3c.dom as owd, owd.Node.*
+    import org.xml.sax as oxs
+    import javax.xml.parsers.*
+    import java.io.*
+
+    val factory = DocumentBuilderFactory.newInstance()
+    val builder = factory.newDocumentBuilder()
+    val root = 
+      try builder.parse(ByteArrayInputStream(content.bytes.unsafeMutable))
+      catch case _: oxs.SAXParseException => throw ParseError()
+    
+    def readNode(node: owd.Node): Ast = node.getNodeType match
+      case CDATA_SECTION_NODE =>
+        Ast.CData(node.getTextContent)
+      
+      case COMMENT_NODE =>
+        Ast.Comment(node.getTextContent)
+      
+      case ELEMENT_NODE =>
+        val tagName = TagName(DefaultNamespace, node.getNodeName)
+        val childNodes = node.getChildNodes
+        val children = (0 until childNodes.getLength).map(childNodes.item(_)).map(readNode(_))
+        val atts = (0 until node.getAttributes.getLength).map(node.getAttributes.item(_))
+        val attributes = atts.map { att => att.getNodeName -> att.getTextContent }.to(Map)
+        Ast.Element(tagName, children, attributes, Map())
+      
+      case PROCESSING_INSTRUCTION_NODE =>
+        val name = node.getNodeName
+        val content = node.getTextContent
+        Ast.ProcessingInstruction(name, content)
+      
+      case TEXT_NODE =>
+        Ast.Text(node.getTextContent)
+    
+    readNode(root.getDocumentElement) match
+      case elem@Ast.Element(_, _, _, _) => Doc(Ast.Root(elem))
+      case _                            => throw Impossible("xylophone: malformed XML")
+
+extension [T](value: T)
+  def xml(using write: Xml.Writer[T]): Doc = Doc(Ast.Root(write.write(value)))
+
+trait XmlPrinter[T]:
+  def write(doc: Xml): T
+
+object XmlPrinter:
+  given XmlPrinter[String] = StandardXmlPrinter(false)
+
+object printers:
+  given compact: XmlPrinter[String] = StandardXmlPrinter(true)
+
+class StandardXmlPrinter(compact: Boolean = false) extends XmlPrinter[String]:
+  def write(doc: Xml): String =
+    var indent: Int = 0
+    var linebreak: Boolean = false
+    val buf: StringBuilder = StringBuilder()
+    var pos: Int = 0
+    
+    def newline(n: Int = 0): Unit =
+      if !compact then
+        indent += n
+        linebreak = true
+
+    def append(strings: String*): Unit =
+      for str <- strings do
+        buf.append(str)
+        pos += str.length
+
+    def whitespace(): Unit =
+      if !compact && linebreak then
+        buf.append("\n")
+        for i <- 1 to indent do buf.append("  ")
+        pos = indent*2
+      linebreak = false
+
+    def inline(element: Ast.Element): Boolean = element.children.forall {
+      case Ast.Text(_) => true
+      case _       => false
+    }
+
+    def next(node: Ast): Unit = node match
+      case element@Ast.Element(tagName, children, attributes, namespaces) =>
+        whitespace()
+        append("<", tagName.name)
+        
+        for attribute <- attributes do attribute match
+          case (key, value) => append(" ", key, "=\"", value, "\"")
+        
+        append(">")
+        if !inline(element) then newline(1)
+        
+        for child <- element.children do
+          val splitLine = child match
+            case Ast.Text(_) => false
+            case _           => true
+          if splitLine then newline()
+          next(child)
+          if splitLine then newline()
+        
+        if !inline(element) then newline(-1)
+        
+        whitespace()
+        append("</", tagName.name, ">")
+        if !inline(element) then newline(0)
+
+      case Ast.Text(text) =>
+        whitespace()
+        append(text)
+      
+      case Ast.ProcessingInstruction(target, content) =>
+        whitespace()
+        append("<?", target, " ", content, "?>")
+        newline()
+
+      case Ast.Comment(content) =>
+        whitespace()
+        append("<!--", content, "-->")
+        newline()
+      
+      case e => println("Skipping "+e)
+        
+    doc.root.content.foreach(next(_))
+
+    buf.toString
--- src/test/tests.scala
+++ src/test/tests.scala
@@ -1,76 +1,109 @@
-package euphemism
+package xylophone
 
 import probably.*
-import scala.util.*
+import printers.compact
 
-case class Foo(x: Int, y: String) derives CanEqual, Json.Serializer, Json.Deserializer
+object Tests extends Suite("Xylophone tests"):
 
-object Tests extends Suite("Euphemism Tests"):
+  case class Person(name: String, age: Int)
+  case class Firm(name: String, ceo: Person)
+
+  case class Book(title: String, @xmlAttribute isbn: String)
+  case class Bibliography(author: String, book: Book)
+
+  enum Color:
+    case Rgb(red: Int, green: Int, blue: Int)
+    case Cmyk(cyan: Int, magenta: Int, yellow: Int, key: Int)
+
+  case class Pixel(x: Int, y: Int, color: Color)
+
   def run(using Runner): Unit =
-    suite("Parsing tests") {
-      test("Parse a number") {
-        Json.parse("42").as[Int]
-      }.assert(_ == 42)
-
-      test("Parse a string") {
-        Json.parse("\"string\"").as[String]
-      }.assert(_ == "string")
+    test("extract integer") {
+      Xml.parse("""<message>1</message>""").as[Int]
+    }.assert(_ == 1)
     
-      test("Parse true") {
-        Json.parse("true").as[Boolean]
-      }.assert(identity)
+    test("extract string") {
+      Xml.parse("""<message>Hello world</message>""").as[String]
+    }.assert(_ == "Hello world")
 
-      test("Parse false") {
-        Json.parse("false").as[Boolean]
-      }.assert(!_)
-
-      test("Parse float") {
-        Json.parse("3.1415").as[Float]
-      }.assert(_ == 3.1415f)
-      
-      test("Parse double") {
-        Json.parse("3.1415926").as[Double]
-      }.assert(_ == 3.1415926)
-    }
-
-    suite("Serialization") {
-      test("Serialize string") {
-        "foo".json.toString
-      }.assert(_ == """"foo"""")
-
-      test("Serialize double") {
-        3.14159.json.toString
-      }.assert(_ == "3.14159")
-      
-      test("Serialize true") {
-        true.json.toString
-      }.assert(_ == "true")
+    test("extract string from fragment") {
+      val xml = Xml.parse("""<message><info>Hello world</info></message>""")
+      xml.info.as[String]
+    }.assert(_ == "Hello world")
     
-      test("Serialize false") {
-        false.json.toString
-      }.assert(_ == "false")
-    }
+    test("extract string from node") {
+      val xml = Xml.parse("""<message><info>Hello world</info></message>""")
+      xml.info().as[String]
+    }.assert(_ == "Hello world")
+
+    test("serialize simple case class") {
+      val person = Person("Jack", 50)
+      person.xml.string
+    }.assert(_ == "<Person><name>Jack</name><age>50</age></Person>")
     
-    suite("Basic tests") {
-      test("Serialize to Json") {
-        Foo(1, "two").json
-      }.assert(_ == Json.of(x = 1.json, y = "two".json))
+    test("serialize nested case class") {
+      val person = Person("Jack", 50)
+      val company = Firm("Acme Inc", person)
+      company.xml.string
+    }.assert(_ == "<Firm><name>Acme Inc</name><ceo><name>Jack</name><age>50</age></ceo></Firm>")
 
-      test("Parse from JSON") {
-        Json.parse("""{"x": 1}""").debug
-      }.assert(_ == Json.of(x = 1.json))
+    test("access second element") {
+      val xml = Xml.parse("""<events><eventId>1</eventId><eventId>2</eventId></events>""")
+      xml.eventId(1).as[Int]
+    }.assert(_ == 2)
+    
+    test("extract to simple case class") {
+      val string = "<jack><name>Jack</name><age>50</age></jack>"
+      val xml = Xml.parse(string)
+      xml.as[Person]
+    }.assert(_ == Person("Jack", 50))
+    
+    test("extract to nested case class") {
+      val string = "<Firm><name>Acme Inc</name><ceo><name>Jack</name><age>50</age></ceo></Firm>"
+      val xml = Xml.parse(string)
+      xml.as[Firm]
+    }.assert(_ == Firm("Acme Inc", Person("Jack", 50)))
 
-      test("Read case class") {
-        Json.parse("""{"x": 1, "y": "two"}""").as[Foo].debug
-      }.assert(_ == Foo(1, "two"))
+    test("serialize with attribute") {
+      val book = Book("Lord of the Flies", "9780399529207")
+      book.xml.string
+    }.assert(_ == "<Book isbn=\"9780399529207\"><title>Lord of the Flies</title></Book>")
+    
+    test("serialize nested type with attribute") {
+      val bibliography = Bibliography("William Golding", Book("Lord of the Flies", "9780399529207"))
+      bibliography.xml.string
+    }.assert(_ == "<Bibliography><author>William Golding</author><book isbn=\"9780399529207\"><title>Lord of the Flies</title></book></Bibliography>")
 
-      test("Extract an option") {
-        case class OptFoo(x: Option[Int])
-        Json.parse("""{"x": 1}""").as[OptFoo].x
-      }.assert(_ == Some(1))
-      
-      test("Extract a None") {
-        case class OptFoo(x: Option[Int])
-        Json.parse("""{"y": 1}""").as[OptFoo].x
-      }.assert(_ == None)
-    }
+    test("serialize coproduct") {
+      val color: Color = Color.Rgb(5, 10, 15)
+      color.xml.string
+    }.assert(_ == """<Color type="Rgb"><red>5</red><green>10</green><blue>15</blue></Color>""")
+    
+    test("serialize nested coproduct") {
+      val pixel: Pixel = Pixel(100, 200, Color.Cmyk(1, 2, 3, 4))
+      pixel.xml.string
+    }.assert(_ == """<Pixel><x>100</x><y>200</y><color type="Cmyk"><cyan>1</cyan><magenta>2</magenta><yellow>3</yellow><key>4</key></color></Pixel>""")
+
+    test("read coproduct") {
+      val string = """<Color type="Cmyk"><cyan>1</cyan><magenta>2</magenta><yellow>3</yellow><key>4</key></Color>"""
+      val xml = Xml.parse(string)
+      xml.as[Color]
+    }.assert(_ == Color.Cmyk(1, 2, 3, 4))
+    
+    test("read nested coproduct") {
+      val string = """<Pixel><x>100</x><y>200</y><color type="Cmyk"><cyan>1</cyan><magenta>2</magenta><yellow>3</yellow><key>4</key></color></Pixel>"""
+      val xml = Xml.parse(string)
+      xml.as[Pixel]
+    }.assert(_ == Pixel(100, 200, Color.Cmyk(1, 2, 3, 4)))
+
+    test("read attribute value from fragment") {
+      val string = """<node><content key="value"/></node>"""
+      val xml = Xml.parse(string)
+      xml.content.attribute("key").as[String]
+    }.assert(_ == "value")
+    
+    test("read attribute value from node") {
+      val string = """<node><content key="value"/></node>"""
+      val xml = Xml.parse(string)
+      xml.content().attribute("key").as[String]
+    }.assert(_ == "value")
