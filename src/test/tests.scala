bb00e1c31284b39d945d26fde5307695af14f0fd
Jon Pretty <jon.pretty@propensive.com>
2021-08-11 16:13:54 +0200

    Tidy up tests and fix broken one

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -30,100 +30,100 @@ object Tests extends Suite("Guillotine tests"):
       test("parse a substitution") {
         val flags = "-la"
         val file = "filename"
-        sh"ls $flags".debug
+        sh"ls $flags"
       }.assert(_ == Command("ls", "-la"))
       
       test("parse two substitutions") {
         val flags = "-la"
         val file = "filename"
-        sh"ls $flags $file".debug
+        sh"ls $flags $file"
       }.assert(_ == Command("ls", "-la", "filename"))
       
       test("parse irregular spacing") {
         val flags = "-la"
         val file = "filename"
-        sh"ls  $flags     $file".debug
+        sh"ls  $flags     $file"
       }.assert(_ == Command("ls", "-la", "filename"))
       
       test("parse irregular spacing") {
         val flags = "-la"
         val file = "filename"
-        sh"ls  $flags $file".debug
+        sh"ls  $flags $file"
       }.assert(_ == Command("ls", "-la", "filename"))
       
       test("adjacent substitutions") {
         val a = "a"
         val b = "b"
-        sh"ls $a$b".debug
+        sh"ls $a$b"
       }.assert(_ == Command("ls", "ab"))
       
       test("substitute a list") {
         val a = List("a", "b")
-        sh"ls $a".debug
+        sh"ls $a"
       }.assert(_ == Command("ls", "a", "b"))
       
       test("substitute a single-quoted list") {
         val a = List("a", "b")
-        sh"ls '$a'".debug
+        sh"ls '$a'"
       }.assert(_ == Command("ls", "a b"))
       
       test("substitute in a double-quoted list") {
         val a = List("a", "b")
-        sh"""ls "$a"""".debug
+        sh"""ls "$a""""
       }.assert(_ == Command("ls", "a b"))
       
       test("insertion after arg") {
         val a = List("a", "b")
-        sh"""ls ${a}x""".debug
+        sh"""ls ${a}x"""
       }.assert(_ == Command("ls", "a", "bx"))
       
       test("insertion before arg") {
         val a = List("a", "b")
-        sh"""ls x${a}""".debug
+        sh"""ls x${a}"""
       }.assert(_ == Command("ls", "xa", "b"))
       
       test("insertion before quoted arg") {
         val a = List("a", "b")
-        sh"""ls ${a}'x'""".debug
+        sh"""ls ${a}'x'"""
       }.assert(_ == Command("ls", "a", "bx"))
       
       test("insertion after quoted arg") {
         val a = List("a", "b")
-        sh"""ls 'x'${a}""".debug
+        sh"""ls 'x'${a}"""
       }.assert(_ == Command("ls", "xa", "b"))
       
       test("empty list insertion") {
         val a = List()
-        sh"""ls ${a}""".debug
+        sh"""ls ${a}"""
       }.assert(_ == Command("ls"))
       
       test("empty list insertion") {
         val a = List()
-        sh"""ls '${a}'""".debug
+        sh"""ls '${a}'"""
       }.assert(_ == Command("ls", ""))
       
       test("empty parameters") {
-        sh"""ls '' ''""".debug
+        sh"""ls '' ''"""
       }.assert(_ == Command("ls", "", ""))
       
       test("one empty parameter, specified twice") {
-        sh"""ls ''''""".debug
+        sh"""ls ''''"""
       }.assert(_ == Command("ls", ""))
       
       test("single quote inside double quotes") {
-        sh"""ls "'" """.debug
+        sh"""ls "'" """
       }.assert(_ == Command("ls", "'"))
       
       test("double quote inside single quotes") {
-        sh"""ls '"' """.debug
+        sh"""ls '"' """
       }.assert(_ == Command("ls", "\""))
       
       test("escaped double quote") {
-        sh"""ls \" """.debug
+        sh"""ls \" """
       }.assert(_ == Command("ls", "\""))
       
       test("escaped single quote") {
-        sh"""ls \' """.debug
+        sh"""ls \' """
       }.assert(_ == Command("ls", "'"))
     }
 
@@ -132,9 +132,7 @@ object Tests extends Suite("Guillotine tests"):
       given Env = envs.enclosing
 
       test("Echo string") {
-        val result = sh"echo hello".exec[String]()
-        println(result)
-        result
+        sh"echo hello".exec[String]()
       }.assert(_ == "hello")
     }
 
@@ -143,7 +141,7 @@ object Tests extends Suite("Guillotine tests"):
 
       test("Check final escape character is an error") {
         try Check("""hello\""") catch error => error
-      }.assert(_ == ParseError("An escape character is not permitted at the end"))
+      }.assert(_ == ParseError("an escape character is not permitted at the end"))
       
       test("unclosed single quotes") {
         try Check("""hello ' world""") catch error => error
