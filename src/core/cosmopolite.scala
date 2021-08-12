c417dad1bf16b0fff0cc7bb0b0aee53e91a47e2c
Jon Pretty <jon.pretty@propensive.com>
2021-08-12 11:16:23 +0200

    Avoid `???` in macro

--- src/core/cosmopolite.scala
+++ src/core/cosmopolite.scala
@@ -43,8 +43,7 @@ object Language:
          case ConstantType(StringConstant(lang)) =>
            Set(lang)
          case _ =>
-           report.error("cosmopolite: expected an intersection or union type")
-           ???
+           report.throwError("cosmopolite: expected an intersection or union type")
 
       Expr(langs(TypeRepr.of[L]))
 
