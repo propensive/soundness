bf63cd03c27e04c548168a6626938c7270dd089e
Jon Pretty <jon.pretty@propensive.com>
2021-10-08 09:19:23 +0200

    Remove ANSI code that duplicates what's in Escapade

--- src/core/tabulation.scala
+++ src/core/tabulation.scala
@@ -18,53 +18,7 @@ package escritoire
 
 import rudiments.*
 import gossamer.*
-
-object Ansi:
-  val esc = 27.toChar
-  val reset: String = s"$esc[39;49m"
-  def bold(str: String): String = s"$esc[1m$str$esc[22m"
-  def underline(str: String): String = s"$esc[4m$str$esc[24m"
-  def strike(str: String): String = s"$esc[9m$str$esc[29m"
-  def italic(str: String): String = s"$esc[3m$str$esc[23m"
-  def reverse(str: String): String = s"$esc[7m$str$esc[27m"
-  def up(n: Int): String = s"$esc[${n}A"
-  def down(n: Int): String = s"$esc[${n}B"
-  def right(n: Int): String = s"$esc[${n}C"
-  def left(n: Int): String = s"$esc[${n}D"
-
-  case class Color(red: Int, green: Int, blue: Int):
-    def apply(str: String): String = s"${apply()}$str$reset"
-    def fade(amount: Double) = Color((red*amount).toInt, (green*amount).toInt, (blue*amount).toInt)
-    
-    def brighten(amount: Double) = Color(
-      (255 - (255 - red)*amount).toInt,
-      (255 - (255 - green)*amount).toInt,
-      (255 - (255 - blue)*amount).toInt
-    )
-    
-    def apply(): String = s"$esc[38;2;$red;$green;${blue}m"
-    def escaped: String = s"%{${apply()}%}"
-
-  object Color:
-    // Colors are taken from the solarized palette
-    val base03: Color = Color(0, 43, 54)
-    val base02: Color = Color(7, 54, 66)
-    val base01: Color = Color(88, 110, 117)
-    val base00: Color = Color(101, 123, 131)
-    val base0: Color = Color(131, 148, 150)
-    val base1: Color = Color(147, 161, 161)
-    val base2: Color = Color(238, 232, 213)
-    val base3: Color = Color(253, 246, 227)
-    val yellow: Color = Color(181, 137, 0)
-    val orange: Color = Color(203, 75, 22)
-    val red: Color = Color(220, 50, 47)
-    val magenta: Color = Color(211, 54, 130)
-    val violet: Color = Color(108, 113, 196)
-    val blue: Color = Color(38, 139, 210)
-    val cyan: Color = Color(42, 161, 152)
-    val green: Color = Color(133, 153, 0)
-  
-  def strip(string: String): String = string.replaceAll("""\e\[?.*?[\@-~]""", "").nn
+import escapade.*
 
 enum Align:
   case Left
@@ -79,7 +33,7 @@ object Heading:
   def apply[Row, V: AnsiShow](name: String, getter: Row => V, width: Width = Width.Flexible,
                                   align: Align = Align.Left): Heading[Row] =
     new Heading[Row](name, width, align):
-      def get(r: Row): String = summon[AnsiShow[V]].show(getter(r))
+      def get(r: Row): String = summon[AnsiShow[V]].ansiShow(getter(r)).render
 
 trait Heading[Row](val name: String, val width: Width = Width.Flexible,
                        val align: Align = Align.Left):
@@ -89,10 +43,10 @@ case class Tabulation[Row](headings: Heading[Row]*):
   def padding: Int = 2
 
   def tabulate(maxWidth: Int, rows: Seq[Row], ansi: Option[String] = None): Seq[String] =
-    val reset = ansi.fold("") { _ => Ansi.reset }
+    val reset = ansi.fold("") { _ => s"${27.toChar}${escapes.Reset.on}" }
     val titleStrings = headings.to(List).map(_.name).map(List(_))
     
-    val data: Seq[List[List[String]]] = titleStrings.map(_.map(Ansi.bold(_))) +: (rows.map { row =>
+    val data: Seq[List[List[String]]] = titleStrings.map(_.map { str => ansi"$Bold($str)".render }) +: (rows.map { row =>
       headings.to(List).map(_.get(row).cut("\n").to(List))
     })
 
@@ -162,19 +116,3 @@ case class Tabulation[Row](headings: Heading[Row]*):
         if stripped > width
         then pad(str.drop((stripped - width) / 2), width, Align.Left, reset)+reset
         else pad(str+" "*((width - stripped)/2), width, Align.Right, reset)
-
-object AnsiShow:
-  private val decimalFormat =
-    val df = new java.text.DecimalFormat()
-    df.setMinimumFractionDigits(3)
-    df.setMaximumFractionDigits(3)
-    df
-
-  given AnsiShow[String] = identity(_)
-  given AnsiShow[Int] = _.toString
-  given AnsiShow[Long] = _.toString
-  given AnsiShow[Double] = decimalFormat.format(_).nn
-  given AnsiShow[Seq[String]] = _.join("\n")
-
-trait AnsiShow[T]:
-  def show(value: T): String
--- src/test/tests.scala
+++ src/test/tests.scala
@@ -18,6 +18,8 @@ package escritoire
 
 import probably.*
 
+import unsafeExceptions.canThrowAny
+
 object Tests extends Suite("Escritoire tests"):
   def run(using Runner): Unit =
     ()
