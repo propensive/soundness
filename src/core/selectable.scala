c74252553dba09c547812b6239c88f3b78c56623
Jon Pretty <jon.pretty@propensive.com>
2021-12-15 13:07:52 +0100

    Support typed schemas

--- src/core/selectable.scala
+++ src/core/selectable.scala
@@ -1,18 +1,41 @@
 package polyvinyl
 
 import scala.quoted.*
+import scala.compiletime.*
 
 trait Schema[F]:
   def fields: List[String]
+  transparent inline def record(inline fn: String => F): Record[F]
   
-  final def build[F: Type, S <: Singleton & Schema[F]: Type](fn: Expr[String => F])(using Quotes): Expr[Record[F]] =
+  final def build[F: Type](fn: Expr[String => F])(using Quotes): Expr[Record[F]] =
     import quotes.*, quotes.reflect.*
-    val typ = TypeRepr.of[Record[F]]
-    fields.foldLeft(typ)(Refinement(_, _, TypeRepr.of[F])).asType match
+    fields.foldLeft(TypeRepr.of[Record[F]])(Refinement(_, _, TypeRepr.of[F])).asType match
       case '[typ] => '{new Record[F]($fn(_)).asInstanceOf[typ & Record[F]]}
       case _      => ???
   
-  transparent inline def record(inline fn: String => Int): Record[Int]
-
 class Record[Field](fn: String => Field) extends Selectable:
   def selectDynamic(name: String): Field = fn(name)
+
+class TypedRecord(fn: String => Any) extends Selectable:
+  def selectDynamic(name: String): Any = fn(name)
+
+trait TypedSchema[E <: reflect.Enum]:
+  def types: Map[String, E]
+  transparent inline def record(inline fn: String => Any): TypedRecord
+  type EnumType = E
+  type Result[E1 <: E]
+
+  def build(fn: Expr[String => Any])(using Quotes, Type[E], Type[Result]): Expr[TypedRecord] =
+    import quotes.*, quotes.reflect.*
+
+    types.foldLeft(TypeRepr.of[TypedRecord]):
+      case (acc, (key, etype)) =>
+        val companion = Ref(TypeRepr.of[E].typeSymbol.companionModule)
+        val sym = companion.symbol.declaredField(etype.toString)
+        val returnType = Singleton(companion.select(sym)).tpe.asType match
+          case '[typ] => TypeRepr.of[Result[typ & E]].simplified
+
+        Refinement(acc, key, returnType)
+    .asType match
+      case '[typ] => '{new TypedRecord($fn(_)).asInstanceOf[typ & TypedRecord]}
+      case _      => ???
