8512707e69818303ba8482f2f49ff5374f35f861
Jon Pretty <jon.pretty@propensive.com>
2021-12-15 14:17:04 +0100

    Tidyup of sources, plus full docs

--- src/core/selectable.scala
+++ src/core/selectable.scala
@@ -1,41 +1,44 @@
 package polyvinyl
 
+import rudiments.*
+
 import scala.quoted.*
 import scala.compiletime.*
 
-trait Schema[F]:
+trait SimpleSchema[F]:
   def fields: List[String]
-  transparent inline def record(inline fn: String => F): Record[F]
+  transparent inline def record(inline fn: String => F): SimpleRecord[F]
   
-  final def build[F: Type](fn: Expr[String => F])(using Quotes): Expr[Record[F]] =
+  final def build[F: Type](fn: Expr[String => F])(using Quotes): Expr[SimpleRecord[F]] =
     import quotes.*, quotes.reflect.*
-    fields.foldLeft(TypeRepr.of[Record[F]])(Refinement(_, _, TypeRepr.of[F])).asType match
-      case '[typ] => '{new Record[F]($fn(_)).asInstanceOf[typ & Record[F]]}
-      case _      => ???
+    fields.foldLeft(TypeRepr.of[SimpleRecord[F]])(Refinement(_, _, TypeRepr.of[F])).asType match
+      case '[typ] => '{new SimpleRecord[F]($fn(_)).asInstanceOf[typ & SimpleRecord[F]]}
+      case _      => throw Impossible("the first case should always match")
   
-class Record[Field](fn: String => Field) extends Selectable:
+class SimpleRecord[Field](fn: String => Field) extends Selectable:
   def selectDynamic(name: String): Field = fn(name)
 
-class TypedRecord(fn: String => Any) extends Selectable:
+class Record(fn: String => Any) extends Selectable:
   def selectDynamic(name: String): Any = fn(name)
 
-trait TypedSchema[E <: reflect.Enum]:
+trait Schema[E <: reflect.Enum]:
   def types: Map[String, E]
-  transparent inline def record(inline fn: String => Any): TypedRecord
+  transparent inline def record(inline fn: String => Any): Record
   type EnumType = E
-  type Result[E1 <: E]
+  type Result[_ <: E]
 
-  def build(fn: Expr[String => Any])(using Quotes, Type[E], Type[Result]): Expr[TypedRecord] =
+  def build(fn: Expr[String => Any])(using Quotes, Type[E], Type[Result]): Expr[Record] =
     import quotes.*, quotes.reflect.*
 
-    types.foldLeft(TypeRepr.of[TypedRecord]):
+    types.foldLeft(TypeRepr.of[Record]):
       case (acc, (key, etype)) =>
         val companion = Ref(TypeRepr.of[E].typeSymbol.companionModule)
         val sym = companion.symbol.declaredField(etype.toString)
         val returnType = Singleton(companion.select(sym)).tpe.asType match
           case '[typ] => TypeRepr.of[Result[typ & E]].simplified
+          case _      => throw Impossible("the first case should always match")
 
         Refinement(acc, key, returnType)
     .asType match
-      case '[typ] => '{new TypedRecord($fn(_)).asInstanceOf[typ & TypedRecord]}
+      case '[typ] => '{new Record($fn(_)).asInstanceOf[typ & Record]}
       case _      => ???
