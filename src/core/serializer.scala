0bdba46cea066041597cdbf1aa7cd538233409c6
Jon Pretty <jon.pretty@propensive.com>
2022-01-06 11:40:12 +0100

    Update headers for 2022, and exception fixes

--- src/core/serializer.scala
+++ src/core/serializer.scala
@@ -1,5 +1,5 @@
 /*
-    Honeycomb, version 0.9.0. Copyright 2018-21 Jon Pretty, Propensive OÜ.
+    Honeycomb, version 0.9.0. Copyright 2018-22 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
