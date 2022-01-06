eab81975df8661196e2877f1150f0139da341700
Jon Pretty <jon.pretty@propensive.com>
2022-01-06 11:40:30 +0100

    Update headers for 2022, and exception fixes

--- src/servlet/servlet.scala
+++ src/servlet/servlet.scala
@@ -1,5 +1,5 @@
 /*
-    Scintillate, version 0.16.0. Copyright 2021-21 Jon Pretty, Propensive OÜ.
+    Scintillate, version 0.16.0. Copyright 2021-22 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
