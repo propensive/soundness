b194f4efeb8f5e167aff5d3661c6cc6d3483dbe3
Jon Pretty <jon.pretty@propensive.com>
2022-01-20 10:57:47 +0100

    Update version to 0.4.0

--- src/core/eucalyptus.scala
+++ src/core/eucalyptus.scala
@@ -1,5 +1,5 @@
 /*
-    Eucalyptus, version 0.2.0. Copyright 2018-22 Jon Pretty, Propensive OÜ.
+    Eucalyptus, version 0.4.0. Copyright 2018-22 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
