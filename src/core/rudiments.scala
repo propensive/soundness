9e3f45683296673cdacb74c57af4988dc7dcb1e8
Jon Pretty <jon.pretty@propensive.com>
2022-03-21 11:57:51 +0100

    Break out Turbulence code

--- src/core/rudiments.scala
+++ src/core/rudiments.scala
@@ -27,6 +27,10 @@ import scala.util.CommandLineParser
 
 import language.dynamics
 
+import scala.util.{Try, Success, Failure}
+
+import java.util.concurrent as juc
+
 export scala.reflect.{ClassTag, Typeable}
 export scala.collection.immutable.{Set, List, ListMap, Map, TreeSet, TreeMap}
 
@@ -40,7 +44,6 @@ export scala.jdk.CollectionConverters.*
 export scala.annotation.{tailrec, implicitNotFound, targetName, switch, StaticAnnotation}
 
 type Bytes = IArray[Byte]
-type DataStream = LazyList[IArray[Byte] throws StreamCutError]
 
 opaque type Text = String
 
@@ -66,14 +69,6 @@ object Bytes:
 case class ExcessDataError(size: ByteSize, limit: ByteSize) extends Error:
   def message: Text =
     Text(s"the amount of data in the stream (at least ${size}B) exceeds the limit (${limit}B)")
-
-extension (value: DataStream)
-  def slurp(limit: ByteSize): Bytes throws ExcessDataError | StreamCutError =
-    value.foldLeft(IArray[Byte]()):
-      (acc, next) =>
-        if acc.length + next.length > limit
-        then throw ExcessDataError(acc.length + next.length, limit)
-        else acc ++ next
 
 extension [T](value: T)
   def only[S](pf: PartialFunction[T, S]): Option[S] = Some(value).collect(pf)
@@ -149,149 +144,14 @@ extension (iarray: IArray.type)
 
 extension [T](opt: Option[T]) def maybe: Unset.type | T = opt.getOrElse(Unset)
 
-case class StreamCutError() extends Error:
-  def message: Text = Text("the stream was cut prematurely")
-
-object Util:
-  def readInputStream(in: ji.InputStream, limit: ByteSize): DataStream = in match
-    case in: ji.BufferedInputStream =>
-      def read(): DataStream =
-        try
-          val avail = in.available
-          
-          val buf = new Array[Byte]((if avail == 0 then limit.long.toInt else avail min
-              limit.long.toInt))
-          
-          val count = in.read(buf, 0, buf.length)
-          if count < 0 then LazyList()
-          else if avail == 0 then buf.slice(0, count).unsafeImmutable #:: read()
-          else buf.unsafeImmutable #:: read()
-        catch case error: ji.IOException => LazyList(throw StreamCutError())
-
-      read()
-    
-    case in: ji.InputStream =>
-      readInputStream(ji.BufferedInputStream(in), limit)
-  
-  def write(stream: DataStream, out: ji.OutputStream): Unit throws StreamCutError =
-    stream.map(_.unsafeMutable).foreach(out.write(_))
-
-object Source:
-  given Source[SystemIn.type] with
-    type E = StreamCutError
-    def read(value: SystemIn.type): DataStream throws StreamCutError =
-      if System.in == null then throw StreamCutError() else Util.readInputStream(System.in, 10.mb)
-
-trait Source[T]:
-  type E <: Exception
-  def read(value: T): DataStream throws E
-
-object Sink:
-  given Sink[SystemOut.type] with
-    type E = StreamCutError
-    def write(value: SystemOut.type, stream: DataStream) =
-      if System.out == null then throw StreamCutError() else Util.write(stream, System.out)
-  
-  given Sink[SystemErr.type] with
-    type E = StreamCutError
-    def write(value: SystemErr.type, stream: DataStream) =
-      if System.err == null then throw StreamCutError() else Util.write(stream, System.err)
-  
-  given Sink[ji.OutputStream] with
-    type E = StreamCutError
-    def write(sink: ji.OutputStream, stream: DataStream) =
-      val out = sink match
-        case out: ji.BufferedOutputStream => out
-        case out: ji.OutputStream         => ji.BufferedOutputStream(out)
-      
-      Util.write(stream, out)
-
-trait Sink[T]:
-  type E <: Exception
-  def write(value: T, stream: DataStream): Unit throws E | StreamCutError
-
-object SafeStreamable:
-  given SafeStreamable[LazyList[Bytes]] = identity(_)
-
-object Streamable:
-  given Streamable[DataStream] = identity(_)
-  given Streamable[Bytes] = LazyList(_)
-
-  given Streamable[Text] = value => LazyList(value.s.getBytes("UTF-8").nn.unsafeImmutable)
-
-trait Streamable[T]:
-  def stream(value: T): DataStream
-
-trait SafeStreamable[T] extends Streamable[T]:
-  def safeStream(value: T): LazyList[Bytes]
-  def stream(value: T): DataStream = safeStream(value).map(identity(_))
-
-object Readable:
-  given Readable[DataStream] with
-    type E = Nothing
-    def read(stream: DataStream): DataStream throws E | StreamCutError = stream
-  
-  given Readable[Bytes] with
-    type E = ExcessDataError
-    def read(stream: DataStream): Bytes throws ExcessDataError | StreamCutError =
-      stream.slurp(10.mb)
-
-  given (using enc: Encoding): Readable[Text] with
-    type E = ExcessDataError
-    def read(value: DataStream) = Text(String(value.slurp(1.mb).unsafeMutable, enc.name.s))
-
-  given textReader(using enc: Encoding): Readable[LazyList[Text]] with
-    type E = ExcessDataError
-    def read(stream: DataStream) =
-      
-      def read(stream: DataStream, carried: Array[Byte] = Array.empty[Byte]): LazyList[Text] =
-        if stream.isEmpty then LazyList()
-        else
-          // FIXME: constructing this new array may be unnecessarily costly.
-          val buf = carried ++ stream.head.unsafeMutable
-          val carry = enc.carry(buf)
-          
-          Text(String(buf, 0, buf.length - carry, enc.name.s)) #::
-              read(stream.tail, buf.takeRight(carry))
-      
-      read(stream)
-
 trait Encoding:
   def name: Text
   def carry(array: Array[Byte]): Int
-
-trait Readable[T]:
-  readable =>
-    type E <: Exception
-    def read(stream: DataStream): T throws E | StreamCutError
-    def map[S](fn: T => S): rudiments.Readable[S] { type E = readable.E } =
-      new rudiments.Readable[S]:
-        type E = readable.E
-        def read(stream: DataStream): S throws E | StreamCutError =
-          fn(readable.read(stream))
-
-object SystemIn
-object SystemOut
-object SystemErr
 
 def safely[T](value: => CanThrow[Exception] ?=> T): Maybe[T] =
   try value(using unsafeExceptions.canThrowAny) catch NonFatal => Unset
 
 def unsafely[T](value: => CanThrow[Exception] ?=> T): T = value(using unsafeExceptions.canThrowAny)
-
-extension [T](value: T)
-  def dataStream(using src: Source[T]): DataStream throws src.E = src.read(value)
-  
-  def writeStream(stream: DataStream)(using sink: Sink[T]): Unit throws sink.E | StreamCutError =
-    sink.write(value, stream)
-  
-  def writeTo[S](destination: S)(using sink: Sink[S], streamable: Streamable[T])
-                : Unit throws sink.E | StreamCutError =
-    sink.write(destination, streamable.stream(value))
-
-  def read[S](using readable: Readable[S], src: Source[T])
-      : S throws readable.E | src.E | StreamCutError =
-    readable.read(dataStream)
 
 extension [T](xs: Iterable[T])
   transparent inline def mtwin: Iterable[(T, T)] = xs.map { x => (x, x) }
@@ -320,78 +180,10 @@ extension (bs: Long)
 
 opaque type ByteSize = Long
 
-extension (obj: LazyList.type)
-  def multiplex[T](streams: LazyList[T]*): LazyList[T] =
-    import scala.concurrent.*
-    given ExecutionContext = ExecutionContext.Implicits.global
-
-    def future(stream: LazyList[T], idx: Int): Future[Int] = Future:
-      blocking:
-        stream.isEmpty
-        idx
-    
-    var vector: Vector[LazyList[T]] = streams.to(Vector)
-    var futuresVector: Vector[Future[Int]] = vector.zipWithIndex.map(future(_, _))
-
-    def recur(futures: Set[Future[Int]]): LazyList[T] =
-      if futures.isEmpty then LazyList()
-      else
-        val finished = Future.firstCompletedOf(futures)
-        val idx: Int = finished.await()
-        val stream = vector(idx)
-        if stream.isEmpty then recur(futures - futuresVector(idx))
-        else
-          vector = vector.updated(idx, stream.tail)
-          val oldFuture = futuresVector(idx)
-          val newFuture = future(stream.tail, idx)
-          futuresVector = futuresVector.updated(idx, newFuture)
-          stream.head #:: recur(futures - oldFuture + newFuture)
-    
-    recur(futuresVector.to(Set))
-
-extension [T](stream: LazyList[T])
-  def cluster(interval: Long, maxSize: Maybe[Int] = Unset): LazyList[List[T]] =
-    import scala.concurrent.*
-    given ExecutionContext = ExecutionContext.Implicits.global
-
-    class Timer():
-      private var exp: Long = System.currentTimeMillis + interval
-      
-      val future: Future[None.type] =
-        Future {
-          blocking {
-            @tailrec
-            def waitUntil(time: Long): Unit =
-              Thread.sleep(time - System.currentTimeMillis)
-              if time != exp then waitUntil(exp)
-            
-            waitUntil(exp)
-            None
-          }
-        }
-      
-      def postpone(): Unit = exp = System.currentTimeMillis + interval
-
-    def recur(stream: LazyList[T], list: List[T], timer: Option[Timer]): LazyList[List[T]] =
-      if list.length == maxSize then list #:: recur(stream, Nil, None)
-      else timer match
-        case None =>
-          if stream.isEmpty then LazyList[List[T]]()
-          else recur(stream.tail, stream.head :: list, Some(Timer()))
-        case Some(t) =>
-          val Timeout = t.future
-          val NextReady = Future(blocking(Some(stream.isEmpty)))
-          
-          Future.firstCompletedOf(Iterable(Timeout, NextReady)).await() match
-            case None =>
-              list #:: recur(stream, Nil, None)
-            
-            case Some(empty) =>
-              if empty then LazyList() else
-                t.postpone()
-                recur(stream.tail, stream.head :: list, Some(t))
-
-    recur(stream, Nil, None)
+case class Trigger():
+  private val promise: Promise[Trigger] = Promise()
+  def pull(): Unit = synchronized(if !promise.isCompleted then promise.complete(Success(this)))
+  def future: Future[Trigger] = promise.future
 
 object ByteSize:
 
