319e5c70581d745d738833b3b9495d44a8efc282
Jon Pretty <jon.pretty@propensive.com>
2022-03-28 22:28:25 +0200

    Initial checkin

---
+++ doc/name
@@ -0,0 +1,2 @@
+This project is particular to Linux operating systems, whos mascot is a penguin. While __Imperial__ defines the layout of the
+operating system's own (little) empire, "Emperor" is also a species of penguin.
---
+++ src/core/structure.scala
@@ -0,0 +1,66 @@
+package imperial
+
+import rudiments.*
+import clairvoyant.*
+
+object BaseLayout:
+  case class Dir(path: String)
+
+case class BaseLayout(private val path: String, private val env: Maybe[String] = Unset,
+                            readOnly: Boolean = false)
+                       (using baseDir: BaseLayout.Dir):
+  def absolutePath: String = s"${baseDir.path}/$path"
+  given newBaseDir: BaseLayout.Dir = BaseLayout.Dir(absolutePath)
+  
+  def apply[T]()(using DirectoryProvider[T]): T =
+    val path: String = env.option match
+      case None      => absolutePath
+      case Some(env) => Option(System.getenv(env)).map(_.nn) match
+        case None       => absolutePath
+        case Some(path) => path
+
+    summon[DirectoryProvider[T]].make(path)
+
+object Root extends BaseLayout("")(using BaseLayout.Dir("")):
+  override def absolutePath: String = ""
+  override def apply[T]()(using DirectoryProvider[T]): T = summon[DirectoryProvider[T]].make("/")
+  object Boot extends BaseLayout("boot")
+  object Efi extends BaseLayout("efi")
+  object Etc extends BaseLayout("etc")
+  object Home extends BaseLayout("home")
+  object Srv extends BaseLayout("srv")
+  object Tmp extends BaseLayout("tmp")
+  object Run extends BaseLayout("run"):
+    object Log extends BaseLayout("log")
+    object User extends BaseLayout("user")
+  object Usr extends BaseLayout("usr"):
+    object Bin extends BaseLayout("bin")
+    object Include extends BaseLayout("include")
+    object Lib extends BaseLayout("lib")
+    object Share extends BaseLayout("share"):
+      object Doc extends BaseLayout("doc")
+      object Factory extends BaseLayout("factory"):
+        object Etc extends BaseLayout("etc")
+        object Var extends BaseLayout("var")
+  object Var extends BaseLayout("var"):
+    object Cache extends BaseLayout("cache")
+    object Lib extends BaseLayout("lib")
+    object Log extends BaseLayout("log")
+    object Spool extends BaseLayout("spool")
+    object Tmp extends BaseLayout("tmp", "TMPDIR")
+  object Dev extends BaseLayout("dev"):
+    object Shm extends BaseLayout("shm")
+  object Proc extends BaseLayout("proc"):
+    object Sys extends BaseLayout("sys")
+  object Sys extends BaseLayout("sys")
+
+object Home extends BaseLayout("~")(using BaseLayout.Dir(System.getenv("HOME").nn)):
+  override def absolutePath: String = System.getenv("HOME").nn
+
+  object Cache extends BaseLayout(".cache", "XDG_CACHE_HOME")
+  object Config extends BaseLayout(".config", "XDG_CONFIG_HOME")
+  object Local extends BaseLayout(".local"):
+    object Bin extends BaseLayout("bin")
+    object Lib extends BaseLayout("lib")
+    object Share extends BaseLayout("share", "XDG_DATA_HOME")
+    object State extends BaseLayout("state", "XDG_STATE_HOME")
