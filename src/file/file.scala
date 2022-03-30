f04caab1228ea230a6df4c184a202b5c3f733102
Jon Pretty <jon.pretty@propensive.com>
2022-03-30 11:44:20 +0200

    Add `readOnly` parameter to file/dir providers

--- src/file/file.scala
+++ src/file/file.scala
@@ -4,20 +4,20 @@ import java.nio.file as jnf
 import java.io as ji
 
 trait DirectoryProvider[T]:
-  def make(path: String): Option[T]
+  def make(path: String, readOnly: Boolean = false): Option[T]
   def path(value: T): String
 
 trait FileProvider[T]:
-  def make(path: String): Option[T]
+  def make(path: String, readOnly: Boolean = false): Option[T]
   def path(value: T): String
 
 object files:
   given javaNio: FileProvider[jnf.Path] with DirectoryProvider[jnf.Path] with
-    def make(path: String): Option[jnf.Path] =
+    def make(path: String, readOnly: Boolean = false): Option[jnf.Path] =
       try Some(jnf.Paths.get(path).nn)
       catch case err: jnf.InvalidPathException => None
     def path(value: jnf.Path): String = value.toAbsolutePath.nn.toString
 
   given javaIo: FileProvider[ji.File] with DirectoryProvider[ji.File] with
-    def make(path: String): Option[ji.File] = Some(ji.File(path))
+    def make(path: String, readOnly: Boolean = false): Option[ji.File] = Some(ji.File(path))
     def path(value: ji.File): String = value.getAbsolutePath.nn
