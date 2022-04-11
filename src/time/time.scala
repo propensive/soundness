573f66ecf60f30f0a01bc22804dde3d5851570ce
Jon Pretty <jon.pretty@propensive.com>
2022-04-11 17:51:03 +0200

    Renamed `Timekeeping` to `Timekeeper`

--- src/time/time.scala
+++ src/time/time.scala
@@ -18,21 +18,21 @@ package clairvoyant
 
 import annotation.implicitNotFound
 
-@implicitNotFound("an implicit Timekeeping instance is required to determine what type should be used to represent instants in time")
-trait Timekeeping:
+@implicitNotFound("an implicit Timekeeper instance is required to determine what type should be used to represent instants in time")
+trait Timekeeper:
   type Type
   def from(long: Long): Type
   def to(value: Type): Long
 
 object timekeeping:
-  given long: Timekeeping with
+  given long: Timekeeper with
     type Type = Long
     def from(long: Long): Long = long
     def to(value: Long): Long = value
 
-  given date: Timekeeping with
+  given date: Timekeeper with
     type Type = java.util.Date
     def from(long: Long): java.util.Date = java.util.Date(long)
     def to(value: java.util.Date): Long = value.getTime
 
-def now()(using time: Timekeeping): time.Type = time.from(System.currentTimeMillis)
+def now()(using time: Timekeeper): time.Type = time.from(System.currentTimeMillis)
