0302acb3c1fd5f0b13105f06acf9a598002f2ba1
Jon Pretty <jon.pretty@propensive.com>
2022-07-03 10:25:05 +0200

    Don't repeat `Error` messages

--- src/core/path.scala
+++ src/core/path.scala
@@ -21,14 +21,8 @@ import gossamer.*
 
 import annotation.targetName
 
-case class RootParentError(root: Root)
-extends Error((t"attempted to access parent of root ", root)):
-  def message: Text = txt"""an attempt was made to access the parent of a filesystem root, which (by
-                          definition) has no parent"""
-
-case class InvalidPathError(path: Text) extends Error((t"the path ", path, " was not absolute")):
-  def message: Text =
-    txt"the path $path was not absolute, since it did not begin with the / character"
+case class RootParentError(root: Root) extends Error((t"attempted to access parent of root ", root))
+case class InvalidPathError(path: Text) extends Error((t"the path ", path, " was not absolute"))
 
 object ^ extends Base.Path.Absolute(Nil)
 object ? extends Relative(0, Nil)
