a6b84a88d7f3c42134d06c4f21709ecf8bfb7313
Jon Pretty <jon.pretty@propensive.com>
2022-07-07 19:30:36 +0200

    Split up path/file typeclasses

--- src/file/file.scala
+++ src/file/file.scala
@@ -19,21 +19,35 @@ package anticipation
 import java.nio.file as jnf
 import java.io as ji
 
-trait DirectoryProvider[T]:
+trait DirectoryProvider[+T]:
   def make(path: String, readOnly: Boolean = false): Option[T]
+
+trait DirectoryInterpreter[-T]:
   def path(value: T): String
 
-trait FileProvider[T]:
+trait FileProvider[+T]:
   def make(path: String, readOnly: Boolean = false): Option[T]
+
+trait FileInterpreter[-T]:
   def path(value: T): String
 
-object files:
-  given javaNio: FileProvider[jnf.Path] with DirectoryProvider[jnf.Path] with
+trait PathProvider[+T]:
+  def make(path: String, readOnly: Boolean = false): Option[T]
+
+trait PathInterpreter[-T]:
+  def path(value: T): String
+
+package integration:
+  given javaNio: FileProvider[jnf.Path] with DirectoryProvider[jnf.Path]
+      with DirectoryInterpreter[jnf.Path] with FileInterpreter[jnf.Path]
+      with PathProvider[jnf.Path] with PathInterpreter[jnf.Path] with
     def make(path: String, readOnly: Boolean = false): Option[jnf.Path] =
       try Some(jnf.Paths.get(path).nn)
       catch case err: jnf.InvalidPathException => None
     def path(value: jnf.Path): String = value.toAbsolutePath.nn.toString
 
-  given javaIo: FileProvider[ji.File] with DirectoryProvider[ji.File] with
+  given javaIo: FileProvider[ji.File] with DirectoryProvider[ji.File]
+      with DirectoryInterpreter[ji.File] with FileInterpreter[ji.File]
+      with PathProvider[ji.File] with PathInterpreter[ji.File] with
     def make(path: String, readOnly: Boolean = false): Option[ji.File] = Some(ji.File(path))
     def path(value: ji.File): String = value.getAbsolutePath.nn
