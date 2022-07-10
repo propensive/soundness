6cae225fd9683ca276a78884a75d0042dc9f4c19
Jon Pretty <jon.pretty@propensive.com>
2022-07-10 13:04:21 +0200

    Rename `HtmlAttribute` to avoid conflict with Honeycomb

--- src/html/html.scala
+++ src/html/html.scala
@@ -16,7 +16,7 @@
 
 package anticipation
 
-trait HtmlAttribute[-L <: String & Singleton, -T]:
+trait HtmlAttributeWriter[-L <: String & Singleton, -T]:
   def name: String
   def serialize(value: T): String
 
