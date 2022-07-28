274220f9c8e436c6361b4fa702e80fc8ac8410e3
Jon Pretty <jon.pretty@propensive.com>
2022-07-28 19:51:57 +0200

    Updated for Parasitism

--- src/core/watch.scala
+++ src/core/watch.scala
@@ -20,6 +20,7 @@ import eucalyptus.*
 import turbulence.*
 import gossamer.*
 import rudiments.*
+import parasitism.*
 import serpentine.*
 import anticipation.*
 
@@ -31,15 +32,15 @@ import jnf.StandardWatchEventKinds.*
 case class InotifyError()(using Codepoint)
 extends Error(err"the limit on the number of paths that can be watched has been exceeded")(pos)
 
-extension [Dir](dirs: Seq[Dir])(using DirectoryProvider[Dir], DirectoryInterpreter[Dir])
+extension [Dir](dirs: Seq[Dir])(using DirectoryProvider[Dir], DirectoryInterpreter[Dir], Monitor)
   def watch()(using Log, WatchService[Dir]): Watcher[Dir] throws InotifyError = Watcher[Dir](dirs*)
 
-extension [Dir](dir: Dir)(using DirectoryProvider[Dir], DirectoryInterpreter[Dir])
+extension [Dir](dir: Dir)(using DirectoryProvider[Dir], DirectoryInterpreter[Dir], Monitor)
   def watch()(using Log, WatchService[Dir]): Watcher[Dir] throws InotifyError = Watcher[Dir](dir)
 
 object Watcher:
   def apply[Dir: WatchService]
-           (dirs: Dir*)(using DirectoryInterpreter[Dir], DirectoryProvider[Dir], Log)
+           (dirs: Dir*)(using DirectoryInterpreter[Dir], DirectoryProvider[Dir], Log, Monitor)
            : Watcher[Dir] =
     val svc: jnf.WatchService = summon[WatchService[Dir]]()
     val watcher = Watcher[Dir](svc)
@@ -48,7 +49,7 @@ object Watcher:
     watcher
 
 case class Watcher[Dir](private val svc: jnf.WatchService)
-                  (using fromDir: DirectoryInterpreter[Dir], mkdir: DirectoryProvider[Dir]):
+                  (using fromDir: DirectoryInterpreter[Dir], mkdir: DirectoryProvider[Dir], monitor: Monitor):
   
   private val watches: HashMap[jnf.WatchKey, jnf.Path] = HashMap()
   private val dirs: HashMap[jnf.Path, jnf.WatchKey] = HashMap()
@@ -57,8 +58,7 @@ case class Watcher[Dir](private val svc: jnf.WatchService)
   private def toDir(path: jnf.Path): Dir = mkdir.make(Showable(path).show.s).get
 
   private val funnel = Funnel[Maybe[WatchEvent]]
-  private val pumpTask = Task(pump())
-  pumpTask()
+  private val pumpTask = Task(t"watcher")(pump())
   
   def stream: LazyList[WatchEvent] = funnel.stream.takeWhile(_ != Unset).sift[WatchEvent]
   def removeAll()(using Log): Unit = watches.values.map(toDir(_)).foreach(remove(_))
