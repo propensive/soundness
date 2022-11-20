c9f2a67f50acd6e003f5208228afc54c4cae4b66
Jon Pretty <jon.pretty@propensive.com>
2022-11-20 12:58:07 +0100

    Handle exceptions thrown for Turbulence `stream` methods

--- src/core/watch.scala
+++ src/core/watch.scala
@@ -61,7 +61,9 @@ case class Watcher[Dir](private val svc: jnf.WatchService)
   private val funnel = Funnel[Maybe[WatchEvent]]
   private val pumpTask = Task(t"watcher")(pump())
   
-  def stream: LazyList[WatchEvent] = funnel.stream.takeWhile(_ != Unset).sift[WatchEvent]
+  def stream: LazyList[WatchEvent] throws AlreadyStreamingError =
+    funnel.stream.takeWhile(_ != Unset).sift[WatchEvent]
+  
   def removeAll()(using Log): Unit = watches.values.map(toDir(_)).foreach(remove(_))
 
   @tailrec
