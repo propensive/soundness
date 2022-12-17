d85d1437f8b96e92e1155c9e775a4db6b4e9e6fc
Jon Pretty <jon.pretty@propensive.com>
2022-12-17 22:49:16 +0100

    Provided missing methods for Anticipation integration

--- src/integration/anticipation.scala
+++ src/integration/anticipation.scala
@@ -6,8 +6,8 @@ import _root_.joviality as jov
 
 import language.experimental.captureChecking
 
-given jovialityPath[Fs <: jov.Filesystem](using fs: Fs): (PathProvider[jov.DiskPath[Fs]] & DirectoryProvider[jov.Directory[Fs]] & FileProvider[jov.File[Fs]]) =
-  new PathProvider[jov.DiskPath[Fs]] with DirectoryProvider[jov.Directory[Fs]] with FileProvider[jov.File[Fs]]:
+given jovialityPath[Fs <: jov.Filesystem](using fs: Fs): (PathProvider[jov.DiskPath[Fs]] & DirectoryProvider[jov.Directory[Fs]] & FileProvider[jov.File[Fs]] & PathInterpreter[jov.DiskPath[Fs]] & FileInterpreter[jov.File[Fs]] & DirectoryInterpreter[jov.Directory[Fs]]) =
+  new PathProvider[jov.DiskPath[Fs]] with DirectoryProvider[jov.Directory[Fs]] with FileProvider[jov.File[Fs]] with PathInterpreter[jov.DiskPath[Fs]] with FileInterpreter[jov.File[Fs]] with DirectoryInterpreter[jov.Directory[Fs]]:
     def makePath(str: String, readOnly: Boolean = false): Option[jov.DiskPath[Fs]] =
       safely(fs.parse(Text(str))).option
     
@@ -16,4 +16,7 @@ given jovialityPath[Fs <: jov.Filesystem](using fs: Fs): (PathProvider[jov.DiskP
     
     def makeFile(str: String, readOnly: Boolean = false): Option[jov.File[Fs]] =
       safely(fs.parse(Text(str)).file(jov.Expect)).option
-    
+
+    def getPath(value: jov.DiskPath[Fs]): String = value.fullname.s
+    def directoryPath(value: jov.Directory[Fs]): String = value.path.fullname.s
+    def filePath(value: jov.File[Fs]): String = value.path.fullname.s
