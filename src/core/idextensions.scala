fa8be53572db255d8406e81e6f11ea309b4f1d12
Jon Pretty <jon.pretty@propensive.com>
2023-01-01 11:11:26 +0100

    Updated source headers for 2023

--- src/core/idextensions.scala
+++ src/core/idextensions.scala
@@ -1,5 +1,5 @@
 /*
-    Acyclicity, version 0.4.0. Copyright 2021-22 Jon Pretty, Propensive OÜ.
+    Acyclicity, version 0.4.0. Copyright 2021-23 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
