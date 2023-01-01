8bdfeb9f233925bd41bbb4bf88ae988677fd69cf
Jon Pretty <jon.pretty@propensive.com>
2023-01-01 11:12:34 +0100

    Updated source headers for 2023

--- src/uri/uris.scala
+++ src/uri/uris.scala
@@ -1,5 +1,5 @@
 /*
-    Telekinesis, version 0.4.0. Copyright 2022-22 Jon Pretty, Propensive OÜ.
+    Telekinesis, version 0.4.0. Copyright 2022-23 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
