2ccdcda960fd06765737431554b6ad5bfc237c32
Jon Pretty <jon.pretty@propensive.com>
2023-01-01 11:12:17 +0100

    Updated source headers for 2023

--- src/property/property.scala
+++ src/property/property.scala
@@ -1,5 +1,5 @@
 /*
-    Probably, version 0.4.0. Copyright 2017-22 Jon Pretty, Propensive OÜ.
+    Probably, version 0.4.0. Copyright 2017-23 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
