107e8af96d3cef495e406d8d375ed95ea09b6336
Jon Pretty <jon.pretty@propensive.com>
2023-01-01 11:12:06 +0100

    Updated source headers for 2023

--- src/core/joviality.scala
+++ src/core/joviality.scala
@@ -1,5 +1,5 @@
 /*
-    Joviality, version 0.4.0. Copyright 2020-22 Jon Pretty, Propensive OÜ.
+    Joviality, version 0.4.0. Copyright 2020-23 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
