72b3104cb63d89d439c8d3866a32a24946fa8126
Jon Pretty <jon.pretty@propensive.com>
2023-01-02 20:48:49 +0100

    Implement simple URL interpolator

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -18,6 +18,7 @@ package telekinesis
 
 import rudiments.*
 import probably.*
+import parasitism.*, threading.virtual
 import eucalyptus.*
 import gossamer.*, stdouts.stdout
 import turbulence.*
@@ -27,7 +28,7 @@ import scala.collection.immutable.ListMap
 
 import unsafeExceptions.canThrowAny
 
-given Log(Everything |-> SystemOut)
+import logging.silent
 
 case class Address(house: Int, street: Text, city: Text, country: Text)
 case class Person(name: Text, address: Address)
@@ -37,42 +38,51 @@ object Tests extends Suite(t"Telekinesis tests"):
     suite(t"Parsing tests"):
       test(t"parse Authority with username and password"):
         Authority.parse(t"username:password@example.com")
-      .check(_ == Authority(Host(t"example", t"com"), Some(t"username:password"), None))
+      .assert(_ == Authority(Host(t"example", t"com"), t"username:password"))
       
       test(t"parse Authority with username but not password"):
         Authority.parse(t"username@example.com")
-      .check(_ == Authority(Host(t"example", t"com"), Some(t"username"), None))
+      .assert(_ == Authority(Host(t"example", t"com"), t"username"))
       
       test(t"parse Authority with username, password and port"):
         Authority.parse(t"username:password@example.com:8080")
-      .check(_ == Authority(Host(t"example", t"com"), Some(t"username:password"), Some(8080)))
+      .assert(_ == Authority(Host(t"example", t"com"), t"username:password", 8080))
       
       test(t"parse Authority with username and port"):
         Authority.parse(t"username@example.com:8080")
-      .check(_ == Authority(Host(t"example", t"com"), Some(t"username"), Some(8080)))
+      .assert(_ == Authority(Host(t"example", t"com"), t"username", 8080))
       
       test(t"parse Authority with username, numerical password and port"):
         Authority.parse(t"username:1234@example.com:8080")
-      .check(_ == Authority(Host(t"example", t"com"), Some(t"username:1234"), Some(8080)))
+      .assert(_ == Authority(Host(t"example", t"com"), t"username:1234", 8080))
 
       test(t"Authority with invalid port fails"):
-        capture:
-          Authority.parse(t"username@example.com:no")
-      .check:
+        capture(Authority.parse(t"username@example.com:no"))
+      .assert:
         case InvalidUrlError(_, 21, InvalidUrlError.Expectation.Number) => true
-        case _ => false
+        case _                                                          => false
       
       test(t"Parse full URI"):
         Url.parse(t"http://user:pw@example.com:8080/path/to/location?query=1#ref")
-      .check(_ == Url(Scheme(t"http"), Some(Authority(Host(t"example", t"com"), Some(t"user:pw"),
-          Some(8080))), t"/path/to/location", Some(t"query=1"), Some(t"ref")))
+      .assert(_ == Url(Scheme.Http, Authority(Host(t"example", t"com"), t"user:pw", 8080),
+          t"/path/to/location", t"query=1", t"ref"))
       
       test(t"Parse simple URI"):
         Url.parse(t"https://example.com/foo")
-      .check(_ == Url(Scheme(t"https"), Some(Authority(Host(t"example", t"com"))), t"/foo", None,
-          None))
+      .assert(_ == Url(Scheme.Https, Authority(Host(t"example", t"com")), t"/foo"))
       
       test(t"Show simple URI"):
         Url.parse(t"http://example.com/foo").show
-      .check(_ == t"http://example.com/foo")
+      .assert(_ == t"http://example.com/foo")
+      
+      test(t"Parse full URL at compiletime"):
+        url"http://user:pw@example.com:8080/path/to/location?query=1#ref"
+      .assert(_ == Url(Scheme.Http, Authority(Host(t"example", t"com"), t"user:pw", 8080),
+          t"/path/to/location", t"query=1", t"ref"))
+      
+      test(t"Parse FTP URL at compiletime"):
+        url"ftp://user:pw@example.com:8080/path/to/location"
+      .assert(_ == Url(Scheme(t"ftp"), Authority(Host(t"example", t"com"), t"user:pw", 8080),
+          t"/path/to/location"))
+      
       
--- src/url/url.scala
+++ src/url/url.scala
@@ -20,6 +20,7 @@ import serpentine.*
 import gossamer.*
 import rudiments.*
 import anticipation.*
+import contextual.*
 
 object Host:
   given Show[Host] = _.parts.join(t".")
@@ -38,13 +39,23 @@ object Scheme:
 case class Scheme(name: Text)
 
 object Url:
+  object Interpolator extends contextual.Interpolator[Text, Text, Url]:
+    def complete(value: Text): Url =
+      try Url.parse(value) catch case err: InvalidUrlError =>
+        throw InterpolationError(t"the URL ${err.text} is not valid: expected ${err.expected}", err.offset)
+    
+    def initial: Text = t""
+    def insert(state: Text, value: Text): Text = state+value
+    def parse(state: Text, next: Text): Text = state+next
+    def skip(state: Text): Text = state+t"x"
+
   given (using CanThrow[InvalidUrlError]): GenericUrl[Url] = new GenericUrl[Url]:
     def readUrl(url: Url): String = url.show.s
     def makeUrl(value: String): Url = Url.parse(value.show)
 
   given show: Show[Url] = url =>
-    val auth = url.authority.fold(t"")(t"//"+_.show)
-    val rest = t"${url.query.fold(t"")(t"?"+_)}${url.fragment.fold(t"")(t"#"+_)}"
+    val auth = url.authority.fm(t"")(t"//"+_.show)
+    val rest = t"${url.query.fm(t"")(t"?"+_)}${url.fragment.fm(t"")(t"#"+_)}"
     t"${url.scheme}:$auth${url.path}$rest"
 
   def parse(value: Text): Url throws InvalidUrlError =
@@ -60,30 +71,24 @@ object Url:
         val (pathStart, auth) =
           if value.slice(colon + 1, colon + 3) == t"//" then
             val authEnd = safely(value.where(_ == '/', colon + 3)).or(value.length)
-            authEnd -> Some(Authority.parse(value.slice(colon + 3, authEnd)))
+            authEnd -> Authority.parse(value.slice(colon + 3, authEnd))
           else
-            (colon + 1) -> None
+            (colon + 1) -> Unset
         
         safely(value.where(_ == '?', pathStart)) match
           case Unset => safely(value.where(_ == '#', pathStart)) match
-            case Unset =>
-              Url(scheme, auth, value.drop(pathStart), None, None)
-            
-            case hash: Int =>
-              Url(scheme, auth, value.slice(pathStart, hash), None, Some(value.drop(hash + 1)))
+            case Unset     => Url(scheme, auth, value.drop(pathStart), Unset, Unset)
+            case hash: Int => Url(scheme, auth, value.slice(pathStart, hash), Unset, value.drop(hash + 1))
 
           case qmark: Int =>
             safely(value.where(_ == '#', qmark + 1)) match
-              case Unset =>
-                Url(scheme, auth, value.slice(pathStart, qmark), Some(value.drop(qmark + 1)), None)
-              
-              case hash: Int =>
-                Url(scheme, auth, value.slice(pathStart, qmark), Some(value.slice(qmark + 1, hash)),
-                    Some(value.drop(hash + 1)))
+              case Unset     => Url(scheme, auth, value.slice(pathStart, qmark), value.drop(qmark + 1), Unset)
+              case hash: Int => Url(scheme, auth, value.slice(pathStart, qmark), value.slice(qmark + 1, hash),
+                                    value.drop(hash + 1))
 
 object Authority:
   given Show[Authority] = auth =>
-    t"${auth.userInfo.fold(t"")(_+t"@")}${auth.host}${auth.port.map(_.show).fold(t"")(t":"+_)}"
+    t"${auth.userInfo.fm(t"")(_+t"@")}${auth.host}${auth.port.mm(_.show).fm(t"")(t":"+_)}"
 
   def parse(value: Text): Authority throws InvalidUrlError =
     import InvalidUrlError.Expectation.*
@@ -94,41 +99,27 @@ object Authority:
           Authority(Host.parse(value))
         
         case colon: Int =>
-          val port = safely(value.drop(colon + 1).s.toInt) match
-            case port: Int if port >= 0 && port <= 65535 =>
-              port
-            
-            case port: Int =>
-              throw InvalidUrlError(value, colon + 1, PortRange)
-            
-            case Unset =>
-              throw InvalidUrlError(value, colon + 1, Number)
-          
-          Authority(Host.parse(value.take(colon)), None, Some(port))
+          safely(value.drop(colon + 1).s.toInt).match
+            case port: Int if port >= 0 && port <= 65535 => port
+            case port: Int                               => throw InvalidUrlError(value, colon + 1, PortRange)
+            case Unset                                   => throw InvalidUrlError(value, colon + 1, Number)
+          .pipe(Authority(Host.parse(value.take(colon)), Unset, _))
       
-      case arobase: Int =>
-        safely(value.where(_ == ':', arobase + 1)) match
-          case Unset =>
-            Authority(Host.parse(value.drop(arobase + 1)), Some(value.take(arobase)))
+      case arobase: Int => safely(value.where(_ == ':', arobase + 1)) match
+        case Unset =>
+          Authority(Host.parse(value.drop(arobase + 1)), value.take(arobase))
 
-          case colon: Int =>
-            val port = safely(value.drop(colon + 1).s.toInt) match
-              case port: Int if port >= 0 && port <= 65535 =>
-                port
-              
-              case port: Int =>
-                throw InvalidUrlError(value, colon + 1, PortRange)
-              
-              case Unset =>
-                throw InvalidUrlError(value, colon + 1, Number)
-            
-            Authority(Host.parse(value.slice(arobase + 1, colon)), Some(value.take(arobase)),
-                Some(port))
+        case colon: Int =>
+          safely(value.drop(colon + 1).s.toInt).match
+            case port: Int if port >= 0 && port <= 65535 => port
+            case port: Int                               => throw InvalidUrlError(value, colon + 1, PortRange)
+            case Unset                                   => throw InvalidUrlError(value, colon + 1, Number)
+          .pipe(Authority(Host.parse(value.slice(arobase + 1, colon)), value.take(arobase), _))
 
-case class Authority(host: Host, userInfo: Option[Text] = None, port: Option[Int] = None)
+case class Authority(host: Host, userInfo: Maybe[Text] = Unset, port: Maybe[Int] = Unset)
 
-case class Url(scheme: Scheme, authority: Option[Authority], path: Text, query: Option[Text],
-                   fragment: Option[Text])
+case class Url(scheme: Scheme, authority: Maybe[Authority], path: Text, query: Maybe[Text] = Unset,
+                   fragment: Maybe[Text] = Unset)
 
 object InvalidUrlError:
   enum Expectation:
