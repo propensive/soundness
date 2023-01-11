9e385b8c156d6a7a58becaa4fb112d3c4edbc903
Jon Pretty <jon.pretty@propensive.com>
2023-01-11 20:02:18 +0100

    Moved `Diff.diff` to `diff`, but needs testing

--- src/core/diff.scala
+++ src/core/diff.scala
@@ -16,7 +16,6 @@
 
 package dissonance
 
-import eucalyptus.*
 import gossamer.*
 import rudiments.*
 import annotation.*
@@ -49,8 +48,6 @@ case class Diff[T](changes: Change[T]*):
     recur(changes, seq)
 
 object Diff:
-  import Point.*
-
   object Point:
     opaque type Point = Long
     def apply(x: Int, y: Int): Point = (x.toLong << 32) + y
@@ -63,60 +60,62 @@ object Diff:
       def keep: Point = Point(x + 1, y + 1)
       def unkeep: Point = Point(x - 1, y - 1)
 
-  def diff[T](left: IArray[T], right: IArray[T], cmp: (T, T) -> Boolean = { (a: T, b: T) => a == b }): Diff[T] =
-    val end = Point(left.size, right.size)
-    @tailrec
-    def distance(last: IArray[Point] = IArray(count(Point(0, 0))), trace: List[IArray[Point]] = Nil): Diff[T] =
-      if last.contains(end) then
-        val idx = last.indexOf(end)
+import Diff.Point.*
 
-        if trace.isEmpty then countback(idx, end, Nil)
-        else if trace.head.length > idx && count(trace.head(idx).ins) == end then countback(idx, end, trace)
-        else countback(idx - 1, end, trace)
+def diff[T](left: IArray[T], right: IArray[T], cmp: (T, T) -> Boolean = { (a: T, b: T) => a == b }): Diff[T] =
+  val end = Point(left.size, right.size)
+  @tailrec
+  def distance(last: IArray[Point] = IArray(count(Point(0, 0))), trace: List[IArray[Point]] = Nil): Diff[T] =
+    if last.contains(end) then
+      val idx = last.indexOf(end)
 
-      else
-        val round = last.size
-        val next = IArray.create[Point](round + 1): arr =>
-          arr(0) = last(0).ins
+      if trace.isEmpty then countback(idx, end, Nil)
+      else if trace.head.length > idx && count(trace.head(idx).ins) == end then countback(idx, end, trace)
+      else countback(idx - 1, end, trace)
 
-          last.indices.foreach: i =>
-            arr(i + 1) = count(last(i).del)
-            count(last(i).ins).pipe { pt => if i == round || pt.x > arr(i).x then arr(i) = pt }
+    else
+      val round = last.size
+      val next = IArray.create[Point](round + 1): arr =>
+        arr(0) = last(0).ins
 
-        distance(next, last :: trace)
+        last.indices.foreach: i =>
+          arr(i + 1) = count(last(i).del)
+          count(last(i).ins).pipe { pt => if i == round || pt.x > arr(i).x then arr(i) = pt }
 
-    @tailrec
-    def count(pt: Point): Point =
-      if pt.x >= left.size || pt.y >= right.size || !cmp(left(pt.x), right(pt.y)) then pt else count(pt.keep)
+      distance(next, last :: trace)
 
-    def countback(idx: Int, cur: Point, trace: List[IArray[Point]], result: List[Change[T]] = Nil): Diff[T] =
-      trace match
-        case head :: tail =>
-          val target = head(idx)
-          
-          if cur == Point(0, 0) then Diff(sort(result, Nil, Nil)*)
-          else if cur.x > target.x && cur.y > target.y then
-            countback(idx, cur.unkeep, trace, Keep(cur.x - 1, cur.y - 1, left(cur.x - 1)) :: result)
-          else if cur == target.ins then
-            countback(0 max (idx - 1), target, tail, Ins(target.y, right(target.y)) :: result)
-          else if cur == target.del then
-            countback(0 max (idx - 1), target, tail, Del(cur.x - 1, left(cur.x - 1)) :: result)
-          else throw Mistake("Unexpected")
-  
-        case Nil =>
-          if cur == Point(0, 0) then Diff(sort(result, Nil, Nil)*)
-          else countback(0, cur.unkeep, Nil, Keep(cur.x - 1, cur.y - 1, left(cur.x - 1)) :: result)
+  @tailrec
+  def count(pt: Point): Point =
+    if pt.x >= left.size || pt.y >= right.size || !cmp(left(pt.x), right(pt.y)) then pt else count(pt.keep)
 
-    @tailrec
-    def sort(values: List[Change[T]], cache: List[Change[T]], result: List[Change[T]]): List[Change[T]] =
-      values match
-        case Keep(l, r, v) :: tail => cache match
-          case Nil                    => sort(tail, Nil, Keep(l, r, v) :: result)
-          case cache                  => sort(tail, Nil, Keep(l, r, v) :: (cache ::: result))
-        case Ins(r, v) :: tail     => sort(tail, Ins(r, v) :: cache, result)
-        case Del(l, v) :: tail     => sort(tail, cache, Del(l, v) :: result)
-        case Nil                   => (cache ::: result).reverse
+  def countback(idx: Int, cur: Point, trace: List[IArray[Point]], result: List[Change[T]] = Nil): Diff[T] =
+    trace match
+      case head :: tail =>
+        val target = head(idx)
+        
+        if cur == Point(0, 0) then Diff(sort(result, Nil, Nil)*)
+        else if cur.x > target.x && cur.y > target.y then
+          countback(idx, cur.unkeep, trace, Keep(cur.x - 1, cur.y - 1, left(cur.x - 1)) :: result)
+        else if cur == target.ins then
+          countback(0 max (idx - 1), target, tail, Ins(target.y, right(target.y)) :: result)
+        else if cur == target.del then
+          countback(0 max (idx - 1), target, tail, Del(cur.x - 1, left(cur.x - 1)) :: result)
+        else throw Mistake("Unexpected")
 
-    distance()
+      case Nil =>
+        if cur == Point(0, 0) then Diff(sort(result, Nil, Nil)*)
+        else countback(0, cur.unkeep, Nil, Keep(cur.x - 1, cur.y - 1, left(cur.x - 1)) :: result)
+
+  @tailrec
+  def sort(values: List[Change[T]], cache: List[Change[T]], result: List[Change[T]]): List[Change[T]] =
+    values match
+      case Keep(l, r, v) :: tail => cache match
+        case Nil                    => sort(tail, Nil, Keep(l, r, v) :: result)
+        case cache                  => sort(tail, Nil, Keep(l, r, v) :: (cache ::: result))
+      case Ins(r, v) :: tail     => sort(tail, Ins(r, v) :: cache, result)
+      case Del(l, v) :: tail     => sort(tail, cache, Del(l, v) :: result)
+      case Nil                   => (cache ::: result).reverse
+
+  distance()
 
 given Realm = Realm(t"dissonance")
