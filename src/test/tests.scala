d47c0fc51457a5511397853270396bf3c1d9e911
Jon Pretty <jon.pretty@propensive.com>
2023-01-19 15:26:01 +0100

    Disabled running macro in tests

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -5,5 +5,5 @@ import rudiments.*
 
 object Tests:
   val three = 3
-  Macros.inspect:
-    three match { case x: (Int | String) => x + 1 }
+  // Macros.inspect:
+  //   three match { case x: (Int | String) => x + 1 }
