b52fd90f7a8e590c4d9ff1c37cbd5a7166fcc715
Jon Pretty <jon.pretty@propensive.com>
2023-01-22 13:29:31 +0100

    Updated tests for new Probably API

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -33,7 +33,7 @@ case class Address(house: Int, street: Text, city: Text, country: Text)
 case class Person(name: Text, address: Address)
 
 object Tests extends Suite(t"Scintillate tests"):
-  def run(using Runner): Unit =
+  def run(): Unit =
 
     // val IntParam = RequestParam[Int](t"one")
 
