bde73cd2586307729a9401798f193834fc0e0812
Jon Pretty <jon.pretty@propensive.com>
2023-01-26 15:17:57 +0100

    Use Deviation

--- src/core/tarantula.scala
+++ src/core/tarantula.scala
@@ -24,6 +24,7 @@ import cataclysm.*
 import honeycomb.*
 import eucalyptus.*
 import rudiments.*
+import deviation.*
 import gesticulate.*
 import parasitism.*
 import anticipation.*, timeRepresentation.long
