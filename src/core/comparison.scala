b53dea9c2eb06517448e612999d20c22ea18c836
Jon Pretty <jon.pretty@propensive.com>
2023-01-30 22:41:19 +0100

    Work with recursive diffs on sequences

--- src/core/comparison.scala
+++ src/core/comparison.scala
@@ -43,7 +43,12 @@ object Comparison:
     
     case Different(left, right) => ansi"The value ${colors.YellowGreen}($left) did not equal ${colors.Crimson}($right)"
     case Same(value)            => ansi"The value ${colors.Gray}($value) was expected"
-    
+
+object Similar:
+  given [T]: Similar[T] = (a, b) => a == b
+
+trait Similar[-T]:
+  def similar(a: T, b: T): Boolean
 
 trait Comparable[-T]:
   def compare(a: T, b: T): Comparison
@@ -58,42 +63,37 @@ object Comparable extends Derivation[Comparable]:
     if left.canon == right.canon
     then Comparison.Same(left.canon) else Comparison.Different(left.canon, right.canon)
   
-  // given [T: Comparable]: Comparable[Maybe[T]] = (left, right) => left match
-  //   case Unset    => right match
-  //     case Unset    => Comparison.Same(t"——")
-  //     case right: T => Comparison.Different(t"——", right.toString.show)
-  //   case left: T => right match
-  //     case Unset    => Comparison.Different(left.toString.show, t"——")
-  //     case right: T => summon[Comparable[T]].compare(left, right)
-  
   given Comparable[Text] = (left, right) =>
     if left == right then Comparison.Same(left.debug) else Comparison.Different(left.debug, right.debug)
   
   given Comparable[Int] = (left, right) =>
     if left == right then Comparison.Same(left.debug) else Comparison.Different(left.debug, right.debug)
-  
+
   given Comparable[Exception] = (left, right) =>
     val leftMsg = Option(left.getMessage).fold(t"null")(_.nn.show)
     val rightMsg = Option(right.getMessage).fold(t"null")(_.nn.show)
     if left.getClass == right.getClass && leftMsg == rightMsg then Comparison.Same(leftMsg)
     else Comparison.Different(leftMsg, rightMsg)
   
-  given seq[T: Debug: ClassTag, Coll[X] <: Seq[X]]: Comparable[Coll[T]] = (left, right) =>
+  given seq[T: Debug: Comparable: Similar, Coll[X] <: Seq[X]]: Comparable[Coll[T]] = (left, right) =>
     Comparison.Structural:
       IArray.from:
-        diff(IArray.from[T](left), IArray.from[T](right)).changes.map:
-          case Change.Keep(lid, rid, v) => (if lid == rid then lid.show else t"$lid/$rid") -> Comparison.Same(v.debug)
-          case Change.Ins(rid, v)       => t"/$rid" -> Comparison.Different(t"—", v.debug)
-          case Change.Del(lid, v)       => t"$lid/" -> Comparison.Different(v.debug, t"—")
-      
-  given iarray[T: Debug]: Comparable[IArray[T]] = (left, right) =>
-    Comparison.Structural:
-      IArray.from:
-        diff(left, right).changes.map:
-          case Change.Keep(lid, rid, v) => (if lid == rid then lid.show else t"$lid/$rid") -> Comparison.Same(v.debug)
-          case Change.Ins(rid, v)       => t"/$rid" -> Comparison.Different(t"—", v.debug)
-          case Change.Del(lid, v)       => t"$lid/" -> Comparison.Different(v.debug, t"—")
-
+        diff(left.to(IndexedSeq), right.to(IndexedSeq)).rdiff(summon[Similar[T]].similar).changes.map:
+          case Change.Keep(lid, rid, v) =>
+            (if lid == rid then lid.show else t"$lid/$rid") -> Comparison.Same(v.debug)
+          
+          case Change.Ins(rid, v) =>
+            t"/$rid" -> Comparison.Different(t"—", v.debug)
+          
+          case Change.Del(lid, v) =>
+            t"$lid/" -> Comparison.Different(v.debug, t"—")
+          
+          case Change.Replace(lid, rid, lv, rv) =>
+            t"$lid/$rid" -> summon[Comparable[T]].compare(lv, rv)
+  
+  given iarray[T: Debug: Comparable: Similar]: Comparable[IArray[T]] = (left, right) =>
+    seq[T, IndexedSeq].compare(left.to(IndexedSeq), right.to(IndexedSeq))
+  
   def join[T](caseClass: CaseClass[Comparable, T]): Comparable[T] = (left, right) =>
     val same = caseClass.params.forall: param =>
       param.deref(left) == param.deref(right)
@@ -106,7 +106,6 @@ object Comparable extends Derivation[Comparable]:
   def split[T](sealedTrait: SealedTrait[Comparable, T]): Comparable[T] = (left, right) =>
     sealedTrait.choose(left): subtype =>
       sealedTrait.choose(right): subtype2 =>
-        if subtype.typeInfo == subtype2.typeInfo
+        if subtype.subtype.index == subtype2.subtype.index
         then subtype.typeclass.compare(subtype.cast(left), subtype.cast(right))
-        else Comparison.Different(subtype.typeInfo.short.show, subtype2.typeInfo.short.show)
-    
+        else Comparison.Different(left.toString.show, right.toString.show)
