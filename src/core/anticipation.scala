3ea241a3a5eead01ce2132ae8c0df6ec5a12022d
Jon Pretty <jon.pretty@propensive.com>
2023-02-24 11:33:36 +0100

    Rename package to use new `xApi` style

--- src/core/anticipation.scala
+++ src/core/anticipation.scala
@@ -2,14 +2,14 @@ package anticipation
 
 import diuretic.*
 
-package timeRepresentation:
+package timeApi:
   given javaTime: JavaTime.type = JavaTime
   given long: JavaLongTime.type = JavaLongTime
   given javaUtil: JavaUtilTime.type = JavaUtilTime
 
-package fileRepresentation:
+package fileApi:
   given javaNio: JavaNioFile.type = JavaNioFile
   given javaIo: JavaIoFile.type = JavaIoFile
 
-package urlRepresentation:
+package urlApi:
   given javaNet: JavaNetUrl.type = JavaNetUrl
