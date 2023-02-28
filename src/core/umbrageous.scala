266293e1dc2724faad00ac9abfc90aa74fa058b6
Jon Pretty <jon.pretty@propensive.com>
2023-02-28 14:08:17 +0100

    Automatically import shaded packages

--- src/core/umbrageous.scala
+++ src/core/umbrageous.scala
@@ -1,6 +1,6 @@
 package umbrageous
 
-import dotty.tools.dotc.*, util.*, ast.*, core.Contexts.*, core.Decorators.*, core.Symbols.*, plugins.*
+import dotty.tools.dotc.*, util.*, ast.*, core.*, Contexts.*, Decorators.*, Symbols.*, Names.*, plugins.*
 
 class UmbrageousPlugin() extends StandardPlugin:
   val name: String = "umbrageous"
@@ -18,28 +18,35 @@ class UmbrageousTransformer(options: List[String]) extends PluginPhase:
     
     val prefixes: List[(String, String)] =
       options.flatMap: opt =>
-        opt.split(":").toList match
+        opt.split(":").nn.to(List).map(_.nn) match
           case List(from, to) => List((from, to))
           case other          => report.warning(s"umbrageous: the option '$opt' is not a valid shading mapping; please specify a mapping of the form, '<package>:<new-prefix>'"); Nil
 
     object transformer extends UntypedTreeMap:
-      private def rewritePackage(tree: Ident | Select, fqn: String = ""): Ident | Select = tree match
-        case Ident(name) =>
-          val pkgName = name.decode.toString+"."+fqn
-          prefixes
-            .filter: (k, v) =>
-              pkgName == k || pkgName.startsWith(k+".")
-            .sortBy(_(0).length)
-            .lastOption
-            .fold(tree): (k, v) =>
-              Select(Ident(v.toTermName), name)
-        
-        case Select(pkg: (Ident | Select), name) =>
-          Select(rewritePackage(pkg, name.decode.toString+"."+fqn), name)
+      private def rewritePackage
+          (tree: Ident | Select, fqn: String, defs: List[Tree], select: Select => Select): PackageDef =
+        tree match
+          case Ident(name) =>
+            val pkg = name.decode.toString+"."+fqn
+            val prefixes2 = prefixes.filter { (k, v) => pkg == k || pkg.startsWith(k+".") }.sortBy(_(0).length)
+              
+            val ident = prefixes2.lastOption.fold(tree): (k, v) =>
+              select(Select(Ident(v.toTermName), name))
+            
+            val imports = prefixes2.lastOption.fold(prefixes) { (k, v) => prefixes.filter(_(0) != k) }.map:
+              case (_, prefix) =>
+                Import(Ident(prefix.toTermName), List(ImportSelector(Ident(StdNames.nme.WILDCARD))))
+            
+            PackageDef(ident, imports ::: defs)
+          
+          case Select(pkg: (Ident | Select), name) =>
+            rewritePackage(pkg, s"${name.decode}.$fqn", defs, Select(_, name))
+          
+          case _ => ???
 
       override def transform(tree: Tree)(using Context): Tree =
         tree match
-          case PackageDef(name: (Ident | Select), defs) => PackageDef(rewritePackage(name), defs)
+          case PackageDef(name: (Ident | Select), defs) => rewritePackage(name, "", defs, identity)
           case _                                        => super.transform(tree)
     
     ctx.compilationUnit.untpdTree = transformer.transform(untpdTree)
---
+++ src/test2/example.scala
@@ -0,0 +1,5 @@
+package another.pkg
+
+import testing.quux.*
+
+def foo(): Unit = Main
