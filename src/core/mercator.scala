388032ea21f999de34fd04d4182a109c050e2a3e
Jon Pretty <jon.pretty@propensive.com>
2023-03-20 17:21:23 +0100

    WIP on map and flatMap methods

--- src/core/mercator.scala
+++ src/core/mercator.scala
@@ -25,6 +25,9 @@ trait Functor[FunctorType[_]]:
       (fn: ValueType => ValueType2)
       : FunctorType[ValueType2]
 
+object Monad:
+  inline given [MonadType[_]]: Monad[MonadType] = ${MercatorMacros.monad[MonadType]}
+
 trait Monad[MonadType[_]] extends Functor[MonadType]:
   def flatMap
       [ValueType, ValueType2]
@@ -70,13 +73,62 @@ object MercatorMacros:
     then report.errorAndAbort(s"mercator: the companion object ${companion.name} has no candidate apply methods")
     else report.errorAndAbort(s"mercator: the companion object ${companion.name} has more than one candidate apply method")
 
-  def functor[FunctorType[_]](using Type[FunctorType], Quotes): Expr[Functor[FunctorType]] =
+  def functor[TypeConstructorType[_]](using Type[TypeConstructorType], Quotes): Expr[Functor[TypeConstructorType]] =
     import quotes.reflect.*
+    val functorType = TypeRepr.of[TypeConstructorType].typeSymbol
     
-    '{
-      new Functor[FunctorType]:
-        def point[ValueType](value: ValueType): FunctorType[ValueType] = ${
-          ???
-        }
+    val mapMethods = functorType.memberMethods.filter: method =>
+      method.tree match
+        case DefDef("map", _, _, _) => true
+        case _                      => false
+    
+    val pointExpr: Expr[Point[TypeConstructorType]] = Expr.summon[Point[TypeConstructorType]].getOrElse:
+      report.errorAndAbort(s"mercator: could not find Point value for ${functorType.name}")
 
-    }
+    if mapMethods.length == 1
+    then
+      '{
+        new Functor[TypeConstructorType]:
+          def point[ValueType](value: ValueType): TypeConstructorType[ValueType] =
+            ${pointExpr}.point(value)
+          
+          def map
+              [ValueType, ValueType2]
+              (value: TypeConstructorType[ValueType])
+              (fn: ValueType => ValueType2)
+              : TypeConstructorType[ValueType2] =
+            ${
+              'value.asTerm
+                  .select(mapMethods(0))
+                  .appliedToType(TypeRepr.of[ValueType2])
+                  .appliedTo('fn.asTerm)
+                  .asExprOf[TypeConstructorType[ValueType2]]
+            }
+      }
+    else report.errorAndAbort(s"mercator: the type ${functorType.name} has no candidate map methods")
+
+    def monad[TypeConstructorType[_]](using Type[TypeConstructorType], Quotes): Expr[Monad[TypeConstructorType]] =
+      import quotes.reflect.*
+      val monadType = TypeRepr.of[TypeConstructorType].typeSymbol
+
+      val flatMapMethods = monadType.memberMethods.filter: method =>
+        method.tree match
+          case DefDef("flatMap", _, _, _) => true
+          case _                          => false
+
+          if flatMapMethods == 1
+          then
+            '{
+              new Monad[TypeConstructorType]:
+                def point[ValueType](value: ValueType): TypeConstructorType[ValueType] = $functor.point(value)
+                def map[ValueType, ValueType2](value: TypeConstructorType[ValueType])(fn: ValueType => ValueType2): TypeConstructorType[ValueType2] = $functor.map(value)(fn)
+                def flatMap[ValueType, ValueType2](value: TypeConstructorType[ValueType])(fn: ValueType => TypeConstructorType[ValueType2]): TypeConstructorType[ValueType2] =
+                  ${
+                    'value.asTerm
+                        .select(flatMapMethods(0))
+                        .appliedToType(TypeRepr.of[ValueType2])
+                        .appliedTo('fn.asTerm)
+                        .asExprOf[TypeConstructorType[ValueType2]]
+                  }
+            }
+          else report.errorAndAbort(s"mercator: the type ${monadType.name} has no candidate flatMap methods")
