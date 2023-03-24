074fe540350d1eb6583d354346b6b3d8003a9cd3
Jon Pretty <jon.pretty@propensive.com>
2023-03-24 20:13:10 +0100

    Reduced the number of warnings

--- src/core/numbermacros.scala
+++ src/core/numbermacros.scala
@@ -16,24 +16,30 @@
 
 package cardinality
 
+import rudiments.*
+
 import scala.quoted.*
 
 object CardinalityMacro:
-  def apply[D1 <: Double: Type, D2 <: Double: Type](digits: Expr[String])(using Quotes): Expr[D1 ~ D2] =
-    import quotes.*, reflect.*
+  def apply
+      [D1 <: Double: Type, D2 <: Double: Type](digits: Expr[String])(using Quotes): Expr[D1 ~ D2] =
+    import quotes.reflect.*
+    
     digits.value match
       case Some(str) =>
-        ((TypeRepr.of[D1], TypeRepr.of[D2]): @unchecked) match
-          case (ConstantType(lb), ConstantType(ub)) => ((lb.value, ub.value): @unchecked) match
-            case (lb: Double, ub: Double) =>
-              val value = str.toDouble
-              
-              if value < lb
-              then report.errorAndAbort(s"the value $str is less than the lower bound for this value, $lb")
-              
-              if value > ub
-              then report.errorAndAbort(s"the value $str is greater than the upper bound for this value, $ub")
-   
-              '{${Expr(value)}.asInstanceOf[D1 ~ D2]}
+        (TypeRepr.of[D1], TypeRepr.of[D2]) match
+          case (ConstantType(DoubleConstant(lb)), ConstantType(DoubleConstant(ub))) =>
+            val value = str.toDouble
+            
+            if value < lb
+            then fail(s"the value $str is less than the lower bound for this value, $lb")
+            
+            if value > ub
+            then fail(s"the value $str is greater than the upper bound for this value, $ub")
+  
+            '{${Expr(value)}.asInstanceOf[D1 ~ D2]}
+          
+          case _ =>
+            fail("upper and lower bounds must be Double singleton literal types")
       case None =>
         '{NumericRange($digits.toDouble)}
