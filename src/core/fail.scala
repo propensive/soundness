b7fa45402e8ead128d4dafab970080a274bc7274
Jon Pretty <jon.pretty@propensive.com>
2023-03-25 18:39:11 +0100

    Allow position to be passed to `fail` method

--- src/core/fail.scala
+++ src/core/fail.scala
@@ -2,9 +2,10 @@ package rudiments
 
 import scala.quoted.*
 
-def fail(msg: String)(using Quotes): Nothing =
+def fail(using q: Quotes)(msg: String, pos: Maybe[q.reflect.Position] = Unset): Nothing =
   import quotes.reflect.*
+  
   // Note that this might not be the most reliable way to get the package name, but it appears to
   // work in all the examples that have been tested.
   val pkg = Thread.currentThread.nn.getStackTrace.nn(2).nn.getClassName.nn.split("\\.").nn(0).nn
-  report.errorAndAbort(pkg+": "+msg)
+  pos.mm(report.errorAndAbort(pkg+": "+msg, _)).or(report.errorAndAbort(pkg+": "+msg))
