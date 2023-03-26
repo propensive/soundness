50ed867098acdd969b52dd64b46c99d653b55349
Jon Pretty <jon.pretty@propensive.com>
2023-03-26 19:51:09 +0200

    Initial commit

---
+++ src/core/currency.scala
@@ -0,0 +1,28 @@
+package plutocrat
+
+import gossamer.*, textWidthCalculation.uniform
+import rudiments.*
+
+case class Currency(isoCode: String, symbol: String, name: String, fractionalName: String, modulo: Int):
+  def apply(value: Double): Money[this.type] =
+    val integral = value.toLong
+    Money(this)(integral, ((value - integral + (0.5/modulo))*modulo).toInt)
+
+object Eur extends Currency("EUR", "€", "Euro", "Cent", 100)
+object Usd extends Currency("USD", "$", "US Dollar", "Cent", 100)
+
+object opaques:
+  opaque type Money[CurrencyType <: Currency & Singleton] = Long
+
+  object Money:
+    def apply(currency: Currency & Singleton)(wholePart: Long, fractional: Int): Money[currency.type] =
+      wholePart*currency.modulo + fractional
+
+    given [CurrencyType <: Currency & Singleton: ValueOf]: Show[Money[CurrencyType]] = money =>
+      val currency = valueOf[CurrencyType]
+      val integral = money/currency.modulo
+      val fractional = money%currency.modulo
+        
+      t"${currency.symbol+integral}.${fractional.toString.show.pad(2, Rtl, '0')}"
+
+export opaques.Money
---
+++ src/test/tests.scala
@@ -0,0 +1,11 @@
+package plutocrat
+
+import probably.*
+import gossamer.*
+
+object Tests extends Suite(t"Plutocrat tests"):
+  def run(): Unit =
+    test(t"Show a monetary value"):
+      val amount = Eur(3.01)
+      t"Received $amount"
+    .assert(_ == t"Received €3.01")
