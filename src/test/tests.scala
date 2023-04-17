e89e5219378cd1eab3c81d85f479519b44f703c7
Jon Pretty <jon.pretty@propensive.com>
2023-04-17 16:44:05 +0200

    Added some uninteresting benchmarks

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -97,3 +97,28 @@ object Tests extends Suite(t"Jacinta Tests"):
         case class OptFoo(x: Option[Int])
         Json.parse(t"""{"y": 1}""").as[OptFoo].x
       .assert(_ == None)
+    
+    suite(t"Basic tests"):
+      test(t"Parse a simple object with one string value"):
+        Json.parse(t"""{"foo": "bar"}""")
+      .benchmark(duration = 1000L, warmup = 1000L)
+      
+      test(t"Parse a simple object with one numerical value"):
+        Json.parse(t"""{"foo": 3.1415926 }""")
+      .benchmark(duration = 1000L, warmup = 1000L)
+      
+      test(t"Parse true value"):
+        Json.parse(t"""{"foo": true }""")
+      .benchmark(duration = 1000L, warmup = 1000L)
+      
+      test(t"Parse false value"):
+        Json.parse(t"""{"foo": false }""")
+      .benchmark(duration = 1000L, warmup = 1000L)
+      
+      test(t"Parse array of strings"):
+        Json.parse(t"""["foo", "bar", "baz", "quux", "abcd", "defg", "hijk"]""")
+      .benchmark(duration = 1000L, warmup = 1000L)
+      
+      test(t"Parse array of numbers"):
+        Json.parse(t"""[12345.6789, 98765.4321, 142536.475869]""")
+      .benchmark(duration = 1000L, warmup = 1000L)
