d24a9737534b0102dbe075a63f9de12ee5402257
Jon Pretty <jon.pretty@propensive.com>
2023-04-28 08:50:40 +0200

    Add compile benchmarks

---
+++ src/bench/bench.scala
@@ -0,0 +1,23 @@
+package gossamer
+
+import larceny.*
+import probably.*
+
+case class Group(persons: List[Person])
+case class Person(name: String, age: Int)
+
+object Benchmarks extends Suite(t"Gossamer Benchmarks"):
+  def run(): Unit =
+    suite(t"Compile performance"):
+      test("Resolve a Show instance"):
+        deferCompilation:
+          import gossamer.*
+          Group(List(Person("Jack", 30))).show
+      .benchmark(warmup = 30000L, duration = 30000L)
+      
+      test("Resolve a Debug instance"):
+        deferCompilation:
+          import gossamer.*
+          Group(List(Person("Jack", 30))).debug
+      .benchmark(warmup = 30000L, duration = 30000L)
+      
