97637134a76a76d6c12f2d950c9540e0d8f512b6
Jon Pretty <jon.pretty@propensive.com>
2023-05-11 11:51:28 +0200

    Switch to new capture-checking syntax

--- src/core/functor.scala
+++ src/core/functor.scala
@@ -18,30 +18,30 @@ package rudiments
 
 import language.experimental.captureChecking
 
-extension [FunctorType[+_], ValueType](value: {*} FunctorType[ValueType])(using functor: Functor[FunctorType])
-  def map[ValueType2](fn: ValueType => ValueType2): {value, fn} FunctorType[ValueType2] =
+extension [FunctorType[+_], ValueType](value: FunctorType[ValueType]^)(using functor: Functor[FunctorType])
+  def map[ValueType2](fn: ValueType => ValueType2): FunctorType[ValueType2]^{value, fn} =
     functor.map(value, fn)
 
-extension [CofunctorType[-_], ValueType](value: {*} CofunctorType[ValueType])
+extension [CofunctorType[-_], ValueType](value: CofunctorType[ValueType]^)
           (using cofunctor: Cofunctor[CofunctorType])
-  def contraMap[ValueType2](fn: ValueType2 => ValueType): {value, fn} CofunctorType[ValueType2] =
+  def contraMap[ValueType2](fn: ValueType2 => ValueType): CofunctorType[ValueType2]^{value, fn} =
     cofunctor.contraMap(value, fn)
 
 trait Functor[FunctorType[+_]]:
-  def map[ValueType, ValueType2](value: {*} FunctorType[ValueType], fn: ValueType => ValueType2)
-          : {value, fn} FunctorType[ValueType2]
+  def map[ValueType, ValueType2](value: FunctorType[ValueType]^, fn: ValueType => ValueType2)
+          : FunctorType[ValueType2]^{value, fn}
     
 object Functor:
   given list: Functor[List] with
-    def map[ElemType, ElemType2](list: {*} List[ElemType], fn: ElemType => ElemType2)
-            : {list, fn} List[ElemType2] =
+    def map[ElemType, ElemType2](list: List[ElemType]^, fn: ElemType => ElemType2)
+            : List[ElemType2]^{list, fn} =
       list.map(fn)
   
   given option: Functor[Option] with
-    def map[ValueType, ValueType2](option: {*} Option[ValueType], fn: ValueType => ValueType2)
-            : {option, fn} Option[ValueType2] =
+    def map[ValueType, ValueType2](option: Option[ValueType]^, fn: ValueType => ValueType2)
+            : Option[ValueType2]^{option, fn} =
       option.map(fn)
 
 trait Cofunctor[CofunctorType[-_]]:
-  def contraMap[ValueType, ValueType2](value: {*} CofunctorType[ValueType], fn: ValueType2 => ValueType)
-               : {value, fn} CofunctorType[ValueType2]
+  def contraMap[ValueType, ValueType2](value: CofunctorType[ValueType]^, fn: ValueType2 => ValueType)
+               : CofunctorType[ValueType2]^{value, fn}
--- src/core/maybe.scala
+++ src/core/maybe.scala
@@ -28,21 +28,20 @@ case class UnsetValueError() extends Error(ErrorMessage(List(Text("the value was
 extension [ValueType](opt: Maybe[ValueType])
   def unset: Boolean = opt == Unset
   def cast(using Unsafe.type): ValueType = opt.asInstanceOf[ValueType]
-  def or(value: => ValueType): {value} ValueType = if unset then value else cast(using Unsafe)
-  def presume(using default: => Default[ValueType]): {default} ValueType = or(default())
+  def or(value: => ValueType): ValueType^{value} = if unset then value else cast(using Unsafe)
+  def presume(using default: => Default[ValueType]): ValueType^{default} = or(default())
   
-  def assume(using unsetValue: CanThrow[UnsetValueError]): {unsetValue} ValueType =
+  def assume(using unsetValue: CanThrow[UnsetValueError]): ValueType^{unsetValue} =
     or(throw UnsetValueError())
   
   def option: Option[ValueType] = if unset then None else Some(cast(using Unsafe))
 
   def fm
-      [ValueType2]
-      (default: => ValueType2)(fn: ValueType => ValueType2)
-      : {default, fn} ValueType2 =
+      [ValueType2](default: => ValueType2)(fn: ValueType => ValueType2)
+      : ValueType2^{default, fn} =
     if unset then default else fn(cast(using Unsafe))
 
-  def mm[ValueType2](fn: ValueType => ValueType2): {fn} Maybe[ValueType2] =
+  def mm[ValueType2](fn: ValueType => ValueType2): Maybe[ValueType2]^{fn} =
     if unset then Unset else fn(cast(using Unsafe))
 
 extension [ValueType](opt: Option[ValueType])
