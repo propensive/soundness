f51cab779611b9499fc6fe6fbee888d0bd3d699b
Jon Pretty <jon.pretty@propensive.com>
2023-05-11 16:07:05 +0200

    Run tests

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -25,31 +25,29 @@ import escapes.*
 
 import unsafeExceptions.canThrowAny
 
-given Log(Everything |-> Stdout)
-
 object Tests extends Suite(t"Escapade tests"):
   def run(): Unit =
-    test(t"normal string") {
+    test(t"normal string"):
       ansi"hello world".render
-    }.check(_ == t"hello world")
+    .assert(_ == t"hello world")
     
-    test(t"simple string substitution") {
+    test(t"simple string substitution"):
       ansi"hello ${"world"}".render
-    }.check(_ == t"hello world")
+    .assert(_ == t"hello world")
     
-    test(t"bold text") {
+    test(t"bold text"):
       ansi"$Bold{bold} text".render
-    }.check(_ == t"\e[1mbold\e[22m text")
+    .assert(_ == t"\e[1mbold\e[22m text")
     
-    test(t"italic text") {
+    test(t"italic text"):
       ansi"$Italic{italic} text".render
-    }.check(_ == t"\e[3mitalic\e[23m text")
+    .assert(_ == t"\e[3mitalic\e[23m text")
     
-    test(t"24-bit colored text") {
+    test(t"24-bit colored text"):
       ansi"${iridescence.colors.Tan}[text]".render
-    }.check(_ == t"\e[38;2;210;180;139mtext\e[38;2;255;255;255m")
+    .assert(_ == t"\e[38;2;210;180;139mtext\e[39m")
     
-    test(t"non-escape insertion should not parse brackets") {
+    test(t"non-escape insertion should not parse brackets"):
       val notAnEscape = 42
       ansi"${notAnEscape}[text]".render
-    }.check(_ == t"42[text]")
+    .assert(_ == t"42[text]")
