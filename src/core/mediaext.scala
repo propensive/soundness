9c4cc7b2c17d9a455b9a57a8aeb0c77ff01e6f12
Jon Pretty <jon.pretty@propensive.com>
2023-05-12 09:54:41 +0200

    Use simpler Contextual API

--- src/core/mediaext.scala
+++ src/core/mediaext.scala
@@ -18,4 +18,4 @@ package gesticulate
 
 extension (inline ctx: StringContext)
   transparent inline def media(inline parts: String*): MediaType =
-    ${Media.Prefix.expand('{Media.Prefix}, 'ctx, 'parts)}
+    ${Media.Prefix.expand('ctx, 'parts)}
