192b96cd15894fa2335a5443f4d9b02c0b659219
Jon Pretty <jon.pretty@propensive.com>
2023-05-25 10:27:40 +0200

    Redefine color in terms of a single RGB int value

--- src/color/color.scala
+++ src/color/color.scala
@@ -1,6 +1,10 @@
 package anticipation
 
-trait Color[ColorType]:
-  def red(color: ColorType): Int
-  def green(color: ColorType): Int
-  def blue(color: ColorType): Int
+trait RgbColor[ColorType]:
+  def rgb(color: ColorType): Int
+  def red(color: ColorType): Int = rgb(color)&255
+  def green(color: ColorType): Int = (rgb(color) >> 8)&255
+  def blue(color: ColorType): Int = (rgb(color) >> 16)&255
+
+extension [ColorType](color: ColorType)
+  def asRgb24Int(using rgbColor: RgbColor[ColorType]): Int = rgbColor.rgb(color)
