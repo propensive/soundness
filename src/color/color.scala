bad88b94260b98011f29e95582406d96a4198e45
Jon Pretty <jon.pretty@propensive.com>
2023-06-04 11:42:31 +0200

    Use typeclass style for color extension method

--- src/color/color.scala
+++ src/color/color.scala
@@ -19,10 +19,9 @@ package anticipation
 import language.experimental.captureChecking
 
 trait RgbColor[ColorType]:
-  def rgb(color: ColorType): Int
-  def red(color: ColorType): Int = rgb(color)&255
-  def green(color: ColorType): Int = (rgb(color) >> 8)&255
-  def blue(color: ColorType): Int = (rgb(color) >> 16)&255
+  extension (color: ColorType) def asRgb24Int: Int
+  
+  def red(color: ColorType): Int = asRgb24Int(color)&255
+  def green(color: ColorType): Int = (asRgb24Int(color) >> 8)&255
+  def blue(color: ColorType): Int = (asRgb24Int(color) >> 16)&255
 
-extension [ColorType](color: ColorType)
-  def asRgb24Int(using rgbColor: RgbColor[ColorType]): Int = rgbColor.rgb(color)
