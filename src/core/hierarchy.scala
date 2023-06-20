ed6a21d169085c3c1464085fdcfa76ead2063113
Jon Pretty <jon.pretty@propensive.com>
2023-06-20 19:17:50 +0200

    Split into multiple source files

--- src/core/hierarchy.scala
+++ src/core/hierarchy.scala
@@ -19,7 +19,6 @@ package serpentine
 import rudiments.*
 import digression.*
 import spectacular.*
-import kaleidoscope.*
 import gossamer.*
 
 import scala.quoted.*
@@ -55,18 +54,6 @@ export SerpentineOpaques.*
 
 case class PathError(reason: PathError.Reason)
 extends Error(err"the path is invalid because ${reason.show}")
-
-object UnixRoot:
-  @targetName("Root")
-  final val `%`: UnixPath = UnixPath(Nil)
-    
-    @targetName("child")
-    infix def /(name: PathName[UnixForbidden]): UnixPath = UnixPath(List(name))
-    
-    @targetName("child2")
-    infix def /(name: Text): UnixPath throws PathError = UnixPath(List(PathName(name)))
-
-export UnixRoot.%
 
 @targetName("relative")
 def ?
@@ -174,111 +161,6 @@ extension
       
       absolutePathlike.make(absolutePathlike.root(left), relative.ancestry ::: ancestry)
 
-type UnixForbidden =
-  ".*<.*" | ".*>.*" | ".*:.*" | ".*\".*" | ".*\\\\.*" | ".*\\|.*" | ".*\\?.*" | ".*\\*.*" | ".*/.*"
-
-type WindowsForbidden =
-  "con(\\..*)?" | "prn(\\..*)?" | "aux(\\..*)?" | "nul(\\..*)?" | "com1(\\..*)?" | "com2(\\..*)?" |
-      "com3(\\..*)?" | "com4(\\..*)?" | "com5(\\..*)?" | "com6(\\..*)?" | "com7(\\..*)?" |
-      "com8(\\..*)?" | "com9(\\..*)?" | "lpt1(\\..*)?" | "lpt2(\\..*)?" | "lpt3(\\..*)?" |
-      "lpt4(\\..*)?" | "lpt5(\\..*)?" | "lpt6(\\..*)?" | "lpt7(\\..*)?" | "lpt8(\\..*)?" |
-      "lpt9(\\..*)?" | ".* " | ".*\\."
-
-object UnixPath:
-
-  def parse(text: Text): UnixPath throws PathError = pathlike.parse(text)
-
-  given pathlike: AbsolutePathlike[UnixPath, UnixForbidden, %.type](t"/") with
-    def root(path: UnixPath): %.type = %
-    def prefix(root: %.type): Text = t"/"
-    def child(path: UnixPath, name: PathName[UnixForbidden]): UnixPath =
-      UnixPath(name :: path.ancestry)
-    
-    def make(root: %.type, ancestry: List[PathName[UnixForbidden]]): UnixPath =
-      UnixPath(ancestry)
-
-    def parseRoot(text: Text): (%.type, Text) throws PathError =
-      if text.starts(t"/") then (%, text.drop(1))
-      else throw PathError(PathError.Reason.NotRooted)
-    
-    def ancestry(path: UnixPath): List[PathName[UnixForbidden]] = path.ancestry
-    
-    def parent(path: UnixPath): Maybe[UnixPath] =
-      if path.ancestry == Nil then Unset else UnixPath(path.ancestry.tail)
-    
-    def ancestor(path: UnixPath, n: Int): Maybe[UnixPath] =
-      if path.ancestry.length < n then Unset else UnixPath(path.ancestry.drop(n))
-    
-
-case class UnixPath(ancestry: List[PathName[UnixForbidden]])
-
-object WindowsPath:
-  def parse(text: Text): WindowsPath throws PathError = pathlike.parse(text)
-  
-  given pathlike: AbsolutePathlike[WindowsPath, WindowsForbidden, WindowsDrive](t"\\") with
-    def root(path: WindowsPath): WindowsDrive = path.drive
-    def prefix(drive: WindowsDrive): Text = t"${drive.letter}:\\"
-    
-    def make(drive: WindowsDrive, ancestry: List[PathName[WindowsForbidden]]): WindowsPath =
-      WindowsPath(drive, ancestry)
-    
-    def parseRoot(text: Text): (WindowsDrive, Text) throws PathError = text match
-      case r"$letter([A-Za-z]):\\.*" => (WindowsDrive(unsafely(letter(0).toUpper)), text.drop(3))
-      case _                         => throw PathError(PathError.Reason.NotRooted)
-
-    def child(path: WindowsPath, name: PathName[WindowsForbidden]): WindowsPath =
-      WindowsPath(path.drive, name :: path.ancestry)
-    
-    def ancestry(path: WindowsPath): List[PathName[WindowsForbidden]] = path.ancestry
-    
-    def parent(path: WindowsPath): Maybe[WindowsPath] =
-      if path.ancestry == Nil then Unset else WindowsPath(path.drive, path.ancestry.tail)
-    
-    def ancestor(path: WindowsPath, n: Int): Maybe[WindowsPath] =
-      if path.ancestry.length < n then Unset else WindowsPath(path.drive, path.ancestry.drop(n))
-    
-
-case class WindowsPath(drive: WindowsDrive, ancestry: List[PathName[WindowsForbidden]])
-
-object RelativeUnixPath:
-  def parse(text: Text): RelativeUnixPath throws PathError = pathlike.parse(text)
-  
-  given pathlike: RelativePathlike[RelativeUnixPath, UnixForbidden](t"/", t"..", t".") with
-    def ascent(path: RelativeUnixPath): Int = path.ascent
-
-    def make(ascent: Int, ancestry: List[PathName[UnixForbidden]]): RelativeUnixPath =
-      RelativeUnixPath(ascent, ancestry)
-
-    def child(path: RelativeUnixPath, name: PathName[UnixForbidden]): RelativeUnixPath =
-      RelativeUnixPath(path.ascent, name :: path.ancestry)
-    
-    def ancestry(path: RelativeUnixPath): List[PathName[UnixForbidden]] = path.ancestry
-    
-    def parent(path: RelativeUnixPath): RelativeUnixPath =
-      if path.ancestry == Nil then RelativeUnixPath(path.ascent + 1, Nil)
-      else RelativeUnixPath(path.ascent, path.ancestry.tail)
-    
-object RelativeWindowsPath:
-  def parse(text: Text): RelativeWindowsPath throws PathError = pathlike.parse(text)
-  
-  given pathlike: RelativePathlike[RelativeWindowsPath, WindowsForbidden](t"\\", t"..", t".") with
-    def ascent(path: RelativeWindowsPath): Int = path.ascent
-    
-    def make(ascent: Int, ancestry: List[PathName[WindowsForbidden]]): RelativeWindowsPath =
-      RelativeWindowsPath(ascent, ancestry)
-    
-    def child(path: RelativeWindowsPath, name: PathName[WindowsForbidden]): RelativeWindowsPath =
-      RelativeWindowsPath(path.ascent, name :: path.ancestry)
-    
-    def ancestry(path: RelativeWindowsPath): List[PathName[WindowsForbidden]] = path.ancestry
-    
-    def parent(path: RelativeWindowsPath): RelativeWindowsPath =
-      if path.ancestry == Nil then RelativeWindowsPath(path.ascent + 1, Nil)
-      else RelativeWindowsPath(path.ascent, path.ancestry.tail)
-    
-case class RelativeUnixPath(ascent: Int, ancestry: List[PathName[UnixForbidden]])
-case class RelativeWindowsPath(ascent: Int, ancestry: List[PathName[WindowsForbidden]])
-
 trait Pathlike[PathType <: Matchable, NameType <: Label]:
   def pathSeparator: Text
   def child(path: PathType, name: PathName[NameType]): PathType
@@ -288,6 +170,7 @@ trait Pathlike[PathType <: Matchable, NameType <: Label]:
 
 trait AbsolutePathlike[PathType <: Matchable, NameType <: Label, RootType](val pathSeparator: Text)
 extends Pathlike[PathType, NameType]:
+
   def prefix(root: RootType): Text
   def root(path: PathType): RootType
   def parent(path: PathType): Maybe[PathType]
@@ -306,12 +189,12 @@ extends Pathlike[PathType, NameType]:
       case names       => names
 
     make(root, names.map(PathName(_)))
-
   
 trait RelativePathlike
     [PathType <: Matchable, NameType <: Label]
     (val pathSeparator: Text, val parentRef: Text, val selfRef: Text)
 extends Pathlike[PathType, NameType]:
+
   def parent(path: PathType): PathType
   def ascent(path: PathType): Int
   
@@ -364,19 +247,6 @@ extension
   transparent inline def ancestor(n: Int): Maybe[PathType] = compiletime.summonFrom:
     case pathlike: AbsolutePathlike[PathType, NameType, ?] => pathlike.ancestor(path, n)
     case pathlike: RelativePathlike[PathType, NameType] => pathlike.ancestor(path, n)
-
-case class WindowsDrive(letter: Char):
-  def apply(): WindowsPath = WindowsPath(this, Nil)
-  
-  @targetName("child")
-  infix def /(name: PathName[WindowsForbidden]): WindowsPath = WindowsPath(this, List(name))
-
-package hierarchies:
-  erased given unix: Hierarchy[UnixPath, RelativeUnixPath, %.type, UnixForbidden] = ###
-
-  erased given windows
-      : Hierarchy[WindowsPath, RelativeWindowsPath, WindowsDrive, WindowsForbidden] =
-    ###
 
 extension (inline context: StringContext)
   inline def p[ForbiddenType <: Label](): PathName[ForbiddenType] =
---
+++ src/core/paths.scala
@@ -0,0 +1,137 @@
+package serpentine
+
+import rudiments.*
+import gossamer.*
+import kaleidoscope.*
+import digression.*
+
+object UnixRoot:
+  @targetName("Root")
+  final val `%`: UnixPath = UnixPath(Nil)
+    
+    @targetName("child")
+    infix def /(name: PathName[UnixForbidden]): UnixPath = UnixPath(List(name))
+    
+    @targetName("child2")
+    infix def /(name: Text): UnixPath throws PathError = UnixPath(List(PathName(name)))
+
+export UnixRoot.%
+
+type UnixForbidden =
+  ".*<.*" | ".*>.*" | ".*:.*" | ".*\".*" | ".*\\\\.*" | ".*\\|.*" | ".*\\?.*" | ".*\\*.*" | ".*/.*"
+
+type WindowsForbidden =
+  "con(\\..*)?" | "prn(\\..*)?" | "aux(\\..*)?" | "nul(\\..*)?" | "com1(\\..*)?" | "com2(\\..*)?" |
+      "com3(\\..*)?" | "com4(\\..*)?" | "com5(\\..*)?" | "com6(\\..*)?" | "com7(\\..*)?" |
+      "com8(\\..*)?" | "com9(\\..*)?" | "lpt1(\\..*)?" | "lpt2(\\..*)?" | "lpt3(\\..*)?" |
+      "lpt4(\\..*)?" | "lpt5(\\..*)?" | "lpt6(\\..*)?" | "lpt7(\\..*)?" | "lpt8(\\..*)?" |
+      "lpt9(\\..*)?" | ".* " | ".*\\."
+
+
+object UnixPath:
+
+  def parse(text: Text): UnixPath throws PathError = pathlike.parse(text)
+
+  given pathlike: AbsolutePathlike[UnixPath, UnixForbidden, %.type](t"/") with
+    def root(path: UnixPath): %.type = %
+    def prefix(root: %.type): Text = t"/"
+    def child(path: UnixPath, name: PathName[UnixForbidden]): UnixPath =
+      UnixPath(name :: path.ancestry)
+    
+    def make(root: %.type, ancestry: List[PathName[UnixForbidden]]): UnixPath =
+      UnixPath(ancestry)
+
+    def parseRoot(text: Text): (%.type, Text) throws PathError =
+      if text.starts(t"/") then (%, text.drop(1))
+      else throw PathError(PathError.Reason.NotRooted)
+    
+    def ancestry(path: UnixPath): List[PathName[UnixForbidden]] = path.ancestry
+    
+    def parent(path: UnixPath): Maybe[UnixPath] =
+      if path.ancestry == Nil then Unset else UnixPath(path.ancestry.tail)
+    
+    def ancestor(path: UnixPath, n: Int): Maybe[UnixPath] =
+      if path.ancestry.length < n then Unset else UnixPath(path.ancestry.drop(n))
+    
+
+case class UnixPath(ancestry: List[PathName[UnixForbidden]])
+
+
+object WindowsPath:
+  def parse(text: Text): WindowsPath throws PathError = pathlike.parse(text)
+  
+  given pathlike: AbsolutePathlike[WindowsPath, WindowsForbidden, WindowsDrive](t"\\") with
+    def root(path: WindowsPath): WindowsDrive = path.drive
+    def prefix(drive: WindowsDrive): Text = t"${drive.letter}:\\"
+    
+    def make(drive: WindowsDrive, ancestry: List[PathName[WindowsForbidden]]): WindowsPath =
+      WindowsPath(drive, ancestry)
+    
+    def parseRoot(text: Text): (WindowsDrive, Text) throws PathError = text match
+      case r"$letter([A-Za-z]):\\.*" => (WindowsDrive(unsafely(letter(0).toUpper)), text.drop(3))
+      case _                         => throw PathError(PathError.Reason.NotRooted)
+
+    def child(path: WindowsPath, name: PathName[WindowsForbidden]): WindowsPath =
+      WindowsPath(path.drive, name :: path.ancestry)
+    
+    def ancestry(path: WindowsPath): List[PathName[WindowsForbidden]] = path.ancestry
+    
+    def parent(path: WindowsPath): Maybe[WindowsPath] =
+      if path.ancestry == Nil then Unset else WindowsPath(path.drive, path.ancestry.tail)
+    
+    def ancestor(path: WindowsPath, n: Int): Maybe[WindowsPath] =
+      if path.ancestry.length < n then Unset else WindowsPath(path.drive, path.ancestry.drop(n))
+
+case class WindowsPath(drive: WindowsDrive, ancestry: List[PathName[WindowsForbidden]])
+
+object RelativeUnixPath:
+  def parse(text: Text): RelativeUnixPath throws PathError = pathlike.parse(text)
+  
+  given pathlike: RelativePathlike[RelativeUnixPath, UnixForbidden](t"/", t"..", t".") with
+    def ascent(path: RelativeUnixPath): Int = path.ascent
+
+    def make(ascent: Int, ancestry: List[PathName[UnixForbidden]]): RelativeUnixPath =
+      RelativeUnixPath(ascent, ancestry)
+
+    def child(path: RelativeUnixPath, name: PathName[UnixForbidden]): RelativeUnixPath =
+      RelativeUnixPath(path.ascent, name :: path.ancestry)
+    
+    def ancestry(path: RelativeUnixPath): List[PathName[UnixForbidden]] = path.ancestry
+    
+    def parent(path: RelativeUnixPath): RelativeUnixPath =
+      if path.ancestry == Nil then RelativeUnixPath(path.ascent + 1, Nil)
+      else RelativeUnixPath(path.ascent, path.ancestry.tail)
+    
+object RelativeWindowsPath:
+  def parse(text: Text): RelativeWindowsPath throws PathError = pathlike.parse(text)
+  
+  given pathlike: RelativePathlike[RelativeWindowsPath, WindowsForbidden](t"\\", t"..", t".") with
+    def ascent(path: RelativeWindowsPath): Int = path.ascent
+    
+    def make(ascent: Int, ancestry: List[PathName[WindowsForbidden]]): RelativeWindowsPath =
+      RelativeWindowsPath(ascent, ancestry)
+    
+    def child(path: RelativeWindowsPath, name: PathName[WindowsForbidden]): RelativeWindowsPath =
+      RelativeWindowsPath(path.ascent, name :: path.ancestry)
+    
+    def ancestry(path: RelativeWindowsPath): List[PathName[WindowsForbidden]] = path.ancestry
+    
+    def parent(path: RelativeWindowsPath): RelativeWindowsPath =
+      if path.ancestry == Nil then RelativeWindowsPath(path.ascent + 1, Nil)
+      else RelativeWindowsPath(path.ascent, path.ancestry.tail)
+    
+case class RelativeUnixPath(ascent: Int, ancestry: List[PathName[UnixForbidden]])
+case class RelativeWindowsPath(ascent: Int, ancestry: List[PathName[WindowsForbidden]])
+
+case class WindowsDrive(letter: Char):
+  def apply(): WindowsPath = WindowsPath(this, Nil)
+  
+  @targetName("child")
+  infix def /(name: PathName[WindowsForbidden]): WindowsPath = WindowsPath(this, List(name))
+
+package hierarchies:
+  erased given unix: Hierarchy[UnixPath, RelativeUnixPath, %.type, UnixForbidden] = ###
+
+  erased given windows
+      : Hierarchy[WindowsPath, RelativeWindowsPath, WindowsDrive, WindowsForbidden] =
+    ###
