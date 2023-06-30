22dc8203dac9b130dc98ab0fac046b0e803b9dab
Jon Pretty <jon.pretty@propensive.com>
2023-06-30 16:15:44 +0200

    Added a `hex` interpolator

--- src/core/bin.scala
+++ src/core/bin.scala
@@ -27,5 +27,31 @@ object Rudiments:
       case 64 => Expr[Long](long)
       case _  => fail(s"a binary literal must be 8, 16, 32 or 64 bits long")
 
+  def hex(expr: Expr[StringContext])(using Quotes): Expr[IArray[Byte]] =
+    import quotes.reflect.*
+    
+    val startPos = expr.asTerm.pos
+    val nibbles = expr.valueOrAbort.parts.head
+    val nibbles2 = nibbles.map(_.toLower)
+
+    nibbles2.indexWhere: c =>
+      !(c >= '0' && c <= '9') && !(c >= 'a' && c <= 'f') && c != ' ' && c != '\n'
+    .match
+      case -1  => ()
+      
+      case idx =>
+        val pos = Position(startPos.sourceFile, startPos.start + idx, startPos.start + idx + 1)
+        fail(s"'${nibbles(idx)}' is not a valid hexadecimal character")
+
+    val nibbles3 = nibbles2.filterNot { ch => ch == ' ' || ch == '\n' }
+
+    if nibbles3.length%2 != 0
+    then fail("a hexadecimal value must have an even number of digits", Position.ofMacroExpansion)
+
+    val bytes = nibbles3.grouped(2).map(Integer.parseInt(_, 16).toByte).to(List)
+
+    '{IArray.from(${Expr(bytes)})}
+
 extension (inline context: StringContext)
   transparent inline def bin(): AnyVal = ${Rudiments.bin('context)}
+  transparent inline def hex(): IArray[Byte] = ${Rudiments.hex('context)}
