48a346fb7772ce6e7ed9e08d9073b68939e87381
Jon Pretty <jon.pretty@propensive.com>
2023-07-04 09:10:23 +0200

    Rename `captureCompileErrors` to `demilitarize`

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -26,19 +26,19 @@ object Tests extends Suite(t"Cardinality tests"):
     suite(t"Compile-time tests"):
       
       test(t"Value is less than lower bound"):
-        captureCompileErrors:
+        demilitarize:
           val x: -1.0 ~ 1.0 = -1.01
         .map(_.errorId)
       .assert(_ == List(ErrorId.TypeMismatchID))
       
       test(t"Value is greater than upper bound"):
-        captureCompileErrors:
+        demilitarize:
           val x: -1.0 ~ 1.0 = 1.01
         .map(_.errorId)
       .assert(_ == List(ErrorId.TypeMismatchID))
       
       test(t"Doubling a number doubles its range"):
-        captureCompileErrors:
+        demilitarize:
           val x: -1.0 ~ 1.0 = 0.0
           val y: -2.0 ~ 2.0 = x*2.0
       .aspire(_ == Nil)
