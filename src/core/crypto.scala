3288ebe0e130e07814f6a151ff864e102f06370d
Jon Pretty <jon.pretty@propensive.com>
2023-07-11 14:03:43 +0200

    Remove unused import

--- src/core/crypto.scala
+++ src/core/crypto.scala
@@ -20,7 +20,6 @@ import javax.crypto.*, javax.crypto.spec.*
 import java.security as js, js.spec.*
 
 import rudiments.*
-import digression.*
 import gossamer.*
 import spectacular.*
 import hieroglyph.*
