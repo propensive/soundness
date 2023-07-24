6d17147d1bd983b69f2e73728234ea0fd6b6e62b
Jon Pretty <jon.pretty@propensive.com>
2023-07-24 11:13:39 +0200

    Changes for Symbolism

--- src/text/text.scala
+++ src/text/text.scala
@@ -1,5 +1,7 @@
 package anticipation
 
+import symbolism.*
+
 import scala.quoted.*
 import scala.compiletime.*
 import scala.util.*
@@ -12,6 +14,18 @@ object Anticipation:
     def apply(string: String): Text = string
     extension (text: Text) inline def s: String = text
 
+    given add: ClosedOperator["+", Text] = _+_
+    
+    given times: Operator["*", Text, Int] with
+      type Result = Text
+      
+      private def recur(text: Text, n: Int, acc: Text): Text =
+        if n == 0 then acc else recur(text, n - 1, acc+text)
+      
+      inline def apply(inline left: Text, inline right: Int): Text =
+        recur(left, right.max(0), "")
+
+    given ordering: Ordering[Text] = Ordering.String.on[Text](identity)
     given fromString: CommandLineParser.FromString[Text] = identity(_)
     given genericHttpRequestParam: GenericHttpRequestParam[String, Text] = identity(_)
     
