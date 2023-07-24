9f2586b35ca2fa162850b69b7d9cc7aa8875da78
Jon Pretty <jon.pretty@propensive.com>
2023-07-24 14:59:52 +0200

    Fixed compile errors

--- src/core/complex.scala
+++ src/core/complex.scala
@@ -5,6 +5,7 @@ import rudiments.*
 import spectacular.*
 import quantitative.*
 import anticipation.*
+import symbolism.*
 
 val I: Complex[Double] = Complex(0.0, 1.0)
 
@@ -25,26 +26,27 @@ case class Complex[+ComponentType](real: ComponentType, imaginary: ComponentType
   def +
       [ComponentType2]
       (right: Complex[ComponentType2])
-      (using add: Add[ComponentType, ComponentType2])
+      (using add: Operator["+", ComponentType, ComponentType2])
       : Complex[add.Result] =
-    Complex[add.Result](add(real, right.real, false), add(imaginary, right.imaginary, false))
+    Complex[add.Result](add(real, right.real), add(imaginary, right.imaginary))
   
   def -
       [ComponentType2]
       (right: Complex[ComponentType2])
-      (using add: Add[ComponentType, ComponentType2])
-      : Complex[add.Result] =
-    Complex[add.Result](add(real, right.real, true), add(imaginary, right.imaginary, true))
+      (using subtract: Operator["-", ComponentType, ComponentType2])
+      : Complex[subtract.Result] =
+    Complex[subtract.Result](subtract(real, right.real), subtract(imaginary, right.imaginary))
   
   def *
       [ComponentType2]
       (right: Complex[ComponentType2])
-      (using multiply: Multiply[ComponentType, ComponentType2])
-      (using add: Add[multiply.Result, multiply.Result])
-      : Complex[add.Result] =
+      (using multiply: Operator["*", ComponentType, ComponentType2])
+      (using add: Operator["+", multiply.Result, multiply.Result])
+      (using subtract: Operator["-", multiply.Result, multiply.Result])
+      : Complex[add.Result | subtract.Result] =
     val ac: multiply.Result = multiply(real, right.real)
     val bd: multiply.Result = multiply(imaginary, right.imaginary)
     val ad: multiply.Result = multiply(real, right.imaginary)
     val bc: multiply.Result = multiply(imaginary, right.real)
     
-    Complex[add.Result](add(ac, bd, true), add(ad, bc, false))
+    Complex(subtract(ac, bd), add(ad, bc))
