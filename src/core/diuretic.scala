5ca7feab7e43ea91e64dc3662a110c588dd958b6
Jon Pretty <jon.pretty@propensive.com>
2023-08-02 20:56:40 +0200

    Changed the Anticipation paths API again

--- src/core/diuretic.scala
+++ src/core/diuretic.scala
@@ -44,25 +44,29 @@ object JavaUtilTime extends GenericInstant[ju.Date], GenericDuration[Long]:
 
 object JavaNioFile
 extends GenericFileMaker[jnf.Path], GenericDirectoryMaker[jnf.Path], GenericPathMaker[jnf.Path],
-    GenericPathReader[jnf.Path]:
+    GenericPathReader[jnf.Path], GenericDirectoryReader[jnf.Path], GenericFileReader[jnf.Path]:
 
   def makePath(path: Text): jnf.Path = jnf.Paths.get(path.s).nn
 
   def makeFile(path: Text): jnf.Path = makePath(path)
   def makeDirectory(path: Text): jnf.Path = makePath(path)
 
-  def getPath(value: jnf.Path): Text = value.toAbsolutePath.nn.toString.tt
+  def fromPath(value: jnf.Path): Text = value.toAbsolutePath.nn.toString.tt
+  def fromFile(value: jnf.Path): Text = fromPath(value)
+  def fromDirectory(value: jnf.Path): Text = fromPath(value)
 
 object JavaIoFile
 extends GenericFileMaker[ji.File], GenericDirectoryMaker[ji.File], GenericPathMaker[ji.File],
-    GenericPathReader[ji.File]:
+    GenericPathReader[ji.File], GenericDirectoryReader[ji.File], GenericFileReader[ji.File]:
 
   def makePath(path: Text): ji.File = ji.File(path.s).nn
 
   def makeFile(path: Text): ji.File = makePath(path)
   def makeDirectory(path: Text): ji.File = makePath(path)
 
-  def getPath(value: ji.File): Text = value.getAbsolutePath.nn.toString.tt
+  def fromPath(value: ji.File): Text = value.getAbsolutePath.nn.toString.tt
+  def fromFile(value: ji.File): Text = fromPath(value)
+  def fromDirectory(value: ji.File): Text = fromPath(value)
 
 object JavaNetUrl extends GenericUrl[jn.URL]:
   def urlText(value: jn.URL): Text = value.toString.tt
