021dc174c038066b041c3daeedb4959d16936ff6
Jon Pretty <jon.pretty@propensive.com>
2023-08-03 14:14:09 +0200

    Working, as proof of concept

--- src/core/git.scala
+++ src/core/git.scala
@@ -1,14 +1,14 @@
 package nonagenarian
 
-import ambience.*, systemProperties.jvm
-import anticipation.*
+import anticipation.*, fileApi.galileiApi
 import eucalyptus.*
 import galilei.*, filesystemOptions.{doNotCreateNonexistent, dereferenceSymlinks}
 import gossamer.*
 import guillotine.*
 import kaleidoscope.*
-import rudiments.*
-import serpentine.*
+import ambience.*
+import rudiments.*, workingDirectory.jvm
+import serpentine.*, hierarchies.unix
 import spectacular.*
 import turbulence.*
 
@@ -16,32 +16,42 @@ import language.experimental.captureChecking
 
 object GitError:
   enum Detail:
-    case GitNotOnPath
-    case CloneFailed
-    case InvalidRepoPath
+    case CannotExecuteGit, CloneFailed, InvalidRepoPath, RepoDoesNotExist, BranchDoesNotExist,
+        CommitDoesNotExist
   
   given AsMessage[Detail] =
-    case Detail.GitNotOnPath    => msg"the `git` executable was not on the path"
-    case Detail.CloneFailed     => msg"the `git` executable was not on the path"
-    case Detail.InvalidRepoPath => msg"the repository path was not valid"
+    case Detail.CannotExecuteGit   => msg"the `git` command could not be executed"
+    case Detail.CloneFailed        => msg"the repository could not be cloned"
+    case Detail.InvalidRepoPath    => msg"the repository path was not valid"
+    case Detail.RepoDoesNotExist   => msg"the repository does not exist"
+    case Detail.BranchDoesNotExist => msg"the branch does not exist"
+    case Detail.CommitDoesNotExist => msg"the commit does not exist"
 
 case class GitError(detail: GitError.Detail)
 extends Error(msg"the Git operation could not be completed because $detail")
 
 case class CloneProcess(complete: () => GitRepo, progress: LazyList[Progress])
 
-case class GitRepo(directory: Directory, workTree: Maybe[Directory]):
-  def checkout(ref: Text)(using Log): Unit throws GitError =
-    try sh"git checkout $ref".exec[ExitStatus]()
-    catch
-      case error: SystemPropertyError => throw GitError(GitError.Detail.GitNotOnPath)
-  
-  def push()(using Log, Internet): Unit throws GitError =
-    try sh"git --base-dir=${directory.path} push"
-    catch
-      case error: SystemPropertyError => throw GitError(GitError.Detail.GitNotOnPath)
-      case error: SystemPropertyError => throw GitError(GitError.Detail.GitNotOnPath)
+object GitRepo:
+  def apply(path: Path): GitRepo throws IoError | GitError =
+    if !path.exists() then throw GitError(GitError.Detail.RepoDoesNotExist)
+    if (path / p".git").exists() then GitRepo((path / p".git").as[Directory], path.as[Directory])
+    else GitRepo(path.as[Directory])
 
+case class GitRepo(gitDir: Directory, workTree: Maybe[Directory] = Unset):
+
+  val targetParams = workTree match
+    case Unset               => sh"--git-dir=${gitDir.path}"
+    case workTree: Directory => sh"--git-dir=${gitDir.path} --work-tree=${workTree.path}"
+
+  def checkout(ref: Text)(using Log, GitCommand, WorkingDirectory): Unit =
+    sh"$git $targetParams checkout $ref".exec[ExitStatus]()
+  
+  def push()(using Log, WorkingDirectory, Internet, CanThrow[GitError], GitCommand): Unit =
+    sh"$git $targetParams push"
+
+  def switch(branch: Branch)(using GitCommand, Log, WorkingDirectory): Unit =
+    sh"$git $targetParams switch $branch"
   
   def pull(): Unit = ()
   def commit(): Unit = ()
@@ -56,40 +66,42 @@ case class GitRepo(directory: Directory, workTree: Maybe[Directory]):
   def log(): Unit = ()
   def reflog(): Unit = ()
   def status(): Unit = ()
+    
   def diff(): Unit = ()
 
 enum Progress:
   case Receiving(complete: Double)
   case Resolving(complete: Double)
 
-object Git:
+private[nonagenarian] inline def git(using command: GitCommand): GitCommand = command
 
+object Git:
   def init
       [PathType: GenericPathReader]
       (targetPath: PathType, bare: Boolean = false)
-      (using log: Log, git: CanThrow[GitError], decoder: Decoder[Path])
+      (using Log, WorkingDirectory, CanThrow[GitError])
+      (using command: GitCommand, decoder: Decoder[Path])
       : GitRepo =
     try
       val bareOpt = if bare then sh"--bare" else sh""
       val target: Path = targetPath.fullPath.decodeAs[Path]
-      sh"git init $bareOpt $target".exec[ExitStatus]()
+      sh"$command init $bareOpt $target".exec[ExitStatus]()
       
       if bare then GitRepo(target.as[Directory], Unset)
       else GitRepo((target / p".git").as[Directory], target.as[Directory])
     catch
       case error: PathError           => throw GitError(GitError.Detail.InvalidRepoPath)
-      case error: SystemPropertyError => throw GitError(GitError.Detail.GitNotOnPath)
       case error: IoError             => throw GitError(GitError.Detail.InvalidRepoPath)
   
   def clone
       [PathType: GenericPathReader]
       (source: Text, targetPath: PathType)
-      (using Internet, Log, Decoder[Path], CanThrow[GitError])
+      (using Internet, WorkingDirectory, Log, Decoder[Path], CanThrow[GitError], GitCommand)
       : CloneProcess =
     
     try
       val target: Path = targetPath.fullPath.decodeAs[Path]
-      val process = sh"git clone --progress $source $target".fork[ExitStatus]()
+      val process = sh"$git clone --progress $source $target".fork[ExitStatus]()
       
       val progress =
         try process.stderr().map(_.uString).map(_.trim).collect:
@@ -109,14 +121,29 @@ object Git:
 
     catch
       case error: PathError           => throw GitError(GitError.Detail.InvalidRepoPath)
-      case error: SystemPropertyError => throw GitError(GitError.Detail.GitNotOnPath)
       case error: IoError             => throw GitError(GitError.Detail.InvalidRepoPath)
+
+object GitRef:
+  given Encoder[GitRef] = _.name
 
 trait GitRef:
   def name: Text
 
-case class GitTag(name: Text) extends GitRef
-case class GitBranch(name: Text) extends GitRef
+case class Tag(name: Text) extends GitRef
+case class Branch(name: Text) extends GitRef
+case class CommitHash(name: Text) extends GitRef
 
-case class GitCommit(bytes: Bytes) extends GitRef:
-  def name: Text = bytes.hex
+object GitCommand:
+  given AsParams[GitCommand] = _.file.path.fullname
+
+case class GitCommand(file: File)
+
+case class Commit
+    (commit: CommitHash, tree: CommitHash, parent: List[CommitHash], author: Text, committer: Text,
+        gpgsig: Text, message: List[Text])
+
+package gitCommands:
+  given environmentDefault(using working: WorkingDirectory, path: CanThrow[PathError], log: Log, io: CanThrow[IoError]): GitCommand^{working, path, log, io} =
+    val path: Path = sh"which git"()
+
+    GitCommand(path.as[File])
