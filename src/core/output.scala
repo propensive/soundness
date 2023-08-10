50f0a05be50e11e1211ae9a5fc3eb0df7abc92cf
Jon Pretty <jon.pretty@propensive.com>
2023-08-10 11:07:09 +0200

    Use Perforate, not Digression

--- src/core/output.scala
+++ src/core/output.scala
@@ -20,7 +20,7 @@ import gossamer.*
 import rudiments.*
 import fulminate.*
 import anticipation.*
-import digression.*
+import perforate.*
 import symbolism.*
 import contextual.*
 import spectacular.*
