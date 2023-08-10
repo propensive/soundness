8035dd0c2dae60949294e6617422469f36471fc7
Jon Pretty <jon.pretty@propensive.com>
2023-08-10 10:39:15 +0200

    Almost completely rewritten

--- src/core/perforate.scala
+++ src/core/perforate.scala
@@ -16,101 +16,186 @@
 
 package perforate
 
-import rudiments.*
-import digression.*
 import fulminate.*
+import rudiments.*
 
 import language.experimental.captureChecking
 
+import java.util.concurrent.atomic as juca
+
 @capability
-trait ErrorHandler[-ErrorType <: Error]:
+trait Raises[-ErrorType <: Error]:
   def record(error: ErrorType): Unit
-  def apply(): Unit
+  def abort(error: ErrorType): Nothing
+
+trait ErrorHandler[SuccessType]:
+  type Result
+  type Return
+  type Raiser
+
+  def raiser(label: boundary.Label[Result]): Raiser
+  def wrap(value: SuccessType): Result
+  def finish(value: Result): Return
 
 @capability
-class Aggregator[ErrorType <: Error](flow: ControlFlow[ErrorType])(using CanThrow[AggregateError[ErrorType]])
-extends ErrorHandler[ErrorType]:
-  private var collected: List[ErrorType] = Nil
-  def record(error: ErrorType): Unit = collected ::= error
-  def apply(): Unit = if !collected.isEmpty then throw AggregateError[ErrorType](collected.reverse)
-
-@capability
-class Thrower[ErrorType <: Error: CanThrow](flow: ControlFlow[ErrorType]) extends ErrorHandler[ErrorType]:
+class RaisesThrow[ErrorType <: Error, SuccessType]()(using CanThrow[ErrorType]) extends Raises[ErrorType]:
   def record(error: ErrorType): Unit = throw error
-  def apply(): Unit = ()
-
-case class UnexpectedSuccessError()
-extends Error(msg"the expression was expected to fail, but succeeded")
+  def abort(error: ErrorType): Nothing = throw error
 
 @capability
-class Capturer[ErrorType <: Error](flow: ControlFlow[ErrorType])(using CanThrow[UnexpectedSuccessError])
-extends ErrorHandler[ErrorType]:
-  def record(error: ErrorType): Unit =
-    boundary.break(error)(using flow.label.or(throw Mistake(msg"expected control flow value")))
-  def apply(): Unit = throw UnexpectedSuccessError()
+class RaisesAggregate
+    [ErrorType <: Error, SuccessType]
+    (label: boundary.Label[Either[AggregateError[ErrorType], SuccessType]])
+    (using recovery: Recovery[AggregateError[ErrorType], SuccessType])
+extends Raises[ErrorType]:
 
-trait Recovery[-ErrorType <: Error, +ResultType]:
-  def recover(error: ErrorType): ResultType
-
-object Perforate:
-  opaque type ControlFlow[BreakType] = Maybe[boundary.Label[BreakType]]
-
-  object ControlFlow:
-    def apply[BreakType](label: Maybe[boundary.Label[BreakType]]): ControlFlow[BreakType] = label
+  private val collected: juca.AtomicReference[List[ErrorType]] = juca.AtomicReference(Nil)
   
-  extension [BreakType](flow: ControlFlow[BreakType]) def label: Maybe[boundary.Label[BreakType]] = flow
+  def record(error: ErrorType): Unit = collected.getAndUpdate(error :: _.nn)
+  def abort(error: ErrorType): Nothing = boundary.break(Left(AggregateError(error :: collected.get().nn)))(using label)
 
-export Perforate.ControlFlow
+@capability
+class RaisesErrorResult
+    [ErrorType <: Error, SuccessType]
+    (label: boundary.Label[Either[ErrorType, SuccessType]])
+    (using Raises[UnexpectedSuccessError[SuccessType]])
+extends Raises[ErrorType]:
+
+  def record(error: ErrorType): Unit = boundary.break(Left(error))(using label)
+  def abort(error: ErrorType): Nothing = boundary.break(Left(error))(using label)
+
+@capability
+class RaisesMaybe[ErrorType <: Error, SuccessType](label: boundary.Label[Maybe[SuccessType]])
+extends Raises[ErrorType]:
+  type Result = Maybe[SuccessType]
+  type Return = Maybe[SuccessType]
+  
+  def record(error: ErrorType): Unit = boundary.break(Unset)(using label)
+  def abort(error: ErrorType): Nothing = boundary.break(Unset)(using label)
+
+class ThrowErrors[ErrorType <: Error, SuccessType]()(using CanThrow[ErrorType])
+extends ErrorHandler[SuccessType]:
+  type Result = Either[ErrorType, SuccessType]
+  type Return = SuccessType
+  type Raiser = RaisesThrow[ErrorType, SuccessType]
+ 
+  def raiser(label: boundary.Label[Result]): Raiser = RaisesThrow()
+  def wrap(block: SuccessType): Result = Right(block)
+  
+  def finish(value: Result): Return = value match
+    case Left(error)  => throw error
+    case Right(value) => value
+
+class Validate
+    [ErrorType <: Error, SuccessType]
+    (using Raises[AggregateError[ErrorType]], Recovery[AggregateError[ErrorType], SuccessType])
+extends ErrorHandler[SuccessType]:
+  type Result = Either[AggregateError[ErrorType], SuccessType]
+  type Return = SuccessType
+  type Raiser = RaisesAggregate[ErrorType, SuccessType]
+  
+  def raiser(label: boundary.Label[Result]): Raiser = RaisesAggregate(label)
+  def wrap(fn: SuccessType): Either[AggregateError[ErrorType], SuccessType] = Right(fn)
+  
+  def finish(value: Result): Return = value match
+    case Left(error)  => raise[SuccessType, AggregateError[ErrorType]](error)
+    case Right(value) => value
+
+class Capture
+    [ErrorType <: Error, SuccessType]
+    (using handler: Raises[UnexpectedSuccessError[SuccessType]])
+extends ErrorHandler[SuccessType]:
+  type Result = Either[ErrorType, SuccessType]
+  type Return = ErrorType
+  type Raiser = RaisesErrorResult[ErrorType, SuccessType]
+  
+  def raiser(label: boundary.Label[Result]): Raiser = RaisesErrorResult(label)
+  def wrap(block: SuccessType): Either[ErrorType, SuccessType] = Right(block)
+  
+  def finish(value: Either[ErrorType, SuccessType]): ErrorType = value match
+    case Left(error)  => error
+    case Right(value) => abort(UnexpectedSuccessError(value))
+
+class SafeHandler[ErrorType <: Error, SuccessType]() extends ErrorHandler[SuccessType]:
+  type Result = Maybe[SuccessType]
+  type Return = Maybe[SuccessType]
+  type Raiser = RaisesMaybe[ErrorType, SuccessType]
+  
+  def raiser(label: boundary.Label[Result]): RaisesMaybe[ErrorType, SuccessType] = RaisesMaybe(label)
+  def wrap(block: SuccessType): Maybe[SuccessType] = block
+  def finish(value: Maybe[SuccessType]): Maybe[SuccessType] = value
+
+trait Recovery[-ErrorType <: Error, +SuccessType]:
+  def recover(error: ErrorType): SuccessType
 
 def raise
-    [ResultType, ErrorType <: Error]
+    [SuccessType, ErrorType <: Error]
     (error: ErrorType)
-    (using handler: ErrorHandler[ErrorType], recovery: Recovery[error.type, ResultType])
-    : ResultType =
+    (using handler: Raises[ErrorType], recovery: Recovery[ErrorType, SuccessType])
+    : SuccessType =
   handler.record(error)
   recovery.recover(error)
 
 def raise
-    [ResultType, ErrorType <: Error]
-    (error: ErrorType)(ersatz: => ResultType)
-    (using handler: ErrorHandler[ErrorType])
-    : ResultType =
+    [SuccessType, ErrorType <: Error]
+    (error: ErrorType)(ersatz: => SuccessType)
+    (using handler: Raises[ErrorType])
+    : SuccessType =
   handler.record(error)
   ersatz
 
+def abort[SuccessType, ErrorType <: Error](error: ErrorType)(using handler: Raises[ErrorType]): Nothing =
+  handler.abort(error)
+
+def safely
+    [ErrorType <: Error]
+    (using DummyImplicit)
+    [SuccessType]
+    (block: RaisesMaybe[ErrorType, SuccessType] ?=> SuccessType)
+    : Maybe[SuccessType] =
+  
+  handle(SafeHandler[ErrorType, SuccessType])(block)
+
+def throwErrors
+    [ErrorType <: Error]
+    (using CanThrow[ErrorType])
+    [SuccessType]
+    (block: RaisesThrow[ErrorType, SuccessType] ?=> SuccessType)
+    : Maybe[SuccessType] =
+  
+  handle(ThrowErrors[ErrorType, SuccessType])(block)
+
 def validate
     [ErrorType <: Error]
-    (using aggregate: CanThrow[AggregateError[ErrorType]])
-    [ResultType]
-    (fn: ErrorHandler[ErrorType] ?=> ResultType)
-    : ResultType =
-  
-  boundary:
-    val aggregator = Aggregator[ErrorType](ControlFlow(Unset))
-    fn(using aggregator).tap(aggregator().waive)
+    (using raise: Raises[AggregateError[ErrorType]])
+    [SuccessType]
+    (block: RaisesAggregate[ErrorType, SuccessType] ?=> SuccessType)
+    (using recovery: Recovery[AggregateError[ErrorType], SuccessType])
+    : SuccessType =
 
-def attempt
-    [ErrorType <: Error]
-    (using error: CanThrow[ErrorType])
-    [ResultType]
-    (fn: ErrorHandler[ErrorType] ?=> ResultType)
-    : ResultType =
-  
-  boundary: label ?=>
-    val thrower = Thrower[ErrorType](ControlFlow(Unset))
-    fn(using thrower).tap(thrower().waive)
+  handle(Validate[ErrorType, SuccessType]())(block)
 
 def capture
     [ErrorType <: Error]
-    (using error: CanThrow[UnexpectedSuccessError])
-    [ResultType]
-    (fn: ErrorHandler[ErrorType] ?=> ResultType)
+    (using DummyImplicit)
+    [SuccessType]
+    (block: RaisesErrorResult[ErrorType, SuccessType] ?=> SuccessType)
+    (using raise: Raises[UnexpectedSuccessError[SuccessType]])
     : ErrorType =
   
-  boundary: label ?=>
-    val capturer = Capturer[ErrorType](ControlFlow(label))
-    fn(using capturer)
-    throw UnexpectedSuccessError()
+  handle(Capture[ErrorType, SuccessType])(block)
 
-package errorHandlers:
-  given throwAnything: ErrorHandler[Error] = Thrower[Error](ControlFlow(Unset))(using unsafeExceptions.canThrowAny)
+def handle
+    [SuccessType, HandlerType <: ErrorHandler[SuccessType]]
+    (handler: HandlerType^)
+    (block: handler.Raiser ?=> SuccessType)
+    : handler.Return =
+  handler.finish:
+    boundary: label ?=>
+      handler.wrap(block(using handler.raiser(label)))
+
+case class AggregateError[+ErrorType <: Error](errors: List[ErrorType])
+extends Error(AsMessage.listMessage.message(errors.map(_.message)))
+
+case class UnexpectedSuccessError[ResultType](result: ResultType)
+extends Error(msg"the expression was expected to fail, but succeeded")
