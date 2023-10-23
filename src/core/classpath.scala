d0683ad6ad4677fc12eab54129e548c24380f8e7
Jon Pretty <jon.pretty@propensive.com>
2023-10-23 16:09:09 +0200

    Handling for classloaders

--- src/core/classpath.scala
+++ src/core/classpath.scala
@@ -1,10 +1,25 @@
 package hellenism
 
 import rudiments.*
+import serpentine.*
+import gossamer.*
+import anticipation.*
 
 import scala.quoted.*
 
 import java.net as jn
+
+sealed trait ClasspathEntry
+
+object ClasspathEntry:
+  case class Directory(path: Text) extends ClasspathEntry:
+    def apply[DirectoryType: SpecificDirectory](): DirectoryType = SpecificDirectory(path)
+
+  case class Jarfile(path: Text) extends ClasspathEntry:
+    def apply[FileType: SpecificFile](): FileType = SpecificFile(path)
+
+  case class Other(path: Text) extends ClasspathEntry
+  case object JavaRuntime extends ClasspathEntry
 
 object Classloader:
   inline def apply[ClassType <: AnyKind]: Classloader = ClassRef[ClassType].classloader
@@ -13,6 +28,18 @@ class Classloader(val java: ClassLoader):
   protected def urlClassloader: Maybe[jn.URLClassLoader] = java match
     case java: jn.URLClassLoader => java
     case _                       => Unset
+  
+  def classpath: List[ClasspathEntry] =
+    urlClassloader.mm(_.getURLs.nn.to(List)).or(Nil).map: url =>
+      url.nn.getProtocol.nn.tt match
+        case t"jrt" =>
+          ClasspathEntry.JavaRuntime
+        
+        case t"file" =>
+          val path: Text = url.nn.getPath.nn.tt
+          if path.ends(t"/") then ClasspathEntry.Directory(path) else ClasspathEntry.Jarfile(path)
+        
+        case _ => ClasspathEntry.Other(url.toString.tt)
 
 object Hellenism extends Hellenism2:
 
