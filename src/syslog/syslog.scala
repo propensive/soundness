c2399115494f7aa34bf854269972160473f099cb
Jon Pretty <jon.pretty@propensive.com>
2023-11-03 23:03:16 +0100

    Simplify `syslog` code

--- src/syslog/syslog.scala
+++ src/syslog/syslog.scala
@@ -22,7 +22,6 @@ import parasite.*
 import anticipation.*
 import turbulence.*
 import perforate.*
-import hieroglyph.*, charEncoders.utf8
 
 case class Syslog(tag: Text)
 
@@ -30,11 +29,9 @@ object Syslog:
 
   given (using Monitor): Appendable[Syslog, Text] = (syslog, stream) =>
     safely:
-      Async:
-        import workingDirectories.default
-        given Log = SilentLog
-        val process: Process["logger", Unit] = sh"logger --tag ${syslog.tag}".fork[Unit]()
-        process.stdin(stream)
+      import workingDirectories.default
+      import logging.silent
+      stream.appendTo(sh"logger --tag ${syslog.tag}".fork[Unit]())
 
 package logging:
   given syslog(using realm: Realm, monitor: Monitor): Log = Log:
