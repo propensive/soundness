1d84851ded7b2cde6b0c9779a5285b828a86c550
Jon Pretty <jon.pretty@propensive.com>
2023-11-08 06:45:28 +0100

    Remove compile warnings in macro

--- src/core/eucalyptus.scala
+++ src/core/eucalyptus.scala
@@ -45,10 +45,12 @@ object Eucalyptus:
 
   def route(routes: Expr[PartialFunction[Entry, Any]], monitor: Expr[Monitor])(using Quotes): Expr[Log] =
     import quotes.reflect.*
+
+    def invalidRoutes(): Nothing = fail(msg"the routes must be specified as one or more case clauses")
     
     val count: Int = routes.asTerm match
-      case Inlined(_, _, Block(List(defDef), term)) => defDef match
-        case DefDef(ident, scrutineeType, returnType, Some(Match(matchId, caseDefs))) => caseDefs.length
+      case Inlined(_, _, Block(List(DefDef(_, _, _, Some(Match(_, caseDefs)))), _)) => caseDefs.length
+      case _                                                                        => invalidRoutes()
     
     '{
       val loggers: Array[Logger | Null] = new Array(${Expr(count)})
@@ -59,7 +61,7 @@ object Eucalyptus:
             case Inlined(_, _, Block(List(defDef), term)) => defDef match
               case DefDef(ident, scrutineeType, returnType, Some(Match(matchId, caseDefs))) =>
                 val caseDef = caseDefs(index) match
-                  case CaseDef(pattern, guard, target) => target.asExpr match
+                  case CaseDef(pattern, guard, target) => (target.asExpr: @unchecked) match
                     case '{$target: targetType} =>
                       def typeName = TypeRepr.of[targetType].show
                       
@@ -80,6 +82,12 @@ object Eucalyptus:
                     List(caseDef))))
                 
                 Block(List(definition), term).asExprOf[PartialFunction[Entry, Any]]
+               
+              case _ =>
+                invalidRoutes()
+
+            case _ =>
+              invalidRoutes()
 
           def recur(index: Int, expr: Expr[Unit]): Expr[Unit] = if index >= count then expr else '{
             $expr
