e52089d84c5dc5ed467fddeb0292a985dde60c75
Jon Pretty <jon.pretty@propensive.com>
2023-11-21 21:50:03 +0100

    `vouch` instead of `avow`

--- src/core/diff.scala
+++ src/core/diff.scala
@@ -144,8 +144,8 @@ case class Diff[ElemType](edits: Edit[ElemType]*):
         if inss.length == dels.length && inss.length <= subSize
         then dels.zip(inss).map { (del, ins) => Sub(del.left, ins.right, del.value, ins.value) }
         else
-          val delsSeq = dels.map(_.value.avow(using Unsafe)).to(IndexedSeq)
-          val inssSeq = inss.map(_.value.avow(using Unsafe)).to(IndexedSeq)
+          val delsSeq = dels.map(_.value.vouch(using Unsafe)).to(IndexedSeq)
+          val inssSeq = inss.map(_.value.vouch(using Unsafe)).to(IndexedSeq)
           
           diff(delsSeq, inssSeq, similar).edits.map:
             case Del(index, _) => Del(dels(index).left, dels(index).value)
