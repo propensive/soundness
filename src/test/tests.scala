25968d576e9f06b5bc54aab7cf1487e5c6c71241
Jon Pretty <jon.pretty@propensive.com>
2023-12-05 12:34:46 +0100

    Added some Yossarian tests

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -18,6 +18,7 @@ package escapade
 
 import probably.*
 import yossarian.*
+import iridescence.*, colors.{Red, Yellow}
 import spectacular.*
 import rudiments.*
 import perforate.*
@@ -54,15 +55,15 @@ object Tests extends Suite(t"Escapade tests"):
       .assert(_ == t"42[text]")
 
     suite(t"Escaping tests"):
-      test(t"Check that an escaped tab is a tab"):
+      test(t"an escaped tab is a tab"):
         e"|\t|".plain
       .assert(_.length == 3)
       
-      test(t"Check that a unicode value is converted"):
+      test(t"a unicode value is converted"):
         e"|\u0040|".plain
       .assert(_ == t"|@|")
       
-      test(t"Check that a newline is converted correctly"):
+      test(t"a newline is converted correctly"):
         e"\n".plain
       .assert(_ == t"\n")
 
@@ -70,31 +71,53 @@ object Tests extends Suite(t"Escapade tests"):
       import errorHandlers.throwUnsafely
       val pty = Pty(80, 1)
       val boldSample = pty.consume(e"This text is $Bold(bold).".render).buffer
-      println(e"start$Bold(bold$Italic(bold-italic)unitalic)end".render)
-      println(e"start$Bold(bold$Italic(bold-italic)unitalic)end".render.debug)
       val boldItalicSample = pty.consume(e"start$Bold(bold$Italic(bold-italic)unitalic)end".render).buffer
+      val boldItalicSample2 = pty.consume(e"start$Bold($Italic(bold-italic))end".render).buffer
       
-      test(t"Check that bold text is bold"):
+      test(t"bold text is bold"):
         boldSample.find(t"bold").vouch(using Unsafe).styles
       .assert(_.forall(_.bold))
       
-      test(t"Check that text before bold text is not bold"):
+      test(t"text before bold text is not bold"):
         boldSample.find(t"This text is ").vouch(using Unsafe).styles
       .assert(_.forall(!_.bold))
       
-      test(t"Check that text after bold text is not bold"):
+      test(t"text after bold text is not bold"):
         boldSample.find(t".").vouch(using Unsafe).styles
       .assert(_.forall(!_.bold))
 
-      test(t"Check that nested bold/italic is both"):
+      test(t"nested bold/italic is both"):
         boldItalicSample.find(t"bold-italic").vouch(using Unsafe).styles
       .assert(_.forall { style => style.bold && style.italic })
       
-      test(t"Check that nested italic is removed but not bold"):
-        println(boldItalicSample.find(t"unitalic").vouch(using Unsafe).styles.debug)
+      test(t"nested italic is removed but not bold"):
         boldItalicSample.find(t"unitalic").vouch(using Unsafe).styles
       .assert(_.forall { style => style.bold && !style.italic })
       
-      test(t"Check that nested non-bold, non-italic text is neither"):
+      test(t"nested non-bold, non-italic text is neither"):
         boldItalicSample.find(t"end").vouch(using Unsafe).styles
       .assert(_.forall { style => !style.bold && !style.italic })
+      
+      test(t"nested non-bold, non-italic text is neither"):
+        boldItalicSample.find(t"end").vouch(using Unsafe).styles
+      .assert(_.forall { style => !style.bold && !style.italic })
+      
+      test(t"nested bold/italic (without intermediate characters) is both"):
+        boldItalicSample2.find(t"bold-italic").vouch(using Unsafe).styles
+      .assert(_.forall { style => style.bold && style.italic })
+      
+      test(t"normal text following doubly-nested text (without intermediate characters) is normal"):
+        boldItalicSample2.find(t"end").vouch(using Unsafe).styles
+      .assert(_.forall(_ == Style()))
+
+      test(t"double color change uses latter"):
+        pty.consume(e"$Red($Yellow(yellow))".render).buffer.find(t"yellow").vouch(using Unsafe).styles
+      .assert(_.forall(_.foreground == Yellow))
+      
+      test(t"double color change and removal of nested uses former"):
+        pty.consume(e"$Red($Yellow(yellow)red)".render).buffer.find(t"red").vouch(using Unsafe).styles
+      .assert(_.forall(_.foreground == Red))
+      
+      test(t"double color change and removal uses default"):
+        pty.consume(e"$Red($Yellow(yellow)red)default".render).buffer.find(t"default").vouch(using Unsafe).styles
+      .assert(_.forall(_.foreground == Rgb24(255, 255, 255)))
