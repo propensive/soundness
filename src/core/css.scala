46571632cbeb4f31f4e165a539764ab7150261fc
Jon Pretty <jon.pretty@propensive.com>
2023-12-08 10:39:44 +0100

    Rename `Maybe` to `Optional`

--- src/core/css.scala
+++ src/core/css.scala
@@ -57,12 +57,12 @@ object To extends Dynamic:
     ${Cataclysm.keyframe('{"to"}, 'properties)}
 
 case class FontFace
-    (ascentOverride: Maybe[Text] = Unset, descentOverride: Maybe[Text] = Unset,
-        fontDisplay: Maybe[Text] = Unset, fontFamily: Maybe[Text] = Unset,
-        fontStretch: Maybe[Text] = Unset, fontStyle: Maybe[Text] = Unset,
-        fontWeight: Maybe[Text] = Unset, fontVariationSettings: Maybe[Text] = Unset,
-        lineGapOverride: Maybe[Text] = Unset, sizeAdjust: Maybe[Text] = Unset,
-        src: Maybe[Text] = Unset, unicodeRange: Maybe[Text] = Unset)
+    (ascentOverride: Optional[Text] = Unset, descentOverride: Optional[Text] = Unset,
+        fontDisplay: Optional[Text] = Unset, fontFamily: Optional[Text] = Unset,
+        fontStretch: Optional[Text] = Unset, fontStyle: Optional[Text] = Unset,
+        fontWeight: Optional[Text] = Unset, fontVariationSettings: Optional[Text] = Unset,
+        lineGapOverride: Optional[Text] = Unset, sizeAdjust: Optional[Text] = Unset,
+        src: Optional[Text] = Unset, unicodeRange: Optional[Text] = Unset)
 extends CssStylesheet.Item:
   
   def text: Text =
