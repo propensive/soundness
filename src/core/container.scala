1b67f8154909a8f06a7ec05bf8a2cb61d7fa524f
Jon Pretty <jon.pretty@propensive.com>
2023-12-19 22:24:51 +0100

    Work in progress

--- src/core/container.scala
+++ src/core/container.scala
@@ -2,10 +2,21 @@ package superlunary
 
 import jacinta.*
 import gossamer.*
+import inimitable.*
 import anticipation.*
 import perforate.*, errorHandlers.throwUnsafely
 
 case class Foo(x: Int, y: String)
 
 @main
-def run(): Unit = println(container[Foo, String]('{ foo => Thread.sleep(2000); foo.y.length.toString+"!" })(Foo(12, "hello")))
+def run(): Unit =
+
+  given Baz = new Baz {}
+  val fn = container[Foo, String]('{ foo =>
+    s"This is running on $jvmInstanceId.")
+  })
+  
+  println(s"This is running on $jvmInstanceId")
+
+  println(fn(Foo(11, "hello")))
+  println(fn(Foo(13, "hello world!")))
--- src/core/sandbox.scala
+++ src/core/sandbox.scala
@@ -6,6 +6,7 @@ import spectacular.*
 import guillotine.*
 import turbulence.*
 import rudiments.*
+import vacuous.*
 import ambience.*, systemProperties.jvm
 import gossamer.*
 import inimitable.*
@@ -16,77 +17,42 @@ import hellenism.*
 
 import scala.compiletime.*
 import scala.quoted.*, staging.*
+import scala.reflect.Selectable.reflectiveSelectable
 
 trait Container[+OutputType]
 
+object Baz:
+  given Baz = new Baz {}
+
+trait Baz
 
 class Sandbox[InputType, OutputType](expr: Quotes ?=> Expr[InputType => OutputType]):
 
   def body(using Quotes): Expr[InputType => OutputType] = expr
 
-// object Macros:
-//   def container
-//       [ThisType, InputType, OutputType]
-//       (input: Expr[InputType])
-//       (using Quotes, Type[InputType], Type[OutputType])(using thisType: Type[ThisType])
-//       : Expr[OutputType] =
-//     import quotes.reflect.*
-
-    
-//     val className = TypeRepr.of(using thisType).typeSymbol.fullName
-//     val dest = Uuid().show
-//     val settings: Compiler.Settings = Compiler.Settings.make(Some("/home/propensive/tmp/staging/"+dest), List(/*"-scalajs"*/))
-//     given compiler: Compiler = Compiler.make(Macros.getClass.nn.getClassLoader.nn)(using settings)
-
-//     TypeRepr.of(using thisType)
-
-//     val sandbox = Class.forName(className).nn.getField("MODULE$").nn.get(null).asInstanceOf[Sandbox[InputType, OutputType]]
-
-//     val body: Expr[InputType => OutputType] = sandbox.body
-
-//     val serializer: Expr[JsonSerializer[InputType]] = Expr.summon[JsonSerializer[InputType]].get
-
-//     val x = run:
-//       '{
-//         final class Abc():
-//           var result: Any = 0
-//           def main(args: Array[String]): Unit = ()
-//             println($serializer)
-//             //$body(???)
-//       }
-
-//     '{
-//        val text: Text = $input.json(using $serializer).show
-//        println(text)
-//        $body($input)
-//     }
-
-
 inline def container
     [InputType: JsonSerializer: JsonDeserializer, OutputType]
     (body: Quotes ?=> Expr[InputType => OutputType])
-    (input: InputType)
-    : OutputType =
+    : InputType => OutputType =
 
   val dest = Uuid().show
-  val settings: Compiler.Settings = Compiler.Settings.make(Some("/home/propensive/tmp/staging/"+dest), List(/*"-scalajs"*/))
+  val settings: Compiler.Settings = Compiler.Settings.make(Some("/home/propensive/tmp/staging/"+dest), List("-scalajs"))
   given compiler: Compiler = Compiler.make(getClass.nn.getClassLoader.nn)(using settings)
 
 
-  run: (quotes: Quotes) ?=>
+
+  try run: (quotes: Quotes) ?=>
     import quotes.reflect.*
+    println("compiling...")
     val entries: List[Text] = new Classloader(getClass.nn.getClassLoader.nn).classpath.or(???).entries.collect:
       case ClasspathEntry.Jarfile(file)        => file
       case ClasspathEntry.Directory(directory) => directory
     
     val classpath: Text = (entries :+ t"/home/propensive/tmp/staging/$dest").join(t":")
-    val data = Expr(input.json.show)
 
-    '{
+    '{ (input: InputType) =>
       object Eex:
         def run(input: String): String =
-          //val json: Json = safely(Json.parse(args(0).tt)).or(???)
-          //val input: InputType = safely(json.as[InputType](using $deserializer)).or(???)
           unsafely: (raises) ?=>
             import hieroglyph.charEncoders.utf8
             val readable = summonInline[Readable[Text, Bytes]]
@@ -100,13 +66,14 @@ inline def container
             MinimalSerializer.serialize($body(param).json(using serializer).root).s
       
       val cp: Text = ${Expr(classpath)}
-      val d: Text = $data
       
       unsafely: raises ?=>
         import hieroglyph.charEncoders.utf8
         val wd: WorkingDirectory = workingDirectories.jvm
         val log: Log[Text] = logging.silent
-        val cmd = Command(t"java", t"-classpath", cp, t"superlunary.SandboxRunner", t"Generated$$Code$$From$$Quoted$$Eex$$2$$", d)
+        val serializer = summonInline[JsonSerializer[InputType]]
+        val in = MinimalSerializer.serialize(input.json(using serializer).root).s
+        val cmd = Command(t"java", t"-classpath", cp, t"superlunary.SandboxRunner", t"Generated$$Code$$From$$Quoted$$Eex$$2$$", in)
         val output = cmd.exec[Text]()(using wd, log)
 
         val readable = summonInline[Readable[Text, Bytes]]
@@ -114,6 +81,10 @@ inline def container
         val deserializer = summonInline[JsonDeserializer[OutputType]]
         json.as[OutputType](using deserializer)
     }
+  
+  catch case error: Throwable =>
+    println("Thrown")
+    ???
 
 object SandboxRunner:
   def main(args: Array[String]): Unit =
@@ -121,5 +92,4 @@ object SandboxRunner:
     val params = args(1)
     val cls = Class.forName(className).nn
     val runnable = cls.newInstance()
-    import scala.reflect.Selectable.reflectiveSelectable
     println(runnable.asInstanceOf[{ def run(params: String): String }].run(params))
