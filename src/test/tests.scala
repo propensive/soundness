820c83e38f618d612609199e923b881dc48840bf
propensive <propensive@users.noreply.github.com>
2024-01-01 18:21:21 +0000

    Apply automatic changes

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -1,5 +1,5 @@
 /*
-    Escritoire, version [unreleased]. Copyright 2023 Jon Pretty, Propensive OÜ.
+    Escritoire, version [unreleased]. Copyright 2024 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
