3d58b2396033d29a9a64f82908fd7b44b2e18a67
propensive <propensive@users.noreply.github.com>
2024-01-01 18:28:22 +0000

    Apply automatic changes

--- src/core/symbolism.scala
+++ src/core/symbolism.scala
@@ -1,5 +1,5 @@
 /*
-    Symbolism, version [unreleased]. Copyright 2023 Jon Pretty, Propensive OÜ.
+    Symbolism, version [unreleased]. Copyright 2024 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
