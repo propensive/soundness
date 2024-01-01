74e264ff77a0528ee7ce8b16205bb026627e33cd
propensive <propensive@users.noreply.github.com>
2024-01-01 18:20:22 +0000

    Apply automatic changes

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -1,5 +1,5 @@
 /*
-    Caesura, version [unreleased]. Copyright 2023 Jon Pretty, Propensive OÜ.
+    Caesura, version [unreleased]. Copyright 2024 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
