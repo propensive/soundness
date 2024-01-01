c0edbd7b01001741dcd9895dd227f767cb175f0f
propensive <propensive@users.noreply.github.com>
2024-01-01 18:23:05 +0000

    Apply automatic changes

--- src/core/decimalizer.scala
+++ src/core/decimalizer.scala
@@ -1,5 +1,5 @@
 /*
-    Gossamer, version [unreleased]. Copyright 2023 Jon Pretty, Propensive OÜ.
+    Gossamer, version [unreleased]. Copyright 2024 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
