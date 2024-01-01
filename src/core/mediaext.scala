5a7257820ce8bd0329e71f3be61a2c91530a2a30
propensive <propensive@users.noreply.github.com>
2024-01-01 18:22:40 +0000

    Apply automatic changes

--- src/core/mediaext.scala
+++ src/core/mediaext.scala
@@ -1,5 +1,5 @@
 /*
-    Gesticulate, version [unreleased]. Copyright 2023 Jon Pretty, Propensive OÜ.
+    Gesticulate, version [unreleased]. Copyright 2024 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
