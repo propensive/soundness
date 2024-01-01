4aa432484ae8c7e314f2b571b3ac09bf8be83c42
propensive <propensive@users.noreply.github.com>
2024-01-01 18:20:45 +0000

    Apply automatic changes

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -1,5 +1,5 @@
 /*
-    Dendrology, version [unreleased]. Copyright 2023 Jon Pretty, Propensive OÜ.
+    Dendrology, version [unreleased]. Copyright 2024 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
