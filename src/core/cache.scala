54e3cf8f2f072698e07975b4dbc4601f8a335aec
propensive <propensive@users.noreply.github.com>
2024-01-01 18:20:18 +0000

    Apply automatic changes

--- src/core/cache.scala
+++ src/core/cache.scala
@@ -1,5 +1,5 @@
 /*
-    Camouflage, version [unreleased]. Copyright 2023 Jon Pretty, Propensive OÜ.
+    Camouflage, version [unreleased]. Copyright 2024 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
