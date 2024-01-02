74bd4c4d6198e39ed2217be5f4c2c9468aac53cd
Jon Pretty <jon.pretty@propensive.com>
2024-01-02 23:04:24 +0100

    Reimplemented derivation with Wisteria

--- src/core/json.scala
+++ src/core/json.scala
@@ -24,6 +24,7 @@ import gossamer.*
 import anticipation.*
 import merino.*
 import hieroglyph.*
+import wisteria2.*
 import spectacular.*
 import perforate.*
 
@@ -165,7 +166,7 @@ object JsonSerializer extends FallbackJsonSerializer:
   given JsonSerializer[Short] = short => JsonAst(short.toLong)
   given JsonSerializer[Boolean] = JsonAst(_)
 
-  //given [T](using canon: Canonical[T]): JsonSerializer[T] = v => JsonAst(canon.serialize(v).s)
+  given [T](using encoder: Encoder[T]): JsonSerializer[T]^{encoder} = v => JsonAst(encoder.encode(v).s)
   
   given (using jsonAccess: Raises[JsonAccessError]): JsonSerializer[Json]^{jsonAccess} = _.root
   
@@ -184,35 +185,6 @@ object JsonSerializer extends FallbackJsonSerializer:
       case None        => JsonAst(null)
       case Some(value) => summon[JsonSerializer[ValueType]].write(value)
 
-  private transparent inline def deriveProduct
-      [LabelsType <: Tuple]
-      (tuple: Tuple, labels: Array[String], values: Array[JsonAst], index: Int)
-      : (Array[String], Array[JsonAst]) =
-    inline tuple match
-      case EmptyTuple => (labels, values)
-      case cons: (? *: ?) => cons match
-        case head *: tail => inline erasedValue[LabelsType] match
-          case _: (headLabel *: tailLabels) => inline valueOf[headLabel].asMatchable match
-            case label: String =>
-              labels(index) = label
-              values(index) = summonInline[JsonSerializer[head.type]].write(head)
-              deriveProduct[tailLabels](tail, labels, values, index + 1)
-
-  private transparent inline def deriveSum
-      [TupleType <: Tuple, DerivedType, LabelsType <: Tuple]
-      (ordinal: Int)(using Raises[JsonAccessError])
-      : JsonSerializer[DerivedType] =
-    inline erasedValue[TupleType] match
-      case _: (head *: tail) => inline erasedValue[LabelsType] match
-        case _: (headLabel *: tailLabel) =>
-          if ordinal == 0 then inline valueOf[headLabel].asMatchable match
-            case label: String =>
-              summonInline[JsonSerializer[head]].tag(label).asInstanceOf[JsonSerializer[DerivedType]]
-          else deriveSum[tail, DerivedType, tailLabel](ordinal - 1)
-      
-      case _ =>
-        throw Mistake(msg"could not match subtype in its apparent coproduct type")
-
   inline given derived
       [DerivationType](using mirror: Mirror.Of[DerivationType])(using Raises[JsonAccessError])
       : JsonSerializer[DerivationType] =
@@ -220,16 +192,17 @@ object JsonSerializer extends FallbackJsonSerializer:
       case given Mirror.ProductOf[DerivationType & Product] => (value: DerivationType) =>
         (value.asMatchable: @unchecked) match
           case value: Product =>
-            val labels: Array[String] = new Array(value.productArity)
-            val values: Array[JsonAst] = new Array(value.productArity)
+            val labels: IArray[Text] = Derivation.productOf[DerivationType & Product, JsonSerializer](value)(label)
             
-            deriveProduct[mirror.MirroredElemLabels](Tuple.fromProductTyped(value), labels, values,
-                0).asInstanceOf[JsonAst]
+            val values: IArray[JsonAst] = Derivation.productOf[DerivationType & Product, JsonSerializer](value):
+              typeclass.write(param)
+
+            (labels, values).asInstanceOf[JsonAst]
     
       case sumMirror: Mirror.SumOf[DerivationType] =>
         (value: DerivationType) =>
-          deriveSum[sumMirror.MirroredElemTypes, DerivationType,
-              sumMirror.MirroredElemLabels](sumMirror.ordinal(value)).write(value)
+          Derivation.sumOf[DerivationType, JsonSerializer](value):
+            typeclass.tag(label).write(value)
 
 trait JsonSerializer[-ValueType]:
   def omit(value: ValueType): Boolean = false
@@ -238,15 +211,18 @@ trait JsonSerializer[-ValueType]:
   def contraMap[ValueType2](fn: ValueType2 => ValueType): JsonSerializer[ValueType2]^{this, fn} =
     (value: ValueType2) => fn.andThen(write)(value)
 
-  def tag(label: String)(using jsonAccess: Raises[JsonAccessError]): JsonSerializer[ValueType]^{jsonAccess} = (value: ValueType) =>
+  def tag(label: Text)(using jsonAccess: Raises[JsonAccessError]): JsonSerializer[ValueType]^{jsonAccess} = (value: ValueType) =>
     val (keys, values) = write(value).obj
-    (keys :+ "_type", values :+ label).asInstanceOf[JsonAst]
+    (keys :+ "_type", values :+ label.s).asInstanceOf[JsonAst]
 
 trait FallbackJsonDeserializer:
-  given maybe[T](using reader: JsonDeserializer[T]^): JsonDeserializer[Optional[T]]^{reader} =
-    new JsonDeserializer[Optional[T]]:
-      def read(value: JsonAst, missing: Boolean): Optional[T] =
+  given maybe[ValueType](using reader: JsonDeserializer[ValueType]^): JsonDeserializer[Optional[ValueType]]^{reader} =
+    new JsonDeserializer[Optional[ValueType]]:
+      def read(value: JsonAst, missing: Boolean): Optional[ValueType] =
         if missing then Unset else reader.read(value, false)
+  
+  given decoder[T](using jsonAccess: Raises[JsonAccessError], decoder: Decoder[T]): JsonDeserializer[T]^{jsonAccess, decoder} =
+    (v, missing) => decoder.decode(v.string)
 
 object JsonDeserializer extends FallbackJsonDeserializer:
   given jsonAst(using jsonAccess: Raises[JsonAccessError]): JsonDeserializer[JsonAst]^{jsonAccess} = (value, missing) => value
@@ -261,8 +237,6 @@ object JsonDeserializer extends FallbackJsonDeserializer:
   given string(using jsonAccess: Raises[JsonAccessError]): JsonDeserializer[String]^{jsonAccess} = (value, missing) => value.string.s
   given boolean(using jsonAccess: Raises[JsonAccessError]): JsonDeserializer[Boolean]^{jsonAccess} = (value, missing) => value.boolean
   
-  //given [T](using canon: Canonical[T]): JsonDeserializer[T] = v => canon.deserialize(v.string)
-
   given option[T](using reader: JsonDeserializer[T]^)(using Raises[JsonAccessError]): JsonDeserializer[Option[T]]^{reader} =
     new JsonDeserializer[Option[T]]:
       def read(value: JsonAst, missing: Boolean): Option[T] =
@@ -284,61 +258,29 @@ object JsonDeserializer extends FallbackJsonDeserializer:
         keys.indices.foldLeft(Map[String, T]()): (acc, index) =>
           acc.updated(keys(index), reader.read(values(index), false))
 
-  private transparent inline def deriveProduct
-      [LabelsType <: Tuple, ParamsType <: Tuple]
-      (values: Map[String, JsonAst])
-      : Tuple =
-    inline erasedValue[ParamsType] match
-      case _: (paramHead *: paramsTail) => inline erasedValue[LabelsType] match
-        case _: (labelHead *: labelsTail) => inline valueOf[labelHead].asMatchable match
-          case label: String =>
-            val missing = !values.contains(label)
-            val value = if missing then 0.asInstanceOf[JsonAst] else values(label)
-            val paramValue = summonInline[JsonDeserializer[paramHead]].read(value, missing)
-            
-            paramValue *: deriveProduct[labelsTail, paramsTail](values)
-      
-      case _ =>
-        EmptyTuple
-      
   inline given derived
-      [DerivationType](using mirror: Mirror.Of[DerivationType])(using Raises[JsonAccessError])
+      [DerivationType](using mirror: Mirror.Of[DerivationType])(using jsonAccess: Raises[JsonAccessError])
       : JsonDeserializer[DerivationType] =
     inline mirror match
-      case mirror: Mirror.ProductOf[DerivationType & Product] => (value, missing) =>
+      case given Mirror.ProductOf[DerivationType] => (value, missing) =>
         val keyValues = value.obj
         val values = keyValues(0).zip(keyValues(1)).to(Map)
         
-        val product: Product = mirror.fromProduct(deriveProduct[mirror.MirroredElemLabels,
-            mirror.MirroredElemTypes](values))
-        
-        mirror.fromProduct(product)
+        Derivation.productOf[DerivationType, JsonDeserializer]:
+          val missing = !values.contains(label.s)
+          val value = if missing then 0.asInstanceOf[JsonAst] else values(label.s)
+          typeclass.read(value, missing)
     
-      case mirror: Mirror.SumOf[DerivationType] => (value: JsonAst, missing: Boolean) =>
+      case mirror: Mirror.SumOf[DerivationType] => (value, missing) =>
         val values = value.obj
         
         values(0).indexOf("_type") match
-          case -1    =>
-            ???
+          case -1 =>
+            abort(JsonAccessError(Reason.Label(t"_type")))
           
           case index =>
-            deriveSum[mirror.MirroredElemTypes, mirror.MirroredElemLabels, DerivationType](values(1)
-                (index).string(using summonInline[Raises[JsonAccessError]]).s).read(value, missing)
-  
-  private transparent inline def deriveSum
-      [SubtypesType <: Tuple, LabelsType <: Tuple, DerivationType]
-      (subtype: String)
-      : JsonDeserializer[DerivationType] =
-    inline erasedValue[SubtypesType] match
-      case _: (head *: tail) => inline erasedValue[LabelsType] match
-        case _: (headLabel *: tailLabels) => inline valueOf[headLabel].asMatchable match
-          case label: String =>
-            if label == subtype
-            then summonInline[JsonDeserializer[head]].asInstanceOf[JsonDeserializer[DerivationType]]
-            else deriveSum[tail, tailLabels, DerivationType](subtype)
-      
-      case _ =>
-        throw Mistake(msg"could not match subtype in its apparent coproduct type")
+            val discriminant = values(1)(index).string
+            Derivation.sumOf[DerivationType, JsonDeserializer](discriminant)(typeclass.read(value, missing))
 
 trait JsonDeserializer[ValueType]:
   private inline def reader: this.type = this
