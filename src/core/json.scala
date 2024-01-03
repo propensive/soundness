9d5ded849ddb6def42f65495a76da660ce644042
Jon Pretty <jon.pretty@propensive.com>
2024-01-03 13:10:43 +0100

    Much simpler Wisteria API

--- src/core/json.scala
+++ src/core/json.scala
@@ -190,14 +190,13 @@ object JsonEncoder extends JsonEncoder2:
       case Some(value) => summon[JsonEncoder[ValueType]].encode(value)
 
   inline def join[DerivationType: Mirror.ProductOf]: JsonEncoder[DerivationType] = value =>
-    val labels = Derivation.product.of[DerivationType, JsonEncoder](value)(label.s)
-    val values = Derivation.product.of[DerivationType, JsonEncoder](value):
-      typeclass.encode(param)
+    val labels = product.from(value)(label.s)
+    val values = product.from(value)(typeclass.encode(param))
     
     JsonAst((labels, values))
   
   inline def split[DerivationType: Mirror.SumOf]: JsonEncoder[DerivationType] = value =>
-    Derivation.sum.from[DerivationType, JsonEncoder](value):
+    sum.from(value):
       summonInline[Raises[JsonAccessError]].contextually(typeclass.tag(label).encode(value))
 
 trait JsonEncoder[-ValueType]:
@@ -288,7 +287,7 @@ object JsonDecoder extends JsonDecoder2, Derived[JsonDecoder]:
       val keyValues = json.obj
       val values = keyValues(0).zip(keyValues(1)).to(Map)
 
-      Derivation.product.from[DerivationType, JsonDecoder]:
+      product.of:
         val missing = !values.contains(label.s)
         val value = if missing then JsonAst(0L) else values(label.s)
         typeclass.decode(value, missing)
@@ -302,7 +301,7 @@ object JsonDecoder extends JsonDecoder2, Derived[JsonDecoder]:
         
         case index =>
           val discriminant = values(1)(index).string
-          Derivation.sum.of[DerivationType, JsonDecoder](discriminant)(typeclass.decode(json, missing))
+          sum.of(discriminant)(typeclass.decode(json, missing))
 
 trait JsonDecoder[ValueType]:
   private inline def decoder: this.type = this
