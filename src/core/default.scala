9379fd4411675abe70a15bd929ac423e9a36795f
Jon Pretty <jon.pretty@propensive.com>
2024-01-10 08:16:05 +0100

    Make `Default` a SAM type

--- src/core/default.scala
+++ src/core/default.scala
@@ -21,17 +21,18 @@ import anticipation.*
 import scala.compiletime.*
 
 object Default:
-  given Default[Int](0)
-  given [ValueType: ValueOf]: Default[ValueType](valueOf[ValueType])
-  given Default[Long](0L)
-  given Default[Text]("".tt)
-  given Default[String]("")
-  given [ElemType]: Default[List[ElemType]](Nil)
-  given [ElemType]: Default[Set[ElemType]](Set())
-  given [ElemType]: Default[Vector[ElemType]](Vector())
+  def apply[ValueType](value: => ValueType): Default[ValueType] = () => value
+  given int: Default[Int] = () => 0
+  given singleton[ValueType: ValueOf]: Default[ValueType] = () => valueOf[ValueType]
+  given default: Default[Long] = () => 0L
+  given text: Default[Text] = () => "".tt
+  given string: Default[String] = () => ""
+  given list[ElemType]: Default[List[ElemType]] = () => Nil
+  given set[ElemType]: Default[Set[ElemType]] = () => Set()
+  given vector[ElemType]: Default[Vector[ElemType]] = () => Vector()
 
-trait Default[+ValueType](default: ValueType):
-  def apply(): ValueType = default
+trait Default[+ValueType]:
+  def apply(): ValueType
 
 inline def default[ValueType]: ValueType = summonInline[Default[ValueType]]()
 
