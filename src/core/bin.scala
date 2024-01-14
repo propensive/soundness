50eb09c6eb7ede8ac6f780209a1a63f7de7b7054
Jon Pretty <jon.pretty@propensive.com>
2024-01-14 14:04:43 +0100

    Remove inequality code (which is now in Hypotenuse)

--- src/core/bin.scala
+++ src/core/bin.scala
@@ -70,61 +70,6 @@ object Rudiments:
       @targetName("times2")
       def *(right: Long): ByteSize = left*right
 
-  def inequality
-      (expr: Expr[Boolean], bound: Expr[Int | Double | Char | Byte | Short | Long | Float],
-          strict: Expr[Boolean], greaterThan: Expr[Boolean])
-      (using Quotes)
-      : Expr[Boolean] =
-    
-    val errorMessage = msg"this cannot be written as a range expression"
-    
-    val value =
-      if greaterThan.valueOrAbort then expr match
-        case '{($bound: Int) > ($value: Int)}        => value
-        case '{($bound: Int) >= ($value: Int)}       => value
-        case '{($bound: Double) > ($value: Double)}  => value
-        case '{($bound: Double) >= ($value: Double)} => value
-        case '{($bound: Char) > ($value: Char)}      => value
-        case '{($bound: Char) >= ($value: Char)}     => value
-        case '{($bound: Byte) > ($value: Byte)}      => value
-        case '{($bound: Byte) >= ($value: Byte)}     => value
-        case '{($bound: Short) > ($value: Short)}    => value
-        case '{($bound: Short) >= ($value: Short)}   => value
-        case '{($bound: Long) > ($value: Long)}      => value
-        case '{($bound: Long) >= ($value: Long)}     => value
-        case '{($bound: Float) > ($value: Float)}    => value
-        case '{($bound: Float) >= ($value: Float)}   => value
-        case _                                       => fail(errorMessage)
-      else expr match
-        case '{($bound: Int) < ($value: Int)}        => value
-        case '{($bound: Int) <= ($value: Int)}       => value
-        case '{($bound: Double) < ($value: Double)}  => value
-        case '{($bound: Double) <= ($value: Double)} => value
-        case '{($bound: Char) < ($value: Char)}      => value
-        case '{($bound: Char) <= ($value: Char)}     => value
-        case '{($bound: Byte) < ($value: Byte)}      => value
-        case '{($bound: Byte) <= ($value: Byte)}     => value
-        case '{($bound: Short) < ($value: Short)}    => value
-        case '{($bound: Short) <= ($value: Short)}   => value
-        case '{($bound: Long) < ($value: Long)}      => value
-        case '{($bound: Long) <= ($value: Long)}     => value
-        case '{($bound: Float) < ($value: Float)}    => value
-        case '{($bound: Float) <= ($value: Float)}   => value
-        case _                                       => fail(errorMessage)
-    
-    val (lessStrict, less) =
-      (if greaterThan.valueOrAbort then (bound, value) else (value, bound)) match
-        case ('{$left: Int}, '{$right: Int})       => ('{$left < $right}, '{$left <= $right})
-        case ('{$left: Float}, '{$right: Float})   => ('{$left < $right}, '{$left <= $right})
-        case ('{$left: Double}, '{$right: Double}) => ('{$left < $right}, '{$left <= $right})
-        case ('{$left: Long}, '{$right: Long})     => ('{$left < $right}, '{$left <= $right})
-        case ('{$left: Char}, '{$right: Char})     => ('{$left < $right}, '{$left <= $right})
-        case ('{$left: Byte}, '{$right: Byte})     => ('{$left < $right}, '{$left <= $right})
-        case ('{$left: Short}, '{$right: Short})   => ('{$left < $right}, '{$left <= $right})
-        case _                                      => fail(errorMessage)
-
-    '{$expr && ${if strict.valueOrAbort then lessStrict else less}}
-  
   def bin(expr: Expr[StringContext])(using Quotes): Expr[AnyVal] =
     import quotes.reflect.*
     val bits = expr.valueOrAbort.parts.head
