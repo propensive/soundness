83e622bbc7723146d7da5ba239d7e58a8abd8737
Jon Pretty <jon.pretty@propensive.com>
2024-01-17 07:51:35 +0100

    Use type parameters for image type

--- src/core/hallucination.scala
+++ src/core/hallucination.scala
@@ -10,28 +10,45 @@ import java.awt.image as jai
 import java.awt as ja
 import javax.imageio as ji
 
-enum ImageFormat:
-  case Png, Jpeg, Gif, Bmp, Tiff
+class ImageCodec[ImageFormatType <: ImageFormat](name: Text):
+  protected val reader: ji.ImageReader = ji.ImageIO.getImageReaders(name.s).nn.next().nn
+  
+  def read[InputType](inputType: InputType)(using Readable[InputType, Bytes]): Image[?] =
+    reader.synchronized:
+      reader.setInput(ji.ImageIO.createImageInputStream(inputType.readAs[Bytes].javaInputStream).nn)
+      Image(reader.read(0).nn).also(reader.dispose())
 
-case class Image(private[hallucination] val image: jai.BufferedImage):
+erased trait ImageFormat
+erased trait Bmp extends ImageFormat
+erased trait Jpeg extends ImageFormat
+erased trait Gif extends ImageFormat
+erased trait Png extends ImageFormat
+
+object Jpeg extends ImageCodec[Jpeg]("JPEG".tt)
+object Bmp extends ImageCodec[Bmp]("BMP".tt)
+object Gif extends ImageCodec[Gif]("GIF".tt)
+object Png extends ImageCodec[Png]("PNG".tt)
+
+case class Image[ImageFormatType <: ImageFormat](private[hallucination] val image: jai.BufferedImage):
   def width: Int = image.getWidth
   def height: Int = image.getHeight
+  
   def apply(x: Int, y: Int): Rgb24 =
     val color: ja.Color = ja.Color(image.getRGB(x, y), true)
     Rgb24(color.getRed, color.getGreen, color.getBlue)
 
   def render: Text =
     val buffer = new StringBuilder()
-    for y <- (height - 1) until 0 by -2 do
+    for y <- 0 until (height - 1) by 2 do
       for x <- 0 until width do
         val foreground = apply(x, y)
-        val background = apply(x, y - 1)
-        buffer.append(e"${apply(x, y)}(${Bg(apply(x, y - 1))}(▀))".render)
+        val background = apply(x, y + 1)
+        buffer.append(e"${apply(x, y)}(${Bg(apply(x, y + 1))}(▀))".render)
       
       buffer.append('\n')
     
     buffer.toString.tt
 
 object Image:
-  def read[InputType](inputType: InputType)(using Readable[InputType, Bytes]): Image =
+  def apply[InputType](inputType: InputType)(using Readable[InputType, Bytes]): Image[?] =
     Image(ji.ImageIO.read(inputType.readAs[Bytes].javaInputStream).nn)
