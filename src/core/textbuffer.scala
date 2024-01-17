90150336a68b1aba76f78e5c583ca085d8c1698b
Jon Pretty <jon.pretty@propensive.com>
2024-01-17 08:59:32 +0100

    Improve `TextBuffer` implementation

--- src/core/textbuffer.scala
+++ src/core/textbuffer.scala
@@ -2,23 +2,33 @@ package gossamer
 
 import rudiments.*
 import anticipation.*
+import spectacular.*
+
+def append
+    [TextType, ValueType]
+    (using buffer: Buffer[TextType])
+    (using textual: Textual[TextType])
+    (value: ValueType)
+    (using show: textual.ShowType[ValueType]) =
+  buffer.append(textual.show(value))
+
+extension (textObject: Text.type)
+  def make(block: (buffer: TextBuffer) ?=> Unit): Text =
+    val buffer = TextBuffer()
+    block(using buffer)
+    buffer()
 
 abstract class Buffer[TextType]():
-  protected val textual: Textual[TextType]
-  protected def append(text: TextType): Unit
+  protected def put(text: TextType): Unit
   protected def wipe(): Unit
   protected def result(): TextType
   
-  @targetName("append")
-  def += [ValueType: textual.ShowType](value: ValueType): this.type =
-    this.also(append(textual.show(value)))
-  
+  def append(text: TextType): this.type = this.also(put(text))
   def apply(): TextType = result()
   def clear(): this.type = this.also(wipe())
 
-case class TextBuffer() extends Buffer[Text]():
+class TextBuffer() extends Buffer[Text]():
   private val buffer: StringBuilder = StringBuilder()
-  protected val textual: Textual[Text] = Textual.text
-  protected def append(text: Text): Unit = buffer.append(text)
+  protected def put(text: Text): Unit = buffer.append(text)
   protected def wipe(): Unit = buffer.clear()
   protected def result(): Text = buffer.toString().tt
