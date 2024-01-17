59095fb2e3312e8bae101ae71afc36502039698d
Jon Pretty <jon.pretty@propensive.com>
2024-01-17 09:02:33 +0100

    Use `Text.make` for slightly cleaner code

--- src/core/hallucination.scala
+++ src/core/hallucination.scala
@@ -20,14 +20,17 @@ import escapade.*
 import turbulence.*
 import rudiments.*
 import anticipation.*
+import spectacular.*
 import iridescence.*
+import gossamer.*
 
 import java.awt.image as jai
 import java.awt as ja
 import javax.imageio as ji
 
 class ImageCodec[ImageFormatType <: ImageFormat](name: Text):
-  protected val reader: ji.ImageReader = ji.ImageIO.getImageReaders(name.s).nn.next().nn
+  protected lazy val reader: ji.ImageReader = ji.ImageIO.getImageReaders(name.s).nn.next().nn
+  protected lazy val writer: ji.ImageWriter = ji.ImageIO.getImageWriter(reader).nn
   
   def read[InputType](inputType: InputType)(using Readable[InputType, Bytes]): Image[?] =
     reader.synchronized:
@@ -53,17 +56,10 @@ case class Image[ImageFormatType <: ImageFormat](private[hallucination] val imag
     val color: ja.Color = ja.Color(image.getRGB(x, y), true)
     Rgb24(color.getRed, color.getGreen, color.getBlue)
 
-  def render: Text =
-    val buffer = new StringBuilder()
+  def render: Text = Text.make:
     for y <- 0 until (height - 1) by 2 do
-      for x <- 0 until width do
-        val foreground = apply(x, y)
-        val background = apply(x, y + 1)
-        buffer.append(e"${apply(x, y)}(${Bg(apply(x, y + 1))}(▀))".render)
-      
-      buffer.append('\n')
-    
-    buffer.toString.tt
+      for x <- 0 until width do append(e"${apply(x, y)}(${Bg(apply(x, y + 1))}(▀))".render)
+      append('\n')
 
 object Image:
   def apply[InputType](inputType: InputType)(using Readable[InputType, Bytes]): Image[?] =
