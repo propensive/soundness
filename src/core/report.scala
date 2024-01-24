48870feb813f155006827eb5d34f3f85043ecda4
Jon Pretty <jon.pretty@propensive.com>
2024-01-24 07:10:23 +0100

    Renamed `TextWidthCalculator` to `TextMetrics`

--- src/core/report.scala
+++ src/core/report.scala
@@ -249,7 +249,7 @@ class TestReport(using Environment):
   
 
   def complete(coverage: Option[CoverageResults])(using Stdio): Unit =
-    given TextWidthCalculator with
+    given TextMetrics with
       private val eastAsian = textMetrics.eastAsianScripts
       def width(text: Text): Int = text.s.foldLeft(0)(_ + width(_))
       def width(char: Char): Int = char match
