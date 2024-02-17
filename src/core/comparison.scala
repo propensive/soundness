6da1a40cf252ba400e35e2d72602cc9551f5a830
Jon Pretty <jon.pretty@propensive.com>
2024-02-17 20:44:29 +0100

    Show difference for numbers

--- src/core/comparison.scala
+++ src/core/comparison.scala
@@ -33,7 +33,7 @@ import scala.deriving.*
 
 enum Semblance:
   case Identical(value: Text)
-  case Different(left: Text, right: Text)
+  case Different(left: Text, right: Text, difference: Optional[Text] = Unset)
   case Breakdown(comparison: IArray[(Text, Semblance)], left: Text, right: Text)
 
 object Semblance:
@@ -42,11 +42,11 @@ object Semblance:
       import tableStyles.horizontalGaps
       
       def children(comp: (Text, Semblance)): List[(Text, Semblance)] = comp(1) match
-        case Identical(value)                 => Nil
-        case Different(left, right)           => Nil
+        case Identical(value)                   => Nil
+        case Different(left, right, difference) => Nil
         case Breakdown(comparison, left, right) => comparison.to(List)
       
-      case class Row(treeLine: Text, left: Output, right: Output)
+      case class Row(treeLine: Text, left: Output, right: Output, difference: Output)
 
       given TreeStyle[Row] = (tiles, row) =>
         row.copy(treeLine = tiles.map(treeStyles.default.text(_)).join+row.treeLine)
@@ -56,23 +56,25 @@ object Semblance:
         
         data(1) match
           case Identical(v) =>
-            Row(line(t"▪"), e"${rgb"#667799"}($v)", e"${rgb"#667799"}($v)")
+            Row(line(t"▪"), e"${rgb"#667799"}($v)", e"${rgb"#667799"}($v)", e"")
           
-          case Different(left, right) =>
-            Row(line(t"▪"), e"${colors.YellowGreen}($left)", e"${colors.Crimson}($right)")
+          case Different(left, right, difference) =>
+            Row(line(t"▪"), e"${colors.YellowGreen}($left)", e"${colors.Crimson}($right)",
+                e"${rgb"#40bbcb"}(${difference.or(t"")})")
           
           case Breakdown(cmp, left, right) =>
-            Row(line(t"■"), e"$left", e"$right")
+            Row(line(t"■"), e"$left", e"$right", e"")
       
       val table = Table[Row](
         Column(e"")(_.treeLine),
         Column(e"Expected", align = Alignment.Right)(_.left),
-        Column(e"Found")(_.right)
+        Column(e"Found")(_.right),
+        Column(e"Difference")(_.difference.or(e""))
       )
 
       table.tabulate(TreeDiagram.by(children(_))(cmp*).render(mkLine), maxWidth = 200).join(e"\n")
     
-    case Different(left, right) =>
+    case Different(left, right, difference) =>
       val whitespace = if right.contains('\n') then e"\n" else e" "
       val whitespace2 = if left.contains('\n') then e"\n" else e" "
       e"The result$whitespace${colors.Crimson}($right)${whitespace}did not equal$whitespace${colors.YellowGreen}($left)"
@@ -97,6 +99,18 @@ extension [ValueType](left: ValueType)
 object Contrast extends Derivation[Contrast]:
   def nothing[ValueType]: Contrast[ValueType] = (left, right) => Semblance.Identical(left.debug)
   
+  given int: Contrast[Int] = (left, right) =>
+    if left == right then Semblance.Identical(left.show)
+    else Semblance.Different(left.show, right.show, t"${math.abs(right - left)}")
+  
+  given double: Contrast[Double] = (left, right) =>
+    given Decimalizer = Decimalizer(3)
+    if left == right then Semblance.Identical(left.show)
+    else
+      val size = 100*(right - left)/left
+      val sizeText = if size.isFinite then t"${if size > 0 then t"+" else t""}$size%" else t""
+      Semblance.Different(left.show, right.show, sizeText)
+
   inline def general[ValueType]: Contrast[ValueType] = (left, right) =>
     if left == right then Semblance.Identical(left.debug) else Semblance.Different(left.debug, right.debug)
 
