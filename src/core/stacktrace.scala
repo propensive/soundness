b985fef2d8062693cbf75c8b5058ff915252b4e8
Jon Pretty <jon.pretty@propensive.com>
2024-02-27 09:55:19 +0100

    Tweak whitespace

--- src/core/stacktrace.scala
+++ src/core/stacktrace.scala
@@ -219,6 +219,7 @@ object StackTrace:
 case class StackTrace
     (component: Text, className: Text, message: Message, frames: List[StackTrace.Frame],
         cause: Optional[StackTrace]):
+
   def crop(cutClassName: Text, cutMethod: Text): StackTrace =
     val frames2 = frames.takeWhile { f => f.method.className != cutClassName || f.method.method != cutMethod }
     StackTrace(component, className, message, frames2, cause)
