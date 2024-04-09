aa68a7dafd634831f11854a9fa36146eee3badb0
Jon Pretty <jon.pretty@propensive.com>
2024-04-09 08:28:38 +0200

    Do not require `CodlDecoder` for encoding

--- src/core/interpolator.scala
+++ src/core/interpolator.scala
@@ -21,5 +21,5 @@ import language.experimental.captureChecking
 extension (inline ctx: StringContext)
   transparent inline def codl(inline parts: Any*): CodlDoc = ${Codl.Prefix.expand('ctx, 'parts)}
 
-extension [T](value: T)(using encoder: CodlEncoder[T], decoder: CodlDecoder[T])
-  def codl: CodlDoc = CodlDoc(IArray.from(encoder.encode(value).flatten), decoder.schema, 0)
+extension [ValueType](value: ValueType)(using encoder: CodlEncoder[ValueType])
+  def codl: CodlDoc = CodlDoc(IArray.from(encoder.encode(value).flatten), encoder.schema, 0)
