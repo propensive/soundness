c72a0ce886c5339def4ca749ac5f80a4efe8c592
Jon Pretty <jon.pretty@propensive.com>
2024-05-06 15:33:45 +0200

    Export to `soundness` package

---
+++ src/core/soundness.vacuous.scala
@@ -0,0 +1,3 @@
+package soundness
+
+export vacuous.{Default, default, Unset, Optional, UnsetError, or, absent, present, vouch, mask, stdlib, presume, option, assume, lay, layGiven, let, letGiven, compact, optional, puncture, only, Unsafe, Extractor}
