4e792d87199bdb3aa195b58fb0e8d6fb355f2eb5
Jon Pretty <jon.pretty@propensive.com>
2024-05-13 07:39:26 +0200

    Add `soundness` exports

---
+++ src/core/soundness+rudiments.scala
@@ -0,0 +1,22 @@
+package soundness
+
+export rudiments.{ByteSize, bin, hex, Hex, Bytes, b, kb, mb, gb, tb, byteSize, sift, has, where,
+    interleave, each, all, sumBy, bi, tri, indexBy, longestTrain, mutable, immutable, snapshot,
+    place, at, upsert, collate, establish, plus, runs, runsBy, Cursor, cursor, precursor, postcursor,
+    cursorIndex, cursorOffset, curse, ult, create, javaInputStream, DecimalConverter, as, Irrefutable,
+    Unapply, As, ClassTag, Typeable, Set, List, ListMap, Map, TreeSet, TreeMap, TrieMap, NonFatal,
+    boundary, break, IteratorHasAsScala, ListHasAsScala, MapHasAsScala, SeqHasAsJava, MapHasAsJava,
+    EnumerationHasAsScala, tailrec, missingContext, targetName, switch, StaticAnnotation, capability,
+    uncheckedVariance, uncheckedCaptures, uncheckedStable, ###, map, contramap, Functor, Cofunctor,
+    ExitStatus, Pid, Quickstart, Nat, Label, unit, waive, twin, triple, is, matchable, give, pipe,
+    tap, also, Counter, loop, Loop, &, tuple, to, WorkingDirectoryError, HomeDirectoryError,
+    WorkingDirectory, HomeDirectory, workingDirectory, homeDirectory}
+
+package quickstart:
+  export rudiments.quickstart.defaults as defaults
+
+package workingDirectories:
+  export rudiments.workingDirectories.default as default
+
+package homeDirectories:
+  export rudiments.homeDirectories.default as default
