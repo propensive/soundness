0593c9d1ccf8b2d5d47f7343930d303e555d1bd7
Jon Pretty <jon.pretty@propensive.com>
2024-05-16 14:07:23 +0200

    Added `soundness` exports

---
+++ src/core/soundness+wisteria.scala
@@ -0,0 +1,7 @@
+package soundness
+
+export wisteria.{FieldIndex, ContextRequirement, VariantIndex, VariantError, ProductDerivation, Derivation,
+    Reflection, ProductReflection, SumReflection}
+
+package derivationContext:
+  export wisteria.derivationContext.{required, relaxed}
