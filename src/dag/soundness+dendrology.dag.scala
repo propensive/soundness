7ff424795e1df71952968a9085e22145f7cb2edc
Jon Pretty <jon.pretty@propensive.com>
2024-05-21 15:56:40 +0200

    Prepare for republish with `soundness` export

---
+++ src/dag/soundness+dendrology.dag.scala
@@ -0,0 +1,6 @@
+package soundness
+
+export dendrology.{DagTile, TextualDagStyle, DagStyle, DagDiagram}
+
+package dagStyles:
+  export dendrology.dagStyles.{default, ascii}
---
+++ src/tree/soundness+dendrology.tree.scala
@@ -0,0 +1,6 @@
+package soundness
+
+export dendrology.{TreeStyle, TextualTreeStyle, TreeTile, TreeDiagram, Expandable}
+
+package treeStyles:
+  export dendrology.treeStyles.{default, rounded, ascii}
