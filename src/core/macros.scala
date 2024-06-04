fd93fb12150b9a820648b4f63e957e6f9e72c85c
Jon Pretty <jon.pretty@propensive.com>
2024-06-04 12:36:02 +0200

    Use `abandon` instead of `fail`

--- src/core/macros.scala
+++ src/core/macros.scala
@@ -28,43 +28,43 @@ import scala.compiletime.*
 
 object SerpentineMacro:
   given Realm = realm"serpentine"
-  
+
   def parse
       [NameType <: Label: Type](context: Expr[StringContext])(using Quotes)
       : Expr[PExtractor[NameType]] =
     import quotes.reflect.*
-    
+
     val (element: String, pos: Position) = (context: @unchecked) match
       case '{StringContext(${Varargs(Seq(str))}*)} => (str.value.get, str.asTerm.pos)
-    
+
     patterns(TypeRepr.of[NameType]).each: pattern =>
       if element.matches(pattern) then pattern match
         case r"\.\*\\?$char(.)\.\*" =>
-          fail(msg"a path element may not contain the character $char", pos)
+          abandon(msg"a path element may not contain the character $char", pos)
 
         case r"$start([a-zA-Z0-9]*)\.\*" =>
-          fail(msg"a path element may not start with $start", pos)
+          abandon(msg"a path element may not start with $start", pos)
 
         case r"\.\*$end([a-zA-Z0-9]*)" =>
-          fail(msg"a path element may not end with $end", pos)
+          abandon(msg"a path element may not end with $end", pos)
 
         case pattern@r"[a-zA-Z0-9]*" =>
-          fail(msg"a path element may not be $pattern", pos)
+          abandon(msg"a path element may not be $pattern", pos)
 
         case other =>
-          fail(msg"a path element may not match the pattern $other")
+          abandon(msg"a path element may not match the pattern $other")
 
     '{
       new PExtractor[NameType]():
         def apply(): PathName[NameType] = ${Expr(element)}.asInstanceOf[PathName[NameType]]
         def unapply(name: PathName[NameType]): Boolean = name.render.s == ${Expr(element)}
     }
-  
+
   private[serpentine] def patterns
       (using quotes: Quotes)(repr: quotes.reflect.TypeRepr)
       : List[String] =
     import quotes.reflect.*
-    
+
     (repr.dealias.asMatchable: @unchecked) match
       case OrType(left, right)                   => patterns(left) ++ patterns(right)
       case ConstantType(StringConstant(pattern)) => List(pattern)
@@ -76,24 +76,24 @@ object SerpentineMacro:
     import quotes.reflect.*
 
     val checks: List[String] = patterns(TypeRepr.of[NameType])
-    
+
     def recur(patterns: List[String], statements: Expr[Unit]): Expr[Unit] = patterns match
       case pattern :: tail =>
         import PathError.Reason.*
-        
+
         def reasonExpr: Expr[PathError.Reason] = pattern match
           case r"\.\*\\?$char(.)\.\*"       => '{InvalidChar(${Expr(char.head)})}
           case r"$prefix([a-zA-Z0-9]*)\.\*" => '{InvalidPrefix(Text(${Expr(prefix.toString)}))}
           case r"\.\*$suffix([a-zA-Z0-9]*)" => '{InvalidSuffix(Text(${Expr(suffix.toString)}))}
           case other                        => '{InvalidName(Text(${Expr(pattern)}))}
-        
+
         recur(tail, '{
           $statements
 
           if $text.s.matches(${Expr(pattern)})
           then raise(PathError($text, $reasonExpr))($text.asInstanceOf[PathName[NameType]])(using $errorHandler)
         })
-      
+
       case _ =>
         statements
 
