96ef3b3b033d8e4e93d43d7aa8761b4ce51142b8
Jon Pretty <jon.pretty@propensive.com>
2024-06-16 18:33:14 +0200

    Remember to export `Derivable` and `ProductDerivable`

--- src/core/soundness+wisteria.scala
+++ src/core/soundness+wisteria.scala
@@ -17,7 +17,7 @@
 package soundness
 
 export wisteria.{FieldIndex, ContextRequirement, VariantIndex, VariantError, ProductDerivation, Derivation,
-    Reflection, ProductReflection, SumReflection}
+    Reflection, ProductReflection, SumReflection, Derivable, ProductDerivable}
 
 package derivationContext:
   export wisteria.derivationContext.{required, relaxed}
