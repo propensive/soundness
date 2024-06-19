fbd308bde0bb3b9061866d79fc4867d2267cf807
Jon Pretty <jon.pretty@propensive.com>
2024-06-19 10:25:30 +0200

    Generalize unpacking for different types

--- src/core/deserialize.scala
+++ src/core/deserialize.scala
@@ -16,66 +16,81 @@
 
 package bifurcate
 
-import wisteria.*
-import rudiments.*
 import anticipation.*
 import hypotenuse.*
+import rudiments.*
+import wisteria.*
 
-object Unpackable extends ProductDerivable[Unpackable]:
+object Unpackable:
+  given [PackType: Unspoolable](using ClassTag[PackType]) => IArray[PackType] is Unpackable:
+    type Wrap[Type] = Int => Type
+    def unpack(spool: Spool): Int => IArray[PackType] = count =>
+      IArray.create[PackType](count): array =>
+        array.indices.each: index =>
+          array(index) = PackType.unspool(spool)
 
-  def apply[DataType](byteWidth: Int)(lambda: (Bytes, Int) => DataType): DataType is Unpackable =
-    new Unpackable:
-      type Self = DataType
-      def unpack(data: Bytes, offset: Int): DataType = lambda(data, offset)
-      def width: Int = byteWidth
-
-  given B8 is Unpackable = Unpackable(1) { (data, offset) => data(offset).bits }
-  given B16 is Unpackable = Unpackable(2)(B16(_, _))
-  given B32 is Unpackable = Unpackable(4)(B32(_, _))
-  given B64 is Unpackable = Unpackable(8)(B64(_, _))
-
-  given I8 is Unpackable = Unpackable(1) { (data, offset) => data(offset).bits.i8 }
-  given I16 is Unpackable = Unpackable(2)(B16(_, _).i16)
-  given I32 is Unpackable = Unpackable(4)(B32(_, _).i32)
-  given I64 is Unpackable = Unpackable(8)(B64(_, _).i64)
-
-  given U8 is Unpackable = Unpackable(1) { (data, offset) => data(offset).bits.u8 }
-  given U16 is Unpackable = Unpackable(2)(B16(_, _).u16)
-  given U32 is Unpackable = Unpackable(4)(B32(_, _).u32)
-  given U64 is Unpackable = Unpackable(8)(B64(_, _).u64)
-
-  given Byte is Unpackable = Unpackable(1) { (data, offset) => data(offset) }
-  given Short is Unpackable = Unpackable(2)(B16(_, _).i16.short)
-  given Int is Unpackable = Unpackable(4)(B32(_, _).i32.int)
-  given Long is Unpackable = Unpackable(8)(B64(_, _).i64.long)
-
-  inline def join[DerivationType <: Product: ProductReflection]: DerivationType is Unpackable = new:
-    def unpack(data: Bytes, offset: Int): DerivationType =
-      var position: Int = offset
-
-      construct:
-        [FieldType] => context =>
-          context.unpack(data, position).also:
-            position += context.width
-
-    def width: Int = contexts { [FieldType] => _.width }.sum
+  given [PackType: Unspoolable] => PackType is Unpackable:
+    type Wrap[Type] = Type
+    def unpack(spool: Spool): PackType = PackType.unspool(spool)
 
 trait Unpackable:
   type Self
-  def unpack(data: Bytes, offset: Int): Self
-  def width: Int
+  type Wrap[_]
+  type Result = Wrap[Self]
+  def unpack(spool: Spool): Wrap[Self]
 
 extension (bytes: Bytes)
-  def unpack[DataType: Unpackable](offset: Int = 0): DataType =
-    DataType.unpack(bytes, offset)
+  def unpackFrom[DataType: Unpackable](offset: Int): DataType.Result =
+    DataType.unpack(Spool(bytes, offset))
 
-  def unpackIArray[DataType: {ClassTag, Unpackable as unpackable}](size: Int, offset: Int = 0)
-          : IArray[DataType] =
+  def spool[ResultType](lambda: Spool ?=> ResultType): ResultType = lambda(using Spool(bytes))
 
-    val width = unpackable.width
+def unpack[ValueType: Unpackable](using spool: Spool): ValueType.Result =
+  ValueType.unpack(spool)
 
-    IArray.create[DataType](size): array =>
-      array.indices.each: index =>
-        array(index) = unpackable.unpack(bytes, offset + index*width)
+class Spool(private[bifurcate] val bytes: Bytes, initialPosition: Int = 0):
+  private[bifurcate] var position: Int = initialPosition
+  def offset: Int = position
+  def advance(count: Int): Unit = position += count
 
-def byteWidth[DataType: Unpackable]: Int = DataType.width
+def byteWidth[DataType: Unspoolable]: Int = DataType.width
+
+object Unspoolable extends ProductDerivable[Unspoolable]:
+  def apply[DataType](byteWidth: Int)(lambda: (Bytes, Int) => DataType): DataType is Unspoolable =
+    new:
+      def width: Int = byteWidth
+
+      def unspool(spool: Spool): DataType =
+        lambda(spool.bytes, spool.offset).also(spool.advance(width))
+
+  given B8 is Unspoolable = Unspoolable(1)(_(_).bits)
+  given B16 is Unspoolable = Unspoolable(2)(B16(_, _))
+  given B32 is Unspoolable = Unspoolable(4)(B32(_, _))
+  given B64 is Unspoolable = Unspoolable(8)(B64(_, _))
+
+  given I8 is Unspoolable = Unspoolable(1)(_(_).bits.i8)
+  given I16 is Unspoolable = Unspoolable(2)(B16(_, _).i16)
+  given I32 is Unspoolable = Unspoolable(4)(B32(_, _).i32)
+  given I64 is Unspoolable = Unspoolable(8)(B64(_, _).i64)
+
+  given U8 is Unspoolable = Unspoolable(1)(_(_).bits.u8)
+  given U16 is Unspoolable = Unspoolable(2)(B16(_, _).u16)
+  given U32 is Unspoolable = Unspoolable(4)(B32(_, _).u32)
+  given U64 is Unspoolable = Unspoolable(8)(B64(_, _).u64)
+
+  given Byte is Unspoolable = Unspoolable(1)(_(_))
+  given Short is Unspoolable = Unspoolable(2)(B16(_, _).i16.short)
+  given Int is Unspoolable = Unspoolable(4)(B32(_, _).i32.int)
+  given Long is Unspoolable = Unspoolable(8)(B64(_, _).i64.long)
+
+  inline def join[DerivationType <: Product: ProductReflection]: DerivationType is Unspoolable =
+    new:
+      def unspool(spool: Spool): DerivationType =
+        construct { [FieldType] => context => context.unspool(spool) }
+
+      def width = contexts { [FieldType] => _.width }.sum
+
+trait Unspoolable:
+  type Self
+  def width: Int
+  def unspool(spool: Spool): Self
