452b14b9975f2a78b4db1d1506c7b879ab519b28
Jon Pretty <jon.pretty@propensive.com>
2024-06-22 15:20:59 +0200

    Export to `soundness` package

---
+++ src/core/soundness+nettlesome-core.scala
@@ -0,0 +1,5 @@
+package soundness
+
+export nettlesome.{ip, mac, tcp, udp, on, internet, online, EmailAddress, EmailAddressError,
+    Endpoint, Hostname, HostnameError, Internet, IpAddressError, LocalPart, MacAddressError,
+    OfflineError, Online, Port, PortError, Remote}
