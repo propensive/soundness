d1f87dac3778f8af76a795752c64a931b0745497
Jon Pretty <jon.pretty@propensive.com>
2024-06-30 12:03:27 +0200

    Split up source files

--- src/core/phoenicia.Ttf.scala
+++ src/core/phoenicia.Ttf.scala
@@ -16,26 +16,17 @@
 
 package phoenicia
 
-import rudiments.*
-import hypotenuse.*
 import anticipation.*
+import bifurcate.*
 import contingency.*
-import gossamer.{at as _, *}
 import fulminate.*
-import vacuous.*
+import gossamer.{at as _, *}
+import hypotenuse.*
+import quantitative.*
+import rudiments.*
 import symbolism.*
 import turbulence.*
-import bifurcate.*
-import quantitative.*
-
-erased trait FontSize extends Dimension
-erased given fontSize: PhysicalQuantity[Units[1, FontSize], "font size"] = ###
-
-object Ems:
-  given UnitName[Ems[1]] = () => t"ems"
-
-erased trait Ems[Power <: Nat] extends Units[Power, FontSize]
-val Em: Quantity[Ems[1]] = Quantity(1.0)
+import vacuous.*
 
 object Ttf:
   def apply[SourceType: Readable by Bytes](source: SourceType): Ttf =
@@ -47,62 +38,6 @@ object Ttf:
 
   enum EncodingId:
     case Unicode1, Unicode1_1, IsoIec10646, Unicode2Bmp, Unicode2Full, UnicodeVariation, UnicodeFull
-
-object FontError:
-  enum Reason:
-    case MissingTable(tag: TableTag)
-    case UnknownFormat
-    case MagicNumber
-
-  given Reason is Communicable =
-    case Reason.MissingTable(tag) => msg"the table ${tag.text} was not found"
-    case Reason.UnknownFormat     => msg"the table contains data in an unknown format"
-    case Reason.MagicNumber       => msg"the font did not contain expected check data"
-
-case class FontError(reason: FontError.Reason)
-extends Error(msg"the font could not be read because $reason")
-
-object Phoenicia:
-  opaque type Glyph[TtfType <: Ttf & Singleton] = Int
-
-  object Glyph:
-    def apply(ttf: Ttf, id: Int): Glyph[ttf.type] = id
-
-  extension [TtfType <: Ttf & Singleton](glyph: Glyph[TtfType])
-    def id: Int = glyph
-
-export Phoenicia.Glyph
-
-sealed trait TableTag:
-  def text: Text
-
-enum TtfTag extends TableTag:
-  case Avar, Cmap, Cvar, Cvt, Fpgm, Fvar, Gasp, Glyf, Gvar, Hdmx, Head, Hhea, Hmtx, Kern, Loca,
-      Maxp, Meta, Name, Post, Prep, Sbix, Vhea, Vmtx
-
-  def text: Text = this match
-    case Cvt   => t"cvt "
-    case table => table.toString.tt.lower
-
-object TtfTag extends Extractor[Text, TtfTag]:
-  def extract(text: Text): Optional[TtfTag] = text match
-    case t"cvt " => Cvt
-    case other   => safely(TtfTag.valueOf(other.lower.capitalize.s))
-
-enum OtfTag extends TableTag:
-  case Base, Cbdt, Cblc, Cff, Cff2, Colr, Cpal, Dsig, Ebdt, Eblc, Ebsc, Gdef, Gpos, Gsub, Hvar,
-      Jstf, Ltsh, Math, Merg, Mvar, Os2, Pclt, Stat, Svg, Vdmx, Vorg, Vvar
-
-  def text: Text = this match
-    case Os2   => t"OS/2"
-    case Cff   => t"CFF "
-    case table => table.toString.tt.upper
-
-object OtfTag extends Extractor[Text, OtfTag]:
-  def extract(text: Text): Optional[OtfTag] = text match
-    case t"OS/2" => Os2
-    case t"CFF " => Cff
-    case other   => safely(OtfTag.valueOf(other.lower.capitalize.s))
 
 case class Ttf(data: Bytes):
   ttf =>
