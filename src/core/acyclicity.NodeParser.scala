62c6cb1045a17dfa0678557be42652252f854deb
Jon Pretty <jon.pretty@propensive.com>
2024-07-01 08:11:12 +0200

    Renamed `msg""` to `m""`

--- src/core/acyclicity.NodeParser.scala
+++ src/core/acyclicity.NodeParser.scala
@@ -40,7 +40,7 @@ object NodeParser extends Interpolator[Unit, Option[Dot.Ref], Dot.Ref]:
             Some(Dot.CompassPoint.valueOf(point.capitalize.s)))))
 
       case _ =>
-        throw InterpolationError(msg"not a valid node ID")
+        throw InterpolationError(m"not a valid node ID")
     }
 
   def initial: Option[Dot.Ref] = None
