1ecabd16c24de634a80b2a39ac472db11ffae9de
Jon Pretty <jon.pretty@propensive.com>
2024-07-12 00:22:40 +0200

    Rename `encodingMitigation`

--- src/core/daemon.scala
+++ src/core/daemon.scala
@@ -28,7 +28,7 @@ import contingency.*
 import exoskeleton.*
 import feudalism.*
 import eucalyptus.*
-import hieroglyph.*, charEncoders.utf8, charDecoders.utf8, encodingMitigation.strict
+import hieroglyph.*, charEncoders.utf8, charDecoders.utf8, sanitization.strict
 import parasite.*
 import profanity.*
 import symbolism.*
