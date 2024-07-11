9ac3a55ed2e84860452d75c82ce014ba0936fe96
Jon Pretty <jon.pretty@propensive.com>
2024-07-12 00:13:06 +0200

    Remember to export `serve` method

--- src/core/soundness+nettlesome-core.scala
+++ src/core/soundness+nettlesome-core.scala
@@ -18,4 +18,4 @@ package soundness
 
 export nettlesome.{ip, mac, tcp, udp, on, internet, online, EmailAddress, EmailAddressError,
     Endpoint, Hostname, HostnameError, Internet, IpAddressError, LocalPart, MacAddressError,
-    OfflineError, Online, Port, PortError, Connectable}
+    OfflineError, Online, Port, PortError, Connectable, serve}
