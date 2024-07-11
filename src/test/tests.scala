351e0d095424e978fc9f5d43d74232ef8a368b5f
Jon Pretty <jon.pretty@propensive.com>
2024-07-12 00:22:16 +0200

    Rename `encodingMitigation`

--- src/test/tests.scala
+++ src/test/tests.scala
@@ -24,7 +24,7 @@ import enigmatic.*
 import monotonous.*
 import contingency.*, strategies.throwUnsafely
 import spectacular.*
-import hieroglyph.*, charDecoders.utf8, charEncoders.utf8, encodingMitigation.skip
+import hieroglyph.*, charDecoders.utf8, charEncoders.utf8, sanitization.skip
 
 import alphabets.hex.upperCase
 
