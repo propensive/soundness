5979cbe4d76689e34e0afd6246ac1e39fd56207b
Jon Pretty <jon.pretty@propensive.com>
2024-07-12 11:29:41 +0200

    Remember to export `/`

--- src/core/soundness+serpentine-core.scala
+++ src/core/soundness+serpentine-core.scala
@@ -19,4 +19,4 @@ package soundness
 export serpentine.{Directional, Followable, Hierarchy, Navigable, PathCreator, PathEquality, Name,
     PathError, PExtractor, Radical, RootParser, Serpentine, SimpleLink, SimplePath, `%`, `?`,
     `?^`, `?^^`, `?^^^`, ascent, inWorkingDirectory, root, +, relativeTo, keep, conjunction,
-    precedes, Slash, /-, render, descent, depth, parent, ancestor, append, p}
+    precedes, Slash, /-, render, descent, depth, parent, ancestor, append, p, `/`}
