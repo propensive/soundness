6e212e1354937e6923eb7e5a6d44a1272c1e5692
Jon Pretty <jon.pretty@propensive.com>
2024-08-08 08:34:21 +0200

    Tweaks for version 0.4.0

--- src/example/example.scala
+++ src/example/example.scala
@@ -18,9 +18,11 @@ package escritoire
 
 import gossamer.*
 import rudiments.*
+import escapade.*
+import iridescence.*
 import spectacular.*
 import contingency.*, strategies.throwUnsafely
-import turbulence.*, stdioSources.virtualMachine.textOnly
+import turbulence.*, stdioSources.virtualMachine.ansi
 import anticipation.*
 import hieroglyph.*, textMetrics.uniform
 
@@ -40,12 +42,15 @@ import columnAttenuation.fail
 def run(): Unit =
   val table =
     Table[Library]
-     (Column(t"Name", sizing = columnar.Prose)(_.name),
-      Column(t"Identifier", sizing = columnar.Collapsible(0.9))(_.id),
-      Column(t"LoC", sizing = columnar.Collapsible(0.3))(_.linesOfCode),
-      Column(t"Year", sizing = columnar.Collapsible(0.5))(_.year),
-      Column(t"Description", textAlign = TextAlignment.Justify, sizing = columnar.Prose)
-       (_.description))
+     (Column(e"$Bold(Name)", sizing = columnar.Prose): lib =>
+        e"$Bold(${lib.name})",
+      Column(e"$Bold(Identifier)", sizing = columnar.Collapsible(0.9))(_.id),
+      Column(e"$Bold(LoC)", sizing = columnar.Collapsible(0.3))(_.linesOfCode),
+      Column(e"$Bold(Year)", sizing = columnar.Collapsible(0.5)): lib =>
+        if lib.year > 2020 then e"${webColors.SandyBrown}(${lib.year})"
+        else e"${webColors.Chocolate}(${lib.year})",
+      Column(e"$Bold(Description)", textAlign = TextAlignment.Justify, sizing = columnar.Prose): lib =>
+       e"$Italic(${lib.description})")
 
   // given TableRelabelling[Person] = () => Map(
   //   t"name" -> t"Addressee",
@@ -53,13 +58,42 @@ def run(): Unit =
   // )
 
   locally:
-    import tableStyles.default
-    Out.println(libraries.table.grid(100))
-    Out.println(List[Foo.User](Foo.User(t"Jon", 12.4)).table.grid(100))
-    Out.println(table.tabulate(libraries).grid(37))
+    import tableStyles.thinRounded
+    Out.println(table.tabulate(libraries).grid(70))
+
+    case class Digit(n: Int, digitName: Text)
+    val data = List(Digit(1, t"one"), Digit(2, t"two"), Digit(3, t"three"))
+    Out.println(data.table)
+
+    val table2 = Table[Fruit]
+     (Column(e"$Bold(Color)", textAlign = TextAlignment.Center): fruit =>
+        e"${Bg(fruit.color)}(   )",
+      Column(e"$Bold(English)")(_.english),
+      Column(e"$Bold(Russian)")(_.russian),
+      Column(e"$Bold(Chinese)")(_.chinese),
+      Column(e"$Bold(Japanese)")(_.japanese))
+
+    Out.println(table2.tabulate(fruits).grid(100))
+
+
+    case class Member(name: Text, handle: Text, age: Int)
+
+    val pinkFloyd =
+      List(Member(t"Nick Mason", t"@nickmasondrums", 80), Member(t"David Gilmour", t"@davidgilmour", 78))
+
+    Out.println(pinkFloyd.table)
 
   // for style <- List(tableStyles.default, tableStyles.thinRounded, tableStyles.horizontal, tableStyles.midOnly, tableStyles.vertical, tableStyles.minimal)
   // do style.give(Out.println(table.tabulate(libraries.take(2)).grid(50)))
+
+case class Fruit(chinese: Text, english: Text, russian: Text, japanese: Text, color: Rgb24)
+
+val fruits =
+  List
+   (Fruit(t"苹果", t"apple", t"яблоко", t"リンゴ", webColors.Crimson),
+    Fruit(t"梨", t"pear", t"груша", t"ナシ", webColors.YellowGreen),
+    Fruit(t"橙子", t"orange", t"апельсин", t"オレンジ", webColors.DarkOrange),
+    Fruit(t"柚子", t"yuzu", t"юдзу", t"柚子", webColors.Yellow))
 
 enum Foo:
   case User(name: Text, size: Double)
