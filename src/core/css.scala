1fe500318a9875984bc4f7e868d675b035202ecc
Jon Pretty <jon.pretty@propensive.com>
2024-08-15 14:08:07 +0200

    Disable selector

--- src/core/css.scala
+++ src/core/css.scala
@@ -131,11 +131,11 @@ sealed trait Selector(val value: Text):
   infix def ~ (that: Selector): Selector = Selector.Before(this, that)
 
 object Selector:
-  given [SelectorType: Selectable, SelectorType2: Selectable]
-      => CompareGreater[SelectorType, SelectorType2, Selector] as childSelector:
+  // given [SelectorType: Selectable, SelectorType2: Selectable]
+  //     => CompareGreater[SelectorType, SelectorType2, Selector] as childSelector:
 
-    inline def greaterThan(inline left: SelectorType, inline right: SelectorType2): Selector =
-      Selector.Child(SelectorType.selector(left), SelectorType2.selector(right))
+  //   inline def greaterThan(inline left: SelectorType, inline right: SelectorType2): Selector =
+  //     Selector.Child(SelectorType.selector(left), SelectorType2.selector(right))
 
   case class Element(element: Text) extends Selector(element):
     def normalize: Selector = this
