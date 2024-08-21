cf2d3192ec20790230aaeed76b4a16237d0be89f
Jon Pretty <jon.pretty@propensive.com>
2024-08-21 22:08:33 +0200

    Don't export `+`

--- src/core/soundness+serpentine-core.scala
+++ src/core/soundness+serpentine-core.scala
@@ -18,7 +18,7 @@ package soundness
 
 export serpentine.{Directional, Followable, Hierarchy, Navigable, PathCreator, PathEquality, Name,
     PathError, PExtractor, Radical, RootParser, Serpentine, SimpleLink, SimplePath, `%`, `?`,
-    `?^`, `?^^`, `?^^^`, ascent, inWorkingDirectory, root, +, relativeTo, keep, conjunction,
+    `?^`, `?^^`, `?^^^`, ascent, inWorkingDirectory, root, relativeTo, keep, conjunction,
     precedes, Slash, /-, render, descent, depth, parent, ancestor, append, p, `/`}
 
 package pathHierarchies:
