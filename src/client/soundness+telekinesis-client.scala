1d85a885ba498fc8646d84ce277bb5bfc54f23fa
Jon Pretty <jon.pretty@propensive.com>
2024-08-29 09:02:30 +0200

    Soundness exports

---
+++ src/client/soundness+telekinesis-client.scala
@@ -0,0 +1,6 @@
+package soundness
+
+export telekinesis.{Auth, FailureCase, Http, HttpBody, HttpError, HttpEvent, HttpHeaderDecoder,
+    HttpMethod, HttpReadable, HttpRequest, HttpResponse, HttpStatus, Locatable, Params, Postable,
+    QueryEncoder, RequestHeader, ResponseHeader, SimpleRequestHeader, post, put, get, options,
+    trace, patch, head, delete, connect}
