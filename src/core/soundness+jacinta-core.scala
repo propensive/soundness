63274403bcf5ac61ddaa3f1ecb359c5805768b08
Jon Pretty <jon.pretty@propensive.com>
2024-08-29 08:29:17 +0200

    Soundness exports

---
+++ src/core/soundness+jacinta-core.scala
@@ -0,0 +1,7 @@
+package soundness
+
+export jacinta.{dynamicJsonAccess, DynamicJsonEnabler, IndentedJsonPrinter, Json, JsonError,
+    JsonPrimitive, JsonPrinter, MinimalJsonPrinter, Ndjson, json}
+
+package jsonPrinters:
+  export jacinta.jsonPrinters.{indented, minimal}
