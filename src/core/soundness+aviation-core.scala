e8011919cc8f2ff6d7838f30d547a2c788e637fc
Jon Pretty <jon.pretty@propensive.com>
2024-10-03 23:51:19 +0200

    Soundness exports

---
+++ src/core/soundness+aviation-core.scala
@@ -0,0 +1,8 @@
+package soundness
+
+export aviation.{Base24, Base60, Calendar, Clock, Clockface, DateError, Denomination,
+    DiurnalTimespan, FixedDuration, Horology, Interval, LeapSeconds, LocalTime, MonthName,
+    RomanCalendar, StandardTime, TemporalTimespan, Timespan, Timestamp, TimeSystem, Timezone,
+    TimezoneError, Timing, Tzdb, TzdbError, Weekday, YearMonth, now, today, TimeEvent, am, pm,
+    year, month, week, day, hour, minute, second, years, months, weeks, days, hours, minutes,
+    seconds, tz}
