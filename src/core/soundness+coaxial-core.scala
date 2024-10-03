a7578e63edb97c401fc137743116de9f7085a896
Jon Pretty <jon.pretty@propensive.com>
2024-10-03 20:36:54 +0200

    Soundness exports

---
+++ src/core/soundness+coaxial-core.scala
@@ -0,0 +1,4 @@
+package soundness
+
+export coaxial.{Addressable, Bindable, BindError, Connectable, Connection, Control, DomainSocket,
+    Receivable, SocketService, Transmissible, UdpPacket, UdpResponse, listen, connect, transmit}
