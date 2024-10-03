e090ca3ffe7ab3212341ab97300d6b2cdce903a3
Jon Pretty <jon.pretty@propensive.com>
2024-10-03 11:44:48 +0200

    Add `Zerary` extractor as `AnyVal`

--- src/core/denominative.Zerary.scala
+++ src/core/denominative.Zerary.scala
@@ -2,7 +2,7 @@ package denominative
 
 object Zerary:
 
-  final class Match(int: Int):
+  final class Match(val int: Int) extends AnyVal:
     def isEmpty: false = false
     def get: Ordinal = Ordinal.zerary(int)
 
