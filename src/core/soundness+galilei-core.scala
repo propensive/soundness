5d55f365aabeaabf884112569c92c526fba2aeab
Jon Pretty <jon.pretty@propensive.com>
2024-10-03 09:50:13 +0200

    Add Soundness exports

---
+++ src/core/soundness+galilei-core.scala
@@ -0,0 +1,18 @@
+package soundness
+
+export galilei.{CopyAttributes, Creatable, CreateNonexistent, CreateNonexistentParents,
+    DeleteRecursively, DereferenceSymlinks, Dos, DosDrive, Entry, Eof, Filesystem,
+    FilesystemAttribute, Handle, IoError, IoEvent, Linux, MacOs, MoveAtomically, Openable,
+    OverwritePreexisting, Posix, ReadAccess, Socket, Symlink, TraversalOrder, Volume, Windows,
+    WindowsDrive, WriteAccess, WriteSynchronously, C, D, %, $, open, javaPath, javaFile, exists,
+    children, descendants, size, delete, wipe, volume, hardLinkTo, entry, copyTo, copyInto,
+    renameTo, moveTo, moveInto, symlinkTo, symlinkInto, modified, accessed, readable, writable,
+    hidden, touch, create, created, executable, hardLinks}
+
+package pathNavigation:
+  export galilei.pathNavigation.{linux, windows, macOs, posix, operatingSystem}
+
+package filesystemOptions:
+  export galilei.filesystemOptions.{readAccess, writeAccess, dereferenceSymlinks, moveAtomically,
+      copyAttributes, deleteRecursively, overwritePreexisting, createNonexistentParents,
+      createNonexistent, writeSynchronously}
