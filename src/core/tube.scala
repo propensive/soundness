377be64b0f03d58d3daf4559560a2e006de27d1e
Jon Pretty <jon.pretty@propensive.com>
2024-10-07 19:57:38 +0200

    Read `Stations.csv` as text

--- src/core/tube.scala
+++ src/core/tube.scala
@@ -37,4 +37,4 @@ def app(): Unit = cli:
 object Data:
   def stations(using Online) =
     val sourceUrl = url"https://api.tfl.gov.uk/stationdata/tfl-stationdata-detailed.zip"
-    ZipStream(sourceUrl.get()).extract(_ / n"Stations.csv")
+    ZipStream(sourceUrl.get()).extract(_ / n"Stations.csv").read[Text]
