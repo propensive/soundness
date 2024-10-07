675d52555c43fb5452fe6829c7d0a89b121d4c6a
Jon Pretty <jon.pretty@propensive.com>
2024-10-07 20:06:31 +0200

    Specify DSV format as CSV

--- src/core/tube.scala
+++ src/core/tube.scala
@@ -12,6 +12,7 @@ import logging.silent
 import strategies.throwUnsafely
 import charDecoders.utf8
 import textSanitizers.skip
+import dsvFormats.csvWithHeader
 
 val About = Subcommand(t"about", e"find out about the $Underline(tube) tool")
 val Install = Subcommand(t"install", e"[re]install the tab-completions")
