550e2ea6d8e572f2a79e00cbab12cf027ad82032
Jon Pretty <jon.pretty@propensive.com>
2024-10-09 15:29:02 +0200

    Handle JSON errors

--- src/core/tube.scala
+++ src/core/tube.scala
@@ -40,6 +40,7 @@ extension (name: Name[Naptan]) def resolve(using Online): Name[Naptan] = name.te
     mend:
       case error: HttpError      => name
       case error: JsonParseError => name
+      case error: JsonError      => name
     .within:
       import dynamicJsonAccess.enabled
       val json = Json.parse(url"https://api.tfl.gov.uk/StopPoint/$name".get())
