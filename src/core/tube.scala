53cac87c464f7ecd75f761a2d172e41374eebf1d
Jon Pretty <jon.pretty@propensive.com>
2024-10-09 16:43:47 +0200

    Specify orphan disposal

--- src/core/tube.scala
+++ src/core/tube.scala
@@ -21,6 +21,7 @@ import environments.daemonClient
 import homeDirectories.default
 import enumIdentification.kebabCase
 import supervisors.global
+import orphanDisposal.cancel
 
 erased given Naptan is Nominative under MustMatch["(|HUB[A-Z0-9]{3}|9[14]0[A-Z]+)"] = ###
 given StationRow is Suggestible = row => Suggestion(row.ref, row.name)
