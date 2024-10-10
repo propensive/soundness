4d51cc16e5837bda2fb03d32d79fa2a6e4f80c8b
Jon Pretty <jon.pretty@propensive.com>
2024-10-10 15:12:05 +0200

    Make sure the errors are displayed in the order they were added

--- src/core/tube.scala
+++ src/core/tube.scala
@@ -127,7 +127,7 @@ object Data:
       Json.parse(sourceUrl.get(RequestHeader.Accept(media"application/json"))).as[Plan]
 
 case class InitError(detail: Message)(using Diagnostics) extends Error(detail)
-case class UserError(messages: Message*)(using Diagnostics) extends Error(messages.join(m"\n")):
+case class UserError(messages: Message*)(using Diagnostics) extends Error(messages.reverse.join(m"\n")):
   infix def + (message: Message): UserError = UserError(message +: messages*)
 
 case class StationRow(id: Name[Naptan], name: Text):
