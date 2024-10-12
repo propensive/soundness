3db29589f6a6bb4b30824f955cd64074f14d8067
Jon Pretty <jon.pretty@propensive.com>
2024-10-12 18:44:38 +0200

    Add `soundness` exports

---
+++ src/core/soundness+gastronomy-core.scala
@@ -0,0 +1,7 @@
+package soundness
+
+export gastronomy.{Algorithm, Crc32, Digest, Digester, Digestible, Digestion, Feistel, HashFunction,
+    Md5, MessageDigestion, Sha1, Sha2, Sha384, Sha512, digest, checksum}
+
+package hashFunctions:
+  export gastronomy.hashFunctions.{crc32, md5, sha1, sha2}
