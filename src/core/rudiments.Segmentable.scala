0349ab2b28e44f4d9fd400a7ee64bf32df2eb17b
Jon Pretty <jon.pretty@propensive.com>
2024-10-23 10:21:30 +0200

    Fix off-by-one error with segments

--- src/core/rudiments.Segmentable.scala
+++ src/core/rudiments.Segmentable.scala
@@ -5,13 +5,13 @@ import anticipation.*
 
 object Segmentable:
   given [ElementType] => IndexedSeq[ElementType] is Segmentable =
-    (seq, interval) => seq.slice(interval.start.n0, interval.end.n1)
+    (seq, interval) => seq.slice(interval.start.n0, interval.end.n0)
 
   given [ElementType] => IArray[ElementType] is Segmentable as iarray =
-    (iarray, interval) => iarray.slice(interval.start.n0, interval.end.n1)
+    (iarray, interval) => iarray.slice(interval.start.n0, interval.end.n0)
 
   given Text is Segmentable = (text, interval) =>
-    text.s.substring(interval.start.n0, interval.end.n1).nn.tt
+    text.s.substring(interval.start.n0, interval.end.n0).nn.tt
 
 trait Segmentable:
   type Self
