43b6c9d0b1e37ecc556a0c2b2e837c17a47a53c1
Jon Pretty <jon.pretty@propensive.com>
2024-11-06 19:34:22 +0100

    Soundness exports

---
+++ src/core/soundness+enigmatic-core.scala
@@ -0,0 +1,4 @@
+package soundness
+
+export enigmatic.{Aes, Cipher, CryptoError, Dsa, Encryption, ExposeSecretKey, Hmac, Pem, PemError,
+  PemLabel, PrivateKey, Rsa, Signature, Signing, Symmetric, SymmetricKey, hmac}
