091a2ef2ba3d13b0fc117fa5e8af48729f4072cc
Jon Pretty <jon.pretty@propensive.com>
2024-11-06 19:02:15 +0100

    Avoid raises `ConcurrencyError` in `establish`

--- src/core/camouflage.Cache.scala
+++ src/core/camouflage.Cache.scala
@@ -2,11 +2,10 @@ package camouflage
 
 import parasite.*
 import rudiments.*
-import contingency.*
 import vacuous.*
 
 class Cache[ValueType]():
   private val value: Promise[ValueType] = Promise()
 
-  def establish(block: => ValueType): ValueType raises ConcurrencyError = value.synchronized:
-    if value.ready then value().vouch(using Unsafe) else block.tap(value.fulfill(_))
+  def establish(block: => ValueType): ValueType = value.synchronized:
+    if value.ready then value().vouch(using Unsafe) else block.tap(value.offer(_))
