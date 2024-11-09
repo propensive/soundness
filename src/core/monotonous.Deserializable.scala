ba5c8cfcaf429cb004e2222239960be52b683fba
Jon Pretty <jon.pretty@propensive.com>
2024-11-09 08:12:41 +0100

    Remove debug output

--- src/core/monotonous.Deserializable.scala
+++ src/core/monotonous.Deserializable.scala
@@ -37,12 +37,10 @@ trait Deserializable:
   def deserialize(stream: LazyList[Text]): LazyList[Bytes] =
     def recur(stream: LazyList[Text], previous: Text, carry: Int): LazyList[Bytes] = stream match
       case head #:: tail =>
-        println(s"   carry = $carry")
         val carry2 = (carry + head.length)%atomicity
         deserialize(previous, head, -carry, tail.isEmpty) #:: recur(tail, head, carry2)
 
       case _ =>
-        println(s"   carry = $carry")
         if carry > 0 then LazyList(deserialize(previous, t"", -carry, true)) else LazyList()
 
     recur(stream, t"", 0)
@@ -54,14 +52,11 @@ object Deserializable:
       override protected val atomicity = 8.lcm(base)/base
 
       def deserialize(previous: Text, text: Text, index0: Int, last: Boolean): Bytes =
-        //println(s"deserialize(previous.length=${previous.length}, text.length=${text.length}, index0=$index0, last=$last)")
         val padding: Char = if alphabet.padding then alphabet(1 << base) else '\u0000'
 
         val length =
           if last then text.where(_ != padding, bidi = Rtl).let(_.n0 + 1).or(text.length)*base/8
           else ((text.length - index0)/atomicity)*atomicity*base/8
-
-        println(s"   length = $length")
 
         IArray.create[Byte](length): array =>
           var source = if index0 < 0 then previous else text
@@ -76,7 +71,6 @@ object Deserializable:
 
               if bits + base >= 8 then
                 array(count) = ((next >>> (bits + base - 8)) & 0xff).toByte
-                println(t"  array($count) = ${array(count)}")
                 recur(next, bits + base - 8, count + 1, index0 + 1)
               else recur(next, bits + base, count, index0 + 1)
 
