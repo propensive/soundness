7a88d15413dbdaf6714ae5d9a7b1cecef792490c
Jon Pretty <jon.pretty@propensive.com>
2024-11-11 11:32:46 +0100

    Update indentation style

--- src/plugin/umbrageous.scala
+++ src/plugin/umbrageous.scala
@@ -31,7 +31,7 @@ class UmbrageousTransformer(options: List[String]) extends PluginPhase:
   override def transformUnit(tree: tpd.Tree)(using Context): tpd.Tree =
     import untpd.*
     val untpdTree = ctx.compilationUnit.untpdTree
-    
+
     val prefixes: List[(String, String)] =
       options.flatMap: opt =>
         opt.split(":").nn.to(List).map(_.nn) match
@@ -42,30 +42,30 @@ class UmbrageousTransformer(options: List[String]) extends PluginPhase:
 
     object transformer extends UntypedTreeMap:
       private def rewritePackage
-          (tree: Ident | Select, fqn: String, defs: List[Tree], select: Select => Select): PackageDef =
+         (tree: Ident | Select, fqn: String, defs: List[Tree], select: Select => Select): PackageDef =
         tree match
           case Ident(name) =>
             val pkg = name.decode.toString+"."+fqn
             val prefixes2 = prefixes.filter { (k, v) => pkg == k || pkg.startsWith(k+".") }.sortBy(_(0).length)
-              
+
             val ident = prefixes2.lastOption.fold(tree): (k, v) =>
               select(Select(Ident(v.toTermName), name))
-            
+
             val imports = prefixes2.lastOption.fold(prefixes) { (k, v) => prefixes.filter(_(0) != k) }.map:
               case (_, prefix) =>
                 Import(Ident(prefix.toTermName), List(ImportSelector(Ident(StdNames.nme.WILDCARD))))
-            
+
             PackageDef(ident, imports ::: defs)
-          
+
           case Select(pkg: (Ident | Select), name) =>
             rewritePackage(pkg, s"${name.decode}.$fqn", defs, Select(_, name))
-          
+
           case _ => ???
 
       override def transform(tree: Tree)(using Context): Tree =
         tree match
           case PackageDef(name: (Ident | Select), defs) => rewritePackage(name, "", defs, identity)
           case _                                        => super.transform(tree)
-    
+
     ctx.compilationUnit.untpdTree = transformer.transform(untpdTree)
     super.transformUnit(tree)
