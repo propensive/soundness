69a8b749c3a7821d0b5423b0d17c6dab7c996caf
Jon Pretty <jon.pretty@propensive.com>
2024-12-01 21:24:03 +0100

    Reinstate seconds conversion

--- src/core/quantitative.Quantitative.scala
+++ src/core/quantitative.Quantitative.scala
@@ -55,10 +55,8 @@ object Quantitative extends Quantitative2:
 
     erased given [UnitsType <: Measure]: CanEqual[Quantity[UnitsType], Quantity[UnitsType]] = ###
 
-    inline given [UnitsType <: Units[1, Time]]
-        => Quantity[UnitsType] is GenericDuration as genericDuration = quantity =>
-      inline quantity.in[Seconds] match
-        case quantity: Quantity[Seconds[1]] => (quantity*1000).toLong
+    given Quantity[Seconds[1]] is GenericDuration as genericDuration =
+      quantity => (quantity*1000.0).toLong
 
     given [UnitsType <: Measure] => Numeric[Quantity[UnitsType]] as numeric =
       summon[Numeric[Double]]
