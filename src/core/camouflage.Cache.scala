6dc3c34ace17312c46f98519d4023a8e1cb70aee
Jon Pretty <jon.pretty@propensive.com>
2024-12-01 11:20:56 +0100

    Fixed cache

--- src/core/camouflage.Cache.scala
+++ src/core/camouflage.Cache.scala
@@ -1,11 +1,23 @@
 package camouflage
 
 import parasite.*
+import anticipation.*
 import rudiments.*
 import vacuous.*
 
-class Cache[ValueType]():
-  private val value: Promise[ValueType] = Promise()
+object Cache:
+  def apply[DurationType: GenericDuration, ValueType](duration: DurationType): Cache[ValueType] =
+    new Cache[ValueType](duration.milliseconds)
+
+  def apply[ValueType](): Cache[ValueType] = new Cache(Unset)
+
+class Cache[ValueType](lifetime: Optional[Long]):
+  private var expiry: Long = Long.MaxValue
+  private var value: Promise[ValueType] = Promise()
 
   def establish(block: => ValueType): ValueType = value.synchronized:
-    if value.ready then value().vouch(using Unsafe) else block.tap(value.offer(_))
+    if expiry < System.currentTimeMillis then value = Promise()
+
+    if value.ready then value().vouch(using Unsafe) else block.tap: result =>
+      value.offer(result)
+      expiry = lifetime.lay(Long.MaxValue)(_ + System.currentTimeMillis)
