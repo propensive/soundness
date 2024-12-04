ade2fb6a0bc7e4202cf26a753d67c5f2e22315b8
Jon Pretty <jon.pretty@propensive.com>
2024-12-04 12:04:22 +0100

    Soundness exports

---
+++ src/core/soundness+honeycomb-core.scala
@@ -0,0 +1,15 @@
+package soundness
+
+export honeycomb.{cls, id, A, Abbr, Address, Area, Article, Aside, Audio, B, Base, Bdi, Bdo,
+    Blockquote, Body, Br, Button, Canvas, Caption, Cite, Code, Col, Colgroup, Data, Datalist, Dd,
+    Del, Details, Dfn, Dialog, Div, Dl, Dt, Em, Embed, Fieldset, Figcaption, Figure, Footer, Form,
+    H1, H2, H3, H4, H5, H6, HMap, Head, Header, Hgroup, Hr, I, Iframe, Img, Input, Ins, Kbd, Label,
+    Legend, Li, Link, Main, Mark, Menu, Meta, Meter, Nav, Noscript, HObject, Ol, Optgroup, Option,
+    Output, P, Param, Picture, Pre, Progress, Q, Rb, Rp, Rt, Ruby, S, Samp, Script, Section, Select,
+    Slot, Small, Source, Span, Strong, Style, Sub, Summary, Sup, Table, Tbody, Td, Template,
+    Textarea, Tfoot, Th, Thead, Time, Title, Tr, Track, U, Ul, Var, Video, Wbr, Attributes,
+    Autocomplete, Capture, ClearTagType, Crossorigin, CssClass, DomId, Element, Embedded,
+    EventHandlers, Flow, Global, HDir, Heading, Honeycomb, Html, HtmlAttribute, HtmlDoc,
+    HtmlSerializer, HtmlXml, HttpEquiv, Interactive, Kind, Metadata, Method, Node, NonInteractive,
+    Palpable, Phrasing, Preload, Rel, Rev, Sandbox, Scope, ScriptSupporting, Sectioning, Shape,
+    StartTag, TagType, Target, Type, Wrap}
