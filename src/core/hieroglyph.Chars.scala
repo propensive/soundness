2975cc43b974e8259d22fa84fb1463b5feb09f94
propensive <propensive@users.noreply.github.com>
2025-01-02 07:25:48 +0000

    Apply automatic changes

--- src/core/hieroglyph.Chars.scala
+++ src/core/hieroglyph.Chars.scala
@@ -1,5 +1,5 @@
 /*
-    Hieroglyph, version [unreleased]. Copyright 2025 Jon Pretty, Propensive OÜ.
+    Hieroglyph, version 0.24.0. Copyright 2025 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
