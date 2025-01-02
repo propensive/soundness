bfd45d2f057f541058527b7cb3a94027c1020ed2
propensive <propensive@users.noreply.github.com>
2025-01-02 07:25:26 +0000

    Apply automatic changes

--- src/core/galilei-core.scala
+++ src/core/galilei-core.scala
@@ -1,5 +1,5 @@
 /*
-    Galilei, version [unreleased]. Copyright 2025 Jon Pretty, Propensive OÜ.
+    Galilei, version 0.24.0. Copyright 2025 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
