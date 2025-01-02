b2ad02915ee61b6672cc9d1bba7db86a5ddbcc30
propensive <propensive@users.noreply.github.com>
2025-01-02 07:00:30 +0000

    Apply automatic changes

--- src/core/ethereal.DaemonEvent.scala
+++ src/core/ethereal.DaemonEvent.scala
@@ -1,5 +1,5 @@
 /*
-    Ethereal, version [unreleased]. Copyright 2024 Jon Pretty, Propensive OÜ.
+    Ethereal, version [unreleased]. Copyright 2025 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
