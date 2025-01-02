c8b0ee59e99a0d803aec743febd9816268d667ad
propensive <propensive@users.noreply.github.com>
2025-01-02 06:58:56 +0000

    Apply automatic changes

--- src/core/bifurcate.Buffer.scala
+++ src/core/bifurcate.Buffer.scala
@@ -1,5 +1,5 @@
 /*
-    Bifurcate, version [unreleased]. Copyright 2024 Jon Pretty, Propensive OÜ.
+    Bifurcate, version [unreleased]. Copyright 2025 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
