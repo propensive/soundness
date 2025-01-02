4abcb2c1039888f62f928e0a73aa07bd462c1778
Jon Pretty <jon.pretty@propensive.com>
2025-01-02 07:28:14 +0100

    Add a new `across` infix type

--- src/core/prepositional-core.scala
+++ src/core/prepositional-core.scala
@@ -16,12 +16,13 @@
 
 package prepositional
 
-infix type by    [Type <: { type Operand },    OperandType]    = Type { type Operand    = OperandType }
-infix type from  [Type <: { type Source },     SourceType]     = Type { type Source     = SourceType }
-infix type in    [Type <: { type Format },     FormatType]     = Type { type Format     = FormatType }
-infix type into  [Type <: { type Result },     ResultType]     = Type { type Result     = ResultType }
-infix type of    [Type <: { type Subject },    SubjectType]    = Type { type Subject    = SubjectType }
-infix type on    [Type <: { type Platform },   PlatformType]   = Type { type Platform   = PlatformType }
-infix type onto  [Type <: { type Target },     TargetType]     = Type { type Target     = TargetType }
-infix type over  [Type <: { type Carrier },    CarrierType]    = Type { type Carrier    = CarrierType }
-infix type under [Type <: { type Constraint }, ConstraintType] = Type { type Constraint = ConstraintType }
+infix type across [Type <: { type Domain },     DomainType]     = Type { type Domain     = DomainType }
+infix type by     [Type <: { type Operand },    OperandType]    = Type { type Operand    = OperandType }
+infix type from   [Type <: { type Source },     SourceType]     = Type { type Source     = SourceType }
+infix type in     [Type <: { type Format },     FormatType]     = Type { type Format     = FormatType }
+infix type into   [Type <: { type Result },     ResultType]     = Type { type Result     = ResultType }
+infix type of     [Type <: { type Subject },    SubjectType]    = Type { type Subject    = SubjectType }
+infix type on     [Type <: { type Platform },   PlatformType]   = Type { type Platform   = PlatformType }
+infix type onto   [Type <: { type Target },     TargetType]     = Type { type Target     = TargetType }
+infix type over   [Type <: { type Carrier },    CarrierType]    = Type { type Carrier    = CarrierType }
+infix type under  [Type <: { type Constraint }, ConstraintType] = Type { type Constraint = ConstraintType }
