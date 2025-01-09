2e970a034d6ff399761971bbbf6058c704ffc3f0
propensive <propensive@users.noreply.github.com>
2025-01-09 14:05:24 +0000

    Apply automatic changes

--- src/core/telekinesis.Postable.scala
+++ src/core/telekinesis.Postable.scala
@@ -1,5 +1,5 @@
 /*
-    Telekinesis, version 0.24.0. Copyright 2025 Jon Pretty, Propensive OÜ.
+    Telekinesis, version 0.25.0. Copyright 2025 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
