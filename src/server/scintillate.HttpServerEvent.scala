a2fb25f474b3dbc8de411f86a92e791097faa002
propensive <propensive@users.noreply.github.com>
2025-01-14 18:46:20 +0000

    Apply automatic changes

--- src/server/scintillate.HttpServerEvent.scala
+++ src/server/scintillate.HttpServerEvent.scala
@@ -1,5 +1,5 @@
 /*
-    Scintillate, version [unreleased]. Copyright 2025 Jon Pretty, Propensive OÜ.
+    Scintillate, version 0.26.0. Copyright 2025 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
