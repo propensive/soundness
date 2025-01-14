0d27a03b4d2624048ffe0cebc345f483fd6a32df
propensive <propensive@users.noreply.github.com>
2025-01-14 18:50:08 +0000

    Apply automatic changes

--- src/core/fulminate.Message.scala
+++ src/core/fulminate.Message.scala
@@ -1,5 +1,5 @@
 /*
-    Fulminate, version 0.24.0. Copyright 2025 Jon Pretty, Propensive OÜ.
+    Fulminate, version 0.26.0. Copyright 2025 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
