8bc28a9b7775da138bff4cefc60957b6b025e5c8
Jon Pretty <jon.pretty@propensive.com>
2025-01-15 18:53:46 +0100

    Fixed compile errors

--- src/core/hyperbole.scala
+++ src/core/hyperbole.scala
@@ -18,8 +18,9 @@ package hyperbole
 
 import anticipation.*
 import dendrology.*
+import denominative.*
 import escapade.*
-import escritoire.*, tableStyles.minimal, insufficientSpaceHandling.ignore
+import escritoire.*, tableStyles.minimal, columnAttenuation.ignore
 import gossamer.*
 import hieroglyph.*, textMetrics.uniform
 import rudiments.*
@@ -30,14 +31,16 @@ import scala.quoted.*
 import dotty.tools.*, dotc.util as dtdu
 
 object reflection:
-  def expand[T](expr: Expr[T])(using Quotes): Display =
+  def expand[ValueType](expr: Expr[ValueType])(using Quotes): Teletype =
     import quotes.reflect.*
 
     def init = expr.asTerm.pos.startColumn
 
-    def source(tree: Tree): Display = tree.pos match
+    def source(tree: Tree): Teletype = tree.pos match
       case pos: dtdu.SourcePosition =>
-        ((t" "*(pos.startColumn - init))+pos.lineContent.show.slice(pos.startColumn, pos.endColumn)).display
+        val content =
+          pos.lineContent.show.segment(pos.startColumn.z ~ Ordinal.natural(pos.endColumn))
+        ((t" "*(pos.startColumn - init))+content).teletype
 
       case _ =>
         e""
@@ -50,32 +53,72 @@ object reflection:
         if c.length != expr.length then t"$c..." else expr
 
     object TastyTree:
-      def apply(name: Text, tree: Tree, children: List[TastyTree], parameter: Optional[Text] = Unset)
+      def apply
+         (name: Text, tree: Tree, children: List[TastyTree], parameter: Optional[Text] = Unset)
               : TastyTree =
 
         TastyTree(name, tree.show.show, source(tree).plain, children, parameter)
 
       def expand(tree: Tree): TastyTree = tree match
-        case PackageClause(ref, chs)    => TastyTree(t"PackageClause", tree, expand(ref) :: chs.map(expand))
-        case Bind(name, term)           => TastyTree(t"Bind", tree, List(expand(term)), name.show)
-        case Typed(focus, tt)           => TastyTree(t"Typed", tree, List(expand(focus), expand(tt)))
-        case TypedOrTest(focus, tt)     => TastyTree(t"TypedOrTest", tree, List(expand(focus), expand(tt)))
-        case Inlined(_, _, child)       => TastyTree(t"Inlined", tree, List(expand(child)))
-        case Apply(focus, children)     => TastyTree(t"Apply", tree, expand(focus) :: children.map(expand))
-        case TypeApply(focus, children) => TastyTree(t"TypeApply", tree, expand(focus) :: children.map(expand))
-        case Select(focus, name)        => TastyTree(t"Select", tree, List(expand(focus)), name.show)
-        case Ident(name)                => TastyTree(t"Ident", tree, Nil, name.show)
-        case TypeIdent(name)            => TastyTree(t"TypeIdent", tree, Nil, name.show)
-        case TypeSelect(term, name)     => TastyTree(t"TypeIdent", tree, List(expand(term)), name.show)
-        case Block(statements, last)    => TastyTree(t"Block", tree, statements.map(expand) :+ expand(last))
-        case Closure(focus, _)          => TastyTree(t"Closure", tree, List(expand(focus)))
-        case Literal(value)             => TastyTree(t"Literal", tree, Nil, value.show.show)
-        case Match(focus, cases)        => TastyTree(t"Match", tree, expand(focus) :: cases.map(expand))
-        case Applied(name, tts)         => TastyTree(t"Applied", tree, expand(name) :: tts.map(expand))
-        case Repeated(xs, _)            => TastyTree(t"Repeated", tree, Nil)
-        case Unapply(fn, _, tts)        => TastyTree(t"UnApply", tree, expand(fn) :: tts.map(expand))
-        case tree: TypeTree             => TastyTree(t"TypeTree", tree, Nil)
-        case DefDef(name, ps, typs, ch) => TastyTree(t"DefDef", tree, ch.to(List).map(expand))
+        case PackageClause(ref, chs) =>
+          TastyTree(t"PackageClause", tree, expand(ref) :: chs.map(expand))
+
+        case Bind(name, term) =>
+          TastyTree(t"Bind", tree, List(expand(term)), name.show)
+
+        case Typed(focus, tt) =>
+          TastyTree(t"Typed", tree, List(expand(focus), expand(tt)))
+
+        case TypedOrTest(focus, tt) =>
+          TastyTree(t"TypedOrTest", tree, List(expand(focus), expand(tt)))
+
+        case Inlined(_, _, child) =>
+          TastyTree(t"Inlined", tree, List(expand(child)))
+
+        case Apply(focus, children) =>
+          TastyTree(t"Apply", tree, expand(focus) :: children.map(expand))
+
+        case TypeApply(focus, children) =>
+          TastyTree(t"TypeApply", tree, expand(focus) :: children.map(expand))
+
+        case Select(focus, name) =>
+          TastyTree(t"Select", tree, List(expand(focus)), name.show)
+
+        case Ident(name) =>
+          TastyTree(t"Ident", tree, Nil, name.show)
+
+        case TypeIdent(name) =>
+          TastyTree(t"TypeIdent", tree, Nil, name.show)
+
+        case TypeSelect(term, name) =>
+          TastyTree(t"TypeIdent", tree, List(expand(term)), name.show)
+
+        case Block(statements, last) =>
+          TastyTree(t"Block", tree, statements.map(expand) :+ expand(last))
+
+        case Closure(focus, _) =>
+          TastyTree(t"Closure", tree, List(expand(focus)))
+
+        case Literal(value) =>
+          TastyTree(t"Literal", tree, Nil, value.show.show)
+
+        case Match(focus, cases) =>
+          TastyTree(t"Match", tree, expand(focus) :: cases.map(expand))
+
+        case Applied(name, tts) =>
+          TastyTree(t"Applied", tree, expand(name) :: tts.map(expand))
+
+        case Repeated(xs, _) =>
+          TastyTree(t"Repeated", tree, Nil)
+
+        case Unapply(fn, _, tts) =>
+          TastyTree(t"UnApply", tree, expand(fn) :: tts.map(expand))
+
+        case tree: TypeTree =>
+          TastyTree(t"TypeTree", tree, Nil)
+
+        case DefDef(name, ps, typs, ch) =>
+          TastyTree(t"DefDef", tree, ch.to(List).map(expand))
 
         case CaseDef(focus, t1, t2) =>
           TastyTree(t"CaseDef", tree, expand(focus) +: t1.to(List).map(expand) :+ expand(t2))
@@ -86,14 +129,21 @@ object reflection:
     val tree: TastyTree = TastyTree.expand(expr.asTerm)
 
     val seq: Seq[Expansion] = TreeDiagram.by[TastyTree](_.children)(tree).map: node =>
-      Expansion(tiles.drop(1).map(treeStyles.default.text(_)).join+t"▪ "+node.name, node.param, node.shortCode, node.source)
+      Expansion
+       (tiles.drop(1).map(treeStyles.default.text(_)).join+t"▪ "+node.name,
+        node.param,
+        node.shortCode,
+        node.source)
 
-    Table[Expansion](
-      Column(e"TASTy")(_.text.display),
+    Table[Expansion]
+     (Column(e"TASTy")(_.text.teletype),
       Column(e"Param")(_.param.or(t"")),
       Column(e"Source")(_.source),
-      Column(e"Code")(_.expr)
-    ).tabulate(seq).layout(200).render.join(e"\n")
+      Column(e"Code")(_.expr))
 
+    . tabulate(seq)
+    . grid(200)
+    . render
+    . join(e"\n")
 
 case class Expansion(text: Text, param: Optional[Text], expr: Text, source: Text)
