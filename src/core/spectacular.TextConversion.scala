c2f5160820fde52adcaaac8fb9bc32d9e367c2c9
Jon Pretty <jon.pretty@propensive.com>
2025-01-19 18:15:09 +0100

    Gave lots of `given`s names

--- src/core/spectacular.TextConversion.scala
+++ src/core/spectacular.TextConversion.scala
@@ -25,34 +25,34 @@ object TextConversion:
   given [ValueType: Textualizer] => ValueType is Showable = ValueType.textual(_)
 
   given Text is Showable = identity(_)
-  given String is Showable = _.tt
-  given Char is Showable = char => char.toString.tt
-  given Long is Showable = long => long.toString.tt
-  given Int is Showable = int => int.toString.tt
-  given Short is Showable = short => short.toString.tt
-  given Byte is Showable = byte => byte.toString.tt
-  given Message is Showable = _.text
+  given string: String is Showable = _.tt
+  given char: Char is Showable = char => char.toString.tt
+  given long: Long is Showable = long => long.toString.tt
+  given int: Int is Showable = int => int.toString.tt
+  given short: Short is Showable = short => short.toString.tt
+  given byte: Byte is Showable = byte => byte.toString.tt
+  given message: Message is Showable = _.text
   given double: (decimalizer: DecimalConverter) => Double is Showable = decimalizer.decimalize(_)
   given pid: Pid is Showable = pid => ("\u21af"+pid.value).tt
   given boolean: (booleanStyle: BooleanStyle) => Boolean is Showable = booleanStyle(_)
 
-  given [ValueType: Showable] => Option[ValueType] is Showable =
+  given option: [ValueType: Showable] => Option[ValueType] is Showable =
     _.fold("none".tt)(ValueType.text(_))
 
-  given Uuid is Showable = _.text
-  given Memory is Showable = _.text
-  given [EnumType <: reflect.Enum] => EnumType is Showable = _.toString.tt
+  given uid: Uuid is Showable = _.text
+  given memory: Memory is Showable = _.text
+  given enumeration: [EnumType <: reflect.Enum] => EnumType is Showable = _.toString.tt
 
-  given [ElemType: Showable] => Set[ElemType] is Showable =
+  given set: [ElemType: Showable] => Set[ElemType] is Showable =
     _.map(_.show).mkString("{", ", ", "}").tt
 
-  given [ElemType: Showable] => List[ElemType] is Showable =
+  given list: [ElemType: Showable] => List[ElemType] is Showable =
     _.map(_.show).mkString("[", ", ", "]").tt
 
-  given [ElemType: Showable] => Trie[ElemType] is Showable =
+  given trie: [ElemType: Showable] => Trie[ElemType] is Showable =
     _.map(_.show).mkString("[ ", " ", " ]").tt
 
-  given None.type is Showable = none => "none".tt
+  given none: None.type is Showable = none => "none".tt
 
 trait TextConversion:
   type Self
