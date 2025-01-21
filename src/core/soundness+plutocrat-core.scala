42ae820499fc91ec37393d6727cb190bc9e738d0
Jon Pretty <jon.pretty@propensive.com>
2025-01-21 16:07:05 +0100

    Soundness exports

---
+++ src/core/soundness+plutocrat-core.scala
@@ -0,0 +1,10 @@
+package soundness
+
+export plutocrat.{Currency, CurrencyStyle, Price, Money}
+
+package currencyStyles:
+  export plutocrat.currencyStyles.{local, generic}
+
+package currencies:
+  export plutocrat.currencies.{Eur, Usd, Jpy, Gbp, Cny, Aud, Cad, Chf, Hkd, Sgd, Sek, Krw, Nok, Nzd,
+      Inr, Mxn}
