e85dac17674f077cbae1d62f86c0b9771b785e8b
Jon Pretty <jon.pretty@propensive.com>
2025-01-21 18:44:13 +0100

    Export everything to `soundness`

---
+++ src/core/soundness+octogenarian-core.scala
@@ -0,0 +1,7 @@
+package soundness
+
+export octogenarian.{Commit, Git, GitCommand, GitError, GitEvent, GitPathStatus, GitProcess,
+    GitRefError, GitRepo, GitStatus, Progress, SshUrl, Tag, Branch, CommitHash, Refspec}
+
+package gitCommands:
+  export octogenarian.gitCommands.environmentDefault
