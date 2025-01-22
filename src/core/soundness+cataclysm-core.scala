96defbc75ddc0f22c4f805c83994ba0ff0333175
Jon Pretty <jon.pretty@propensive.com>
2025-01-22 12:35:04 +0100

    Soundness exports

---
+++ src/core/soundness+cataclysm-core.scala
@@ -0,0 +1,9 @@
+package soundness
+
+export cataclysm.{AnimationFillMode, BackgroundRepeat, BorderStyle, Cataclysm, Css, CssProperty,
+    CssRule, CssStyle, CssStylesheet, Cursor, Dir, Display, Duration, Float, Font, FontFace,
+    FontStyle, FontWeight, From, Import, Inherit, Initial, Keyframe, Keyframes, Length, MediaRule,
+    MixBlendMode, Overflow, PointerEvents, Position, PropertyDef, PropertyValue, Selectable,
+    Selector, ShowProperty, TextAlign, TextDecorationLine, TextDecorationStyle, To, Transparent,
+    UserSelect, VerticalAlign, select, >>, ~, ||, &&, ~~, Dimension, max, min, sec, ms, px, pt, in,
+    pc, cm, mm, em, ex, ch, rem, vw, vh, vmin, vmax}
