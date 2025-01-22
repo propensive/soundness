306600493aeab69b7698ba4537303c5759246524
Jon Pretty <jon.pretty@propensive.com>
2025-01-22 11:30:42 +0100

    `soundness` exports

---
+++ src/core/soundness+xylophone-core.scala
@@ -0,0 +1,8 @@
+package soundness
+
+export xylophone.{Attribute, Namespace, StandardXmlPrinter, Xml, XmlAccessError, XmlAst,
+    xmlAttribute, XmlDecoder, XmlEncoder, XmlInterpolation, xmlLabel, XmlName, XmlParseError,
+    XmlPrinter, XmlReadError, x, XmlPath}
+
+package xmlPrinters:
+  export xylophone.xmlPrinters.compact
