114fd27f3190e018ddd2c7460681c7f64f441088
Jon Pretty <jon.pretty@propensive.com>
2025-01-23 20:22:41 +0100

    Changes to whitespace in signatures

--- src/core/cosmopolite.Polyglot.scala
+++ src/core/cosmopolite.Polyglot.scala
@@ -22,7 +22,7 @@ case class Polyglot[+ValueType, LanguageType](values: Map[Language, ValueType]):
   @targetName("or")
   transparent inline infix def | [ValueType2 >: ValueType, LanguageType2]
     (polyglot: Polyglot[ValueType2, LanguageType2])
-          : Polyglot[ValueType2, LanguageType & LanguageType2] | ValueType2 =
+  :     Polyglot[ValueType2, LanguageType & LanguageType2] | ValueType2 =
     compiletime.summonFrom:
       case locale: Locale[LanguageType & LanguageType2] =>
         (values ++ polyglot.values)(locale.language)
