cee29f78f22bc74b6f568e5e0721d4f265888e2c
propensive <propensive@users.noreply.github.com>
2025-01-28 10:37:47 +0000

    Apply automatic changes

--- src/core/telekinesis.Cookie.scala
+++ src/core/telekinesis.Cookie.scala
@@ -1,5 +1,5 @@
 /*
-    Scintillate, version 0.26.0. Copyright 2025 Jon Pretty, Propensive OÜ.
+    Telekinesis, version 0.26.0. Copyright 2025 Jon Pretty, Propensive OÜ.
 
     The primary distribution site is: https://propensive.com/
 
