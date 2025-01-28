05cdca1374b7223ed92c0d52c86f7073f245bc32
Jon Pretty <jon.pretty@propensive.com>
2025-01-28 23:15:57 +0100

    Don't export `sec` because it conflicts with Denominative

--- src/core/soundness+cataclysm-core.scala
+++ src/core/soundness+cataclysm-core.scala
@@ -21,5 +21,5 @@ export cataclysm.{AnimationFillMode, BackgroundRepeat, BorderStyle, Cataclysm, C
     FontStyle, FontWeight, From, Import, Inherit, Initial, Keyframe, Keyframes, Length, MediaRule,
     MixBlendMode, Overflow, PointerEvents, Position, PropertyDef, PropertyValue, Selectable,
     Selector, ShowProperty, TextAlign, TextDecorationLine, TextDecorationStyle, To, Transparent,
-    UserSelect, VerticalAlign, select, >>, ~, ||, &&, ~~, Dimension, max, min, sec, ms, px, pt, in,
+    UserSelect, VerticalAlign, select, >>, ~, ||, &&, ~~, Dimension, max, min, ms, px, pt, in,
     pc, cm, mm, em, ex, ch, rem, vw, vh, vmin, vmax}
