f499186f65d1a51ca37a78187a70759aaf931e48
Jon Pretty <jon.pretty@propensive.com>
2025-02-01 16:51:42 +0100

    Reinstate `SpecificDuration`/`GenericDuration` for now

---
+++ src/time/anticipation.GenericDuration.scala
@@ -0,0 +1,27 @@
+/*
+    Anticipation, version 0.26.0. Copyright 2025 Jon Pretty, Propensive OÜ.
+
+    The primary distribution site is: https://propensive.com/
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+    file except in compliance with the License. You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+    either express or implied. See the License for the specific language governing permissions
+    and limitations under the License.
+*/
+
+package anticipation
+
+import language.experimental.captureChecking
+
+trait GenericDuration:
+  type Self
+  def milliseconds(duration: Self): Long
+  def nanoseconds(duration: Self): Long = milliseconds(duration)*1_000_000L
+
+object GenericDuration:
+  given Long is GenericDuration = identity(_)
