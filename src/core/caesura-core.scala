a3f4d637df64a10658a4b1290d231f9f58cc57f1
Jon Pretty <jon.pretty@propensive.com>
2025-02-11 10:26:03 +0100

    Squashed 'lib/caesura/' content from commit 04fd9acd
    git-subtree-dir: lib/caesura
    git-subtree-split: 04fd9acdc999859b50abb57c8cd2c15c7aded288

---
+++ src/core/caesura-core.scala
@@ -0,0 +1,40 @@
+/*
+    Caesura, version 0.26.0. Copyright 2025 Jon Pretty, Propensive OÜ.
+
+    The primary distribution site is: https://propensive.com/
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+    file except in compliance with the License. You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+    either express or implied. See the License for the specific language governing permissions
+    and limitations under the License.
+*/
+
+package caesura
+
+import gossamer.*
+import proscenium.*
+
+package dsvFormats:
+  given csv: DsvFormat = DsvFormat(false, ',', '"', '"')
+  given csvWithHeader: DsvFormat = DsvFormat(true, ',', '"', '"')
+  given tsv: DsvFormat = DsvFormat(false, '\t', '"', '"')
+  given tsvWithHeader: DsvFormat = DsvFormat(true, '\t', '"', '"')
+  given ssv: DsvFormat = DsvFormat(false, ' ', '"', '"')
+  given ssvWithHeader: DsvFormat = DsvFormat(true, ' ', '"', '"')
+
+package dsvRedesignations:
+  given unchanged: DsvRedesignation = identity(_)
+  given lowerDotted: DsvRedesignation = _.uncamel.map(_.lower).join(t" ")
+  given lowerSlashed: DsvRedesignation = _.uncamel.map(_.lower).join(t" ")
+  given capitalizedWords: DsvRedesignation = _.uncamel.map(_.capitalize).join(t" ")
+  given lowerWords: DsvRedesignation = _.uncamel.map(_.lower).join(t" ")
+
+extension [ValueType: DsvEncodable](value: ValueType) def dsv: Row = ValueType.encode(value)
+
+extension [ValueType: DsvEncodable](value: Seq[ValueType])
+  def dsv: Dsv = Dsv(value.to(Stream).map(ValueType.encode(_)))
