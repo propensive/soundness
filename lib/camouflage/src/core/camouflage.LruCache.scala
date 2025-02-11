d8c4b49f80da7f51453e70ed5000381f60695682
Jon Pretty <jon.pretty@propensive.com>
2025-02-11 14:38:06 +0100

    Fix URL query strings (#135)
    * Initial commit
    
    * Apply automatic changes
    
    * Added features documentation
    
    * Apply automatic changes
    
    * Added intro text
    
    * Apply automatic changes
    
    * Add basics text
    
    * Added name
    
    * Apply automatic changes
    
    * Added a status
    
    * Moved documention out of sources back to where it should be
    
    * Described logo
    
    * Apply automatic changes
    
    * Added slogan
    
    * Apply automatic changes
    
    * Remove `toString`
    
    * Apply automatic changes
    
    * Update to latest Wrath version
    
    * Use `let` instead of `mm`
    
    * Import Vacuous to eliminate compile error
    
    * Apply automatic changes
    
    * Fixed errors in docs
    
    * Apply automatic changes
    
    * Use latest Tumult
    
    * Trigger documentation rebuild
    
    * Apply automatic changes
    
    * Use latest Tumult docs
    
    * Apply automatic changes
    
    * Don't depend on Spectacular repository
    
    * Include contributing docs
    
    * Really include contributing docs
    
    * Apply automatic changes
    
    * Rename Perforate to Contingency in docs
    
    * Apply automatic changes
    
    * Changes for Contingency
    
    * Apply automatic changes
    
    * Update to most recent Tumult
    
    * Reorganized sources and exported to `soundness` package
    
    * Apply automatic changes
    
    * Add a simple cached value
    
    * Add POM file
    
    * Update pom.xml to version 0.17.0
    
    * Don't do coverage
    
    * Update pom.xml to version 0.18.0
    
    * Update pom.xml to version 0.19.0
    
    * Update pom.xml to version 0.20.0
    
    * Update pom.xml to version 0.21.0
    
    * Update pom.xml to version 0.22.0
    
    * Avoid raises `ConcurrencyError` in `establish`
    
    * Fixed cache
    
    * Update pom.xml to version 0.23.0
    
    * Whitespace changes
    
    * Reordering of imports
    
    * Update Tumult and regenerate
    
    * Apply automatic changes
    
    * Update pom.xml to version 0.24.0
    
    * Update Tumult and regenerate
    
    * Apply automatic changes
    
    * Update Tumult and regenerate
    
    * Apply automatic changes
    
    * Update Tumult and regenerate
    
    * Apply automatic changes
    
    * Update pom.xml to version 0.25.0
    
    * Update pom.xml to version 0.26.0
    
    * Renamed tests file
    
    * Apply automatic changes
    
    * Remove broken CI
    
    * Eliminated long lines
    
    * Use safer optional values, and other enabling changes
    
    * Use generalized generic/specific instants
    
    * Squashed 'lib/embarcadero/' changes from 29b880e..da28509
    
    da28509 Merge branch 'main' of github.com:propensive/embarcadero
    429bbca Remove old `fury` file
    
    git-subtree-dir: lib/embarcadero
    git-subtree-split: da28509e85472e7f602dacf7f0ad972457d0e0c0
    
    * Fix problems with query strings in URLs
    
    ---------
    
    Co-authored-by: propensive <propensive@users.noreply.github.com>

---
+++ lib/camouflage/src/core/camouflage.LruCache.scala
@@ -0,0 +1,65 @@
+/*
+    Camouflage, version 0.26.0. Copyright 2025 Jon Pretty, Propensive OÜ.
+
+    The primary distribution site is: https://propensive.com/
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+    file except in compliance with the License. You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+    either express or implied. See the License for the specific language governing permissions
+    and limitations under the License.
+*/
+
+package camouflage
+
+import rudiments.*
+import vacuous.*
+
+import scala.collection.mutable as scm
+
+import java.util.concurrent.atomic as juca
+
+class LruCache[KeyType, ValueType](maxSize: Int):
+  private val counter: juca.AtomicInteger = juca.AtomicInteger(0)
+  private val values: scm.HashMap[Int, ValueType] = scm.HashMap()
+  private val ids: scm.HashMap[KeyType, Int] = scm.HashMap()
+  private val keys: scm.TreeMap[Int, KeyType] = scm.TreeMap()
+
+  protected def evict(): Unit = while values.size > maxSize do
+    val id = keys.firstKey
+    val key = keys(id)
+    ids -= key
+    keys -= id
+    values -= id
+
+  protected def touch(oldId: Int, newId: Int, key: KeyType, value: ValueType): Unit =
+    keys(newId) = key
+    values(newId) = value
+    ids(key) = newId
+    keys -= oldId
+    values -= oldId
+
+  def contains(key: KeyType): Boolean = ids.contains(key)
+
+  def remove(key: KeyType): Unit =
+    ids.getOrElse(key, Unset).let: id =>
+      values -= id
+      ids -= key
+      keys -= id
+
+  def apply(key: KeyType)(value: => ValueType): ValueType =
+    val newId = counter.getAndIncrement()
+
+    ids.getOrElse(key, Unset).let: oldId =>
+      values(oldId).tap(touch(oldId, newId, key, _))
+
+    . or:
+        value.tap: value =>
+          values(newId) = value
+          ids(key) = newId
+          keys(newId) = key
+          evict()
