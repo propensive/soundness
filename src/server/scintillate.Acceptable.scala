59b764c079cee84d045210e4025b0991997a11a4
Jon Pretty <jon.pretty@propensive.com>
2025-02-11 10:33:13 +0100

    Squashed 'lib/scintillate/' content from commit f7862e3b
    git-subtree-dir: lib/scintillate
    git-subtree-split: f7862e3b436789c2ca3d6e711f5463dfe76bd3ed

---
+++ src/server/scintillate.Acceptable.scala
@@ -0,0 +1,50 @@
+/*
+    Scintillate, version 0.26.0. Copyright 2025 Jon Pretty, Propensive OÜ.
+
+    The primary distribution site is: https://propensive.com/
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+    file except in compliance with the License. You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+    either express or implied. See the License for the specific language governing permissions
+    and limitations under the License.
+*/
+
+package scintillate
+
+import anticipation.*
+import contingency.*
+import fulminate.*
+import gesticulate.*
+import gossamer.*
+import rudiments.*
+import telekinesis.*
+import turbulence.*
+import vacuous.*
+
+import errorDiagnostics.stackTraces
+
+
+object Acceptable:
+  given Tactic[MultipartError] => Multipart is Acceptable = request =>
+    tend:
+      case _: MediaTypeError => MultipartError(MultipartError.Reason.MediaType)
+
+    . within:
+        val contentType = request.headers.contentType.prim.or:
+          abort(MultipartError(MultipartError.Reason.MediaType))
+
+        if contentType.base == media"multipart/form-data" then
+          val boundary = contentType.at(t"boundary").or:
+            abort(MultipartError(MultipartError.Reason.MediaType))
+
+          Multipart.parse(request.body, boundary)
+        else abort(MultipartError(MultipartError.Reason.MediaType))
+
+trait Acceptable:
+  type Self
+  def accept(request: HttpRequest): Self
---
+++ src/servlet/scintillate.Scintillate.scala
@@ -0,0 +1,50 @@
+/*
+    Scintillate, version 0.26.0. Copyright 2025 Jon Pretty, Propensive OÜ.
+
+    The primary distribution site is: https://propensive.com/
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+    file except in compliance with the License. You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+    either express or implied. See the License for the specific language governing permissions
+    and limitations under the License.
+*/
+
+package scintillate
+
+import scala.annotation.*
+import scala.quoted.*
+
+import fulminate.*
+import telekinesis.*
+
+class servlet extends MacroAnnotation:
+  override def transform(using Quotes)
+     (tree: quotes.reflect.Definition, companion: Option[quotes.reflect.Definition])
+  :     List[quotes.reflect.Definition] =
+    import quotes.reflect.*
+
+    tree match
+      case defDef@DefDef(name, params, returnType, Some(body)) =>
+        if !(returnType.tpe <:< TypeRepr.of[HttpResponse])
+        then halt(m"the return type ${returnType.show} is not a subtype of HttpResponse[?]")
+
+        val ref =
+          Ref(defDef.symbol).etaExpand(tree.symbol.owner).asExprOf[HttpConnection => HttpResponse]
+
+        val parents0 = List('{new JavaServletFn($ref)}.asTerm)
+        val parents = List(TypeTree.of[HttpConnection])
+        val newClassName = Symbol.freshName(name)
+
+        val cls =
+          Symbol.newClass(Symbol.spliceOwner, name, parents.map(_.tpe), _ => Nil, selfType = None)
+
+        val clsDef = ClassDef(cls, parents, body = Nil)
+        List(tree, clsDef)
+
+      case other =>
+        halt(m"the @servlet annotation must be applied to a method")
