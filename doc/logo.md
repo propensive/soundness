65269dd12811fcb5ae41e79518b0a0b92d1043b2
Jon Pretty <jon.pretty@propensive.com>
2025-02-11 10:29:42 +0100

    Squashed 'lib/hellenism/' content from commit 7d1bf1dca
    git-subtree-dir: lib/hellenism
    git-subtree-split: 7d1bf1dca7758ad89ffb7e102d0204ec9175ede7

---
+++ doc/logo.md
@@ -0,0 +1,3 @@
+The logo shows a simplistic rendering of the Parthenon on the Acropolis in
+Athens, a symbol of Hellenic culture.
+
---
+++ src/core/hellenism.Classloader.scala
@@ -0,0 +1,43 @@
+/*
+    Hellenism, version 0.26.0. Copyright 2025 Jon Pretty, Propensive OÜ.
+
+    The primary distribution site is: https://propensive.com/
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+    file except in compliance with the License. You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+    either express or implied. See the License for the specific language governing permissions
+    and limitations under the License.
+*/
+
+package hellenism
+
+import java.io as ji
+import java.net as jn
+
+import anticipation.*
+import contingency.*
+import serpentine.*
+import vacuous.*
+
+object Classloader:
+  def threadContext: Classloader = new Classloader(Thread.currentThread.nn.getContextClassLoader.nn)
+  inline def apply[ClassType <: AnyKind]: Classloader = ClassRef[ClassType].classloader
+
+class Classloader(val java: ClassLoader) extends Root("/".tt, "/".tt, Case.Sensitive):
+  type Platform = Classpath
+  override def parent: Optional[Classloader] = Optional(java.getParent).let(new Classloader(_))
+
+  protected def urlClassloader: Optional[jn.URLClassLoader] = java match
+    case java: jn.URLClassLoader => java
+    case _                       => parent.let(_.urlClassloader)
+
+  def classpath: Optional[Classpath] = urlClassloader.let(Classpath(_))
+
+  private[hellenism] def inputStream(path: Text)(using notFound: Tactic[ClasspathError])
+  :     ji.InputStream =
+    Optional(java.getResourceAsStream(path.s)).or(abort(ClasspathError(path)))
