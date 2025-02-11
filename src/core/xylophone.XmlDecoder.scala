31c7139ae6ffcce77110435d81c7bf885db3b6e1
Jon Pretty <jon.pretty@propensive.com>
2025-02-11 10:34:39 +0100

    Squashed 'lib/xylophone/' content from commit 02bdf8b8c
    git-subtree-dir: lib/xylophone
    git-subtree-split: 02bdf8b8ce9d0dcfaf9d1a68214cc63f3c36630f

---
+++ src/core/xylophone.XmlDecoder.scala
@@ -0,0 +1,63 @@
+/*
+    Xylophone, version [unreleased]. Copyright 2025 Jon Pretty, Propensive OÜ.
+
+    The primary distribution site is: https://propensive.com/
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+    file except in compliance with the License. You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+    either express or implied. See the License for the specific language governing permissions
+    and limitations under the License.
+*/
+
+package xylophone
+
+import anticipation.*
+import contingency.*
+import prepositional.*
+import proscenium.*
+import wisteria.*
+
+trait XmlDecoder[ValueType]:
+  def read(xml: List[XmlAst]): ValueType
+
+  def map[ValueType2](lambda: ValueType => ValueType2): XmlDecoder[ValueType2] =
+    list => lambda(read(list))
+
+object XmlDecoder extends Derivation[XmlDecoder]:
+  given text(using Tactic[XmlReadError]): XmlDecoder[Text] = list =>
+    val elements = childElements(list).collect { case XmlAst.Textual(text) => text }
+    if elements.length == 0 then raise(XmlReadError(), "".tt) else elements.head
+
+  given [ValueType: Decodable in Text]: XmlDecoder[ValueType] = value =>
+    value.absolve match
+      case XmlAst.Element(_, XmlAst.Textual(text) :: _, _, _) +: _ => text.decode[ValueType]
+
+  inline def join[DerivationType <: Product: ProductReflection]: XmlDecoder[DerivationType] =
+    list =>
+      val elements = childElements(list)
+
+      construct:
+        [FieldType] => context =>
+          val element =
+            elements.find:
+              case element: XmlAst.Element if element.name.name == label => true
+              case _                                                     => false
+
+            . get
+
+          context.read(List(element))
+
+  inline def split[DerivationType: SumReflection]: XmlDecoder[DerivationType] = list =>
+    list.head.absolve match
+      case XmlAst.Element(_, children, attributes, _) =>
+        delegate(attributes.get(XmlName("type".tt)).get):
+          [VariantType <: DerivationType] => decoder =>
+            decoder.read(list)
+
+  private def childElements(seq: List[XmlAst]): Seq[XmlAst] =
+    seq.collect { case e@XmlAst.Element(_, children, _, _) => children }.flatten
