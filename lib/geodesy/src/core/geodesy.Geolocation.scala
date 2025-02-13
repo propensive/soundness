1a4d829fb21e56b00ead345dc43aef9128b3b58b
Jon Pretty <jon.pretty@propensive.com>
2025-02-13 22:11:49 +0100

    Allow construction of `Location` in millidegrees (#150)

--- lib/geodesy/src/core/geodesy.Geolocation.scala
+++ lib/geodesy/src/core/geodesy.Geolocation.scala
@@ -114,6 +114,6 @@ object Geolocation:
 case class Geolocation
    (location:    Location,
     altitude:    Optional[Double] = Unset,
-    crs:      Optional[Text]   = Unset,
+    crs:         Optional[Text]   = Unset,
     uncertainty: Optional[Double] = Unset,
     parameters:  Map[Text, Text]  = Map())
