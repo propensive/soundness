1570a4e644499da2adc6a24a0518cbbbae40c839
Jon Pretty <jon.pretty@propensive.com>
2025-02-20 20:17:17 +0100

    Add missing header files (#175)

--- lib/rudiments/src/core/soundness+rudiments-core.scala
+++ lib/rudiments/src/core/soundness+rudiments-core.scala
@@ -37,7 +37,7 @@ export rudiments
     interleave, each, all, sumBy, bi, tri, indexBy, longestTrain, mutable, immutable, snapshot,
     place, upsert, collate, establish, plus, runs, runsBy, Cursor, cursor, precursor, postcursor,
     cursorIndex, cursorOffset, curse, ult, create, javaInputStream, DecimalConverter, !!,
-    Exit, unit, waive, twin, triple, is, matchable, give, pipe,
+    Exit, unit, waive, twin, triple, is, matchable, give, pipe, fuse,
     tap, also, Counter, loop, Loop, &, tuple, to, WorkingDirectoryError, HomeDirectoryError,
     WorkingDirectory, HomeDirectory, workingDirectory, homeDirectory, prim, sec,
     ter, unwind, at, Indexable, yet, Bijection, bijection, segment, Segmentable, Digit,
