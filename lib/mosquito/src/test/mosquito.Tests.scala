551d0a8c6d3f8ec2613278a437b79da2b69746cb
Jon Pretty <jon.pretty@propensive.com>
2025-03-05 10:51:20 +0100

    Fix tests for Mosquito

--- lib/mosquito/src/test/mosquito.Tests.scala
+++ lib/mosquito/src/test/mosquito.Tests.scala
@@ -42,64 +42,62 @@ import spectacular.*
 
 object Tests extends Suite(t"Mosquito tests"):
   def run(): Unit =
-    test(t"Create a Euclidean of Ints"):
-      Euclidean(1, 2, 3)
-    .assert(_ == Euclidean(1, 2, 3))
+    test(t"Create a Vector of Ints"):
+      Vector(1, 2, 3)
+    .assert(_ == Vector(1, 2, 3))
 
-    test(t"A Euclidean of Ints infers the correct size"):
+    test(t"A Vector of Ints infers the correct size"):
       demilitarize:
-        val vector: Euclidean[Int, 3] = Euclidean(1, 3, 4)
+        val vector: Vector[Int, 3] = Vector(1, 3, 4)
       .map(_.message)
     .assert(_ == Nil)
 
-    // test(t"Type error if size is incorrect"):
-    //   demilitarize:
-    //     val vector: Euclidean[Int, 2] = Euclidean(1, 3, 4)
-    //   .map(_.id)
-    // .assert(_ == List(CompileErrorId.TypeMismatch))
+    test(t"Type error if size is incorrect"):
+      demilitarize:
+        val vector: Vector[Int, 2] = Vector(1, 3, 4)
+    .assert(_.nonEmpty)
 
-    // test(t"Type error if type is incorrect"):
-    //   demilitarize:
-    //     val vector: Euclidean[String, 3] = Euclidean(1, 3, 4)
-    //   .map(_.id)
-    // .assert(_ == List(CompileErrorId.TypeMismatch))
+    test(t"Type error if type is incorrect"):
+      demilitarize:
+        val vector: Vector[String, 3] = Vector(1, 3, 4)
+    .assert(_.nonEmpty)
 
     test(t"Calculate integer dot-product"):
-      Euclidean(1, 2, 3).dot(Euclidean(4, 3, 7))
+      Vector(1, 2, 3).dot(Vector(4, 3, 7))
     .assert(_ == 31)
 
     test(t"Calculate Double dot-product"):
-      Euclidean(0.1, 0.2, 0.3).dot(Euclidean(0.4, 0.3, 0.7))
-    .assert(_ meets 0.31 +/- 0.000001)
+      Vector(0.1, 0.2, 0.3).dot(Vector(0.4, 0.3, 0.7))
+    .assert(_ === 0.31 +/- 0.000001)
 
     test(t"Calculate integer cross-product"):
-      Euclidean(1, 2, 3).cross(Euclidean(4, 3, 7))
-    .assert(_ == Euclidean(5, 5, -5))
+      Vector(1, 2, 3).cross(Vector(4, 3, 7))
+    .assert(_ == Vector(5, 5, -5))
 
     test(t"Calculate Double cross-product"):
-      Euclidean(1.4, 2.4, 3.8).cross(Euclidean(4.9, 3.6, 0.7))
-    .assert(_ == Euclidean(-12.0, 17.64, -6.72))
+      Vector(1.4, 2.4, 3.8).cross(Vector(4.9, 3.6, 0.7))
+    .assert(_ == Vector(-12.0, 17.64, -6.72))
 
-    test(t"Show Euclidean 3-vector"):
-      Euclidean(1, 3, 6).show
+    test(t"Show Vector 3-vector"):
+      Vector(1, 3, 6).show
     .assert(_ == t"\u239b 1 \u239e\n\u239c 3 \u239f\n\u239d 6 \u23a0")
 
     test(t"Add two vectors"):
-      Euclidean(1, 2, 3) + Euclidean(3, 4, 5)
-    .assert(_ == Euclidean(4, 6, 8))
+      Vector(1, 2, 3) + Vector(3, 4, 5)
+    .assert(_ == Vector(4, 6, 8))
 
     suite(t"Quantity operations"):
       test(t"Add two quantity vectors"):
-        Euclidean(1*Metre, 2*Metre, 3*Metre) + Euclidean(3*Metre, 4*Metre, 5*Metre)
-      .assert(_ == Euclidean(4*Metre, 6*Metre, 8*Metre))
+        Vector(1*Metre, 2*Metre, 3*Metre) + Vector(3*Metre, 4*Metre, 5*Metre)
+      .assert(_ == Vector(4*Metre, 6*Metre, 8*Metre))
 
       test(t"Add two mixed-quantity vectors"):
-        Euclidean(1*Foot, 1*Foot, 1*Foot) + Euclidean(3*Metre, 4*Metre, 5*Metre)
-      .assert(_ == Euclidean(3.3048*Metre, 4.3048*Metre, 5.3048*Metre))
+        Vector(1*Foot, 1*Foot, 1*Foot) + Vector(3*Metre, 4*Metre, 5*Metre)
+      .assert(_ == Vector(3.3048*Metre, 4.3048*Metre, 5.3048*Metre))
 
       test(t"Map from m to m²"):
-        Euclidean(1*Metre, 2*Metre, 3*Metre, 4*Metre).map(_*Metre)
-      .assert(_ == Euclidean(1*Metre*Metre, 2*Metre*Metre, 3*Metre*Metre, 4*Metre*Metre))
+        Vector(1*Metre, 2*Metre, 3*Metre, 4*Metre).map(_*Metre)
+      .assert(_ == Vector(1*Metre*Metre, 2*Metre*Metre, 3*Metre*Metre, 4*Metre*Metre))
 
     suite(t"Matrix tests"):
       val m1 = Matrix[2, 3]((1, 2, 3), (4, 5, 6))
