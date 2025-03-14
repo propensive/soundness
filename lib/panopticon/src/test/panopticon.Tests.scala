4d680848c8d79882c565f6c5b9021292b89ca306
Jon Pretty <jon.pretty@propensive.com>
2025-03-14 15:32:31 +0100

    Improve Panopticon tests

--- lib/panopticon/src/test/panopticon.Tests.scala
+++ lib/panopticon/src/test/panopticon.Tests.scala
@@ -32,9 +32,7 @@
                                                                                                   */
 package panopticon
 
-import gossamer.*
-import larceny.*
-import probably.*
+import soundness.*
 
 case class Organization(name: String, leader: Person)
 case class Person(name: String, age: Int, role: Role)
@@ -50,13 +48,13 @@ object Tests extends Suite(t"Panopticon tests"):
     test(t"Check that non-existant fields are inaccessible"):
       demilitarize:
         Lens[Organization](_.age)
-      .map(_.message)
-    .assert(_ == List("panopticon:  the field age is not a member of panopticon.Organization"))
+      . map(_.message)
+    .assert(_ == List("panopticon: the field age is not a member of panopticon.Organization"))
 
     test(t"Check that indirect non-existant fields are inaccessible"):
       demilitarize(Lens[Organization](_.leader.size)).map(_.message)
 
-    . assert(_ == List("panopticon:  the field size is not a member of panopticon.Person"))
+    . assert(_ == List("panopticon: the field size is not a member of panopticon.Person"))
 
     test(t"Check that two compatible lenses can be added"):
       val orgLeader = Lens[Organization](_.leader)
@@ -64,13 +62,13 @@ object Tests extends Suite(t"Panopticon tests"):
       orgLeader ++ personName
     .assert()
 
-    // test(t"Check that two incompatible lenses can be added"):
-    //   demilitarize:
-    //     val orgLeader = Lens[Organization](_.leader)
-    //     val roleName = Lens[Role](_.name)
-    //     orgLeader ++ roleName
-    //   .map(_.errorId)
-    // .assert(_ == List(ErrorId.TypeMismatchID))
+    test(t"Check that two incompatible lenses can be added"):
+      demilitarize:
+        val orgLeader = Lens[Organization](_.leader)
+        val roleName = Lens[Role](_.name)
+        orgLeader ++ roleName
+      .map(_.id)
+    .assert(_.nonEmpty)
 
     val ceo = Role("CEO", 120000)
     val leader = Person("Jack Smith", 59, ceo)
@@ -115,12 +113,12 @@ object Tests extends Suite(t"Panopticon tests"):
       lens.get(date)
     .assert(_ == 3)
 
-    // val orgName = new Lens[Organization, "name" *: Zero, String](_.name, (org, name) => org.copy(name = name))
+    val orgName = new Lens[Organization, Mono["name"], String](_.name, (org, name) => org.copy(name = name))
 
-    // test(t"Manual lens can access field"):
-    //   orgName(org)
-    // .assert(_ == "Acme Inc")
+    test(t"Manual lens can access field"):
+      orgName(org)
+    .assert(_ == "Acme Inc")
 
-    // test(t"Manual lens can update field"):
-    //   orgName(org) = "Emca Inc"
-    // .assert(_.name == "Emca Inc")
+    test(t"Manual lens can update field"):
+      orgName(org) = "Emca Inc"
+    .assert(_.name == "Emca Inc")
