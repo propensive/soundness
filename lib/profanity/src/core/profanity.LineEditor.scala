2f584d853263134d0574de4dc356330f7c86e861
Jon Pretty <jon.pretty@propensive.com>
2025-03-24 17:50:06 +0100

    Renamed type parameters in Profanity

--- lib/profanity/src/core/profanity.LineEditor.scala
+++ lib/profanity/src/core/profanity.LineEditor.scala
@@ -79,9 +79,9 @@ case class LineEditor(value: Text = t"", position0: Optional[Int] = Unset) exten
 
   def ask
      (using interactivity: Interactivity[TerminalEvent], interaction: Interaction[Text, LineEditor])
-     [ResultType]
-     (lambda: Interactivity[TerminalEvent] ?=> Text => ResultType)
-  :     ResultType raises DismissError =
+     [result]
+     (lambda: Interactivity[TerminalEvent] ?=> Text => result)
+  :     result raises DismissError =
 
     interaction(interactivity.eventStream(), this)(_(_)).lay(abort(DismissError())):
       (result, stream) => lambda(using Interactivity(stream))(result)
