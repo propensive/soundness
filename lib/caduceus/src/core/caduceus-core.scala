b31744f7bca7696bbec09b443bbe9f47dc8a3a91
Jon Pretty <jon.pretty@propensive.com>
2025-05-09 11:05:34 +0200

    Work in progress on Caduceus

---
+++ lib/caduceus/src/core/caduceus-core.scala
@@ -0,0 +1,81 @@
+                                                                                                  /*
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃                                                                                                  ┃
+┃                                                   ╭───╮                                          ┃
+┃                                                   │   │                                          ┃
+┃                                                   │   │                                          ┃
+┃   ╭───────╮╭─────────╮╭───╮ ╭───╮╭───╮╌────╮╭────╌┤   │╭───╮╌────╮╭────────╮╭───────╮╭───────╮   ┃
+┃   │   ╭───╯│   ╭─╮   ││   │ │   ││   ╭─╮   ││   ╭─╮   ││   ╭─╮   ││   ╭─╮  ││   ╭───╯│   ╭───╯   ┃
+┃   │   ╰───╮│   │ │   ││   │ │   ││   │ │   ││   │ │   ││   │ │   ││   ╰─╯  ││   ╰───╮│   ╰───╮   ┃
+┃   ╰───╮   ││   │ │   ││   │ │   ││   │ │   ││   │ │   ││   │ │   ││   ╭────╯╰───╮   │╰───╮   │   ┃
+┃   ╭───╯   ││   ╰─╯   ││   ╰─╯   ││   │ │   ││   ╰─╯   ││   │ │   ││   ╰────╮╭───╯   │╭───╯   │   ┃
+┃   ╰───────╯╰─────────╯╰────╌╰───╯╰───╯ ╰───╯╰────╌╰───╯╰───╯ ╰───╯╰────────╯╰───────╯╰───────╯   ┃
+┃                                                                                                  ┃
+┃    Soundness, version 0.27.0.                                                                    ┃
+┃    © Copyright 2021-25 Jon Pretty, Propensive OÜ.                                                ┃
+┃                                                                                                  ┃
+┃    The primary distribution site is:                                                             ┃
+┃                                                                                                  ┃
+┃        https://soundness.dev/                                                                    ┃
+┃                                                                                                  ┃
+┃    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file     ┃
+┃    except in compliance with the License. You may obtain a copy of the License at                ┃
+┃                                                                                                  ┃
+┃        https://www.apache.org/licenses/LICENSE-2.0                                               ┃
+┃                                                                                                  ┃
+┃    Unless required by applicable law or agreed to in writing,  software distributed under the    ┃
+┃    License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,    ┃
+┃    either express or implied. See the License for the specific language governing permissions    ┃
+┃    and limitations under the License.                                                            ┃
+┃                                                                                                  ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+                                                                                                  */
+package caduceus
+
+import anticipation.*
+import gesticulate.*
+import nettlesome.*
+import proscenium.*
+
+case class Email[sendable: Sendable]
+            (subject:     Text,
+             headers:     Map[Text, Text],
+             content:     sendable,
+             contentType: MediaType,
+             attachments: List[Attachment])
+
+case class Envelope[sendable: Sendable]
+            (from:        EmailAddress,
+             to:          List[EmailAddress],
+             cc:          List[EmailAddress],
+             bcc:         List[EmailAddress],
+             email:       Email,
+             attachments: List[Attachment])
+
+case class Attachment()
+
+object Sendable
+
+trait Sendable:
+  type Self
+  def email(content: Self): Email
+
+case class Sender(email: EmailAddress)
+
+extension [sendable: Sendable](value: sendable)
+  def send
+       (to:  EmailAddress | List[EmailAddress],
+        cc:  EmailAddress | List[EmailAddress],
+        bcc: EmailAddress | List[EmailAddress])
+       (using sender: Sender): Send =
+
+   def many[entity](value: entity | List[entity]): List[entity] = value match
+     case many: List[`entity` @unchecked] => many
+     case one: (`entity` @unchecked)      => List(one)
+
+   Envelope[sendable](sender.email, many(to),
+
+
+trait Courier:
+  type Result
+  def send(message: Envelope): Result
