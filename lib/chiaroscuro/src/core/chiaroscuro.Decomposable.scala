d4b44cfe9274ecc0e2bff2e21da936feef664c69
Jon Pretty <jon.pretty@propensive.com>
2025-06-30 17:22:47 +0200

    Further cleanup

--- lib/chiaroscuro/src/core/chiaroscuro.Decomposable.scala
+++ lib/chiaroscuro/src/core/chiaroscuro.Decomposable.scala
@@ -52,22 +52,26 @@ trait Decomposable extends Typeclass:
   def decomposition(value: Self): Decomposition
 
 object Decomposable extends Decomposable2:
+  trait Base extends Decomposable:
+    def decomposition(value: Self): Decomposition
+
   given list: [element, collection <: List[element]]
         => (decomposable: => element is Decomposable)
-        => collection is Base =
-    collection =>
-      Decomposition.Sequence
-       (t"List", collection.map(decomposable.decomposition(_)).to(List), collection)
+        => collection is Decomposable =
+    list =>
+      Decomposition.Sequence(t"List", list.map(decomposable.decomposition(_)), list)
 
   given trie: [element, collection <: Trie[element]]
         => (decomposable: => element is Decomposable)
-        => collection is Base =
-    collection =>
-      Decomposition.Sequence
-       (t"Trie", collection.map(decomposable.decomposition(_)).to(Trie), collection)
+        => collection is Decomposable =
+    trie =>
+      Decomposition.Sequence(t"Trie", trie.map(decomposable.decomposition(_)).to(List), trie)
 
-  trait Base extends Decomposable:
-    def decomposition(value: Self): Decomposition
+  given iarray: [element]
+        => (decomposable: => element is Decomposable)
+        => IArray[element] is Decomposable =
+    iarray =>
+      Decomposition.Sequence(t"IArray", iarray.map(decomposable.decomposition(_)).to(List), iarray)
 
   object Base:
     given text: Text is Base =
