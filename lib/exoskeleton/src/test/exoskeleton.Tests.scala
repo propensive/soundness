072b3459d52ea43b5231a98fa1a61bcc8329caf6
Jon Pretty <jon.pretty@propensive.com>
2025-10-07 11:45:45 +0200

    Try warming up `zsh` and other shells before first tests

--- lib/exoskeleton/src/test/exoskeleton.Tests.scala
+++ lib/exoskeleton/src/test/exoskeleton.Tests.scala
@@ -98,7 +98,11 @@ object Tests extends Suite(m"Exoskeleton Tests"):
           t"finished"  }
 
     . sandbox:
-        println(summon[Sandbox.Tool].path.encode)
+        // Warmup runs to avoid timing issues in CI
+        Bash.tmux()(Tmux.completions(t""))
+        Zsh.tmux()(Tmux.completions(t""))
+        Fish.tmux(width = 120)(Tmux.completions(t""))
+
         test(m"Test subcommands on bash"):
           Bash.tmux()(Tmux.completions(t""))
         . assert(_ == t"alpha         beta          distribution")
