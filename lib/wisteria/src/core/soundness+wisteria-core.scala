e3977ae329a5e2b053c333047fdb6039399a1b09
Jon Pretty <jon.pretty@propensive.com>
2025-11-11 22:30:51 +0100

    Export `arithmetic` to Soundness

--- lib/wisteria/src/core/soundness+wisteria-core.scala
+++ lib/wisteria/src/core/soundness+wisteria-core.scala
@@ -34,7 +34,7 @@ package soundness
 
 export wisteria
 . { FieldIndex, ContextRequirement, VariantIndex, VariantError, ProductDerivation, Derivation,
-    Reflection, ProductReflection, SumReflection, Derivable, ProductDerivable }
+    Reflection, ProductReflection, SumReflection, Derivable, ProductDerivable, arithmetic }
 
 package derivationContext:
   export wisteria.derivationContext.{required, relaxed}
