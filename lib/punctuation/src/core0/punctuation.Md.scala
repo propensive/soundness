3589e28aa3cbfce520d835163cddf7624222f971
Jon Pretty <jon.pretty@propensive.com>
2025-11-19 07:54:38 +0100

    Work in progress

--- lib/punctuation/src/core0/punctuation.Md.scala
+++ lib/punctuation/src/core0/punctuation.Md.scala
@@ -30,116 +30,68 @@
 ┃                                                                                                  ┃
 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
                                                                                                   */
-package zephyrine
-
-import scala.collection.mutable as scm
+package punctuation
 
 import anticipation.*
-import denominative.*
+import contextual.*
+import contingency.*
 import fulminate.*
-import prepositional.*
-import rudiments.*
+import gossamer.*
+import turbulence.*
+import vacuous.*
 
-object Cursor:
-  opaque type Mark = Long
-  erased trait Held
+import scala.compiletime.*
 
-  object Mark:
-    final val Initial: Mark = -1
-    
-    def apply(block: Ordinal, position: Ordinal): Mark =
-      (block.n0.toLong << 32) | (position.n0.toLong & 0xffffffffL)
-      
-    given ordered: Ordering[Mark] = Ordering.Long
-  
-  extension (mark: Mark)
-    inline def block: Ordinal = (mark >> 32 & 0xffffffff).toInt.z
-    inline def index: Ordinal = mark.toInt.z
-    private[zephyrine] inline def increment: Mark = mark + 1
-    private[zephyrine] inline def decrement: Mark = mark - 1
+import errorDiagnostics.empty
 
-export Cursor.Mark
+object Md:
+  enum Input:
+    case Block(content: Text)
+    case Inline(content: Text)
 
-case class Cursor[data: Addressable](private val iterator: Iterator[data]):
-  private val buffer: scm.ArrayDeque[data] = scm.ArrayDeque()
-  private var first: Ordinal = (-1).z
-  private var current: data = iterator.next()
-  private var focusBlock: Ordinal = Prim
-  private var focusIndex: Ordinal = (-1).z
-  private var length: Int = Int.MaxValue
-  // FIXME: This should be an ordinal of the first block to keep
-  private var keep: Boolean = false
-  private var done: Int = 0
-  
-  inline def finished: Boolean = position.n0 == length - 1
+  object Input:
+    given insertion: Insertion[Input, Text] = Input.Inline(_)
 
-  protected inline def store(ordinal: Ordinal, value: data): Unit =
-    val index = ordinal - first
-    if buffer.length <= index then buffer.append(value) else buffer(index) = value
+  object Interpolator extends contextual.Interpolator[Input, Input, Markdown[Markdown.Ast.Node]]:
 
-  protected inline def forward(): Unit =
-    val block: Ordinal = focusBlock.next
-    val offset: Int = block - first
-    done += data.length(current)
-    
-    current =
-      if buffer.length > offset then
-        focusBlock = block
-        focusIndex = Prim
-        buffer(offset)
-      else if iterator.hasNext then
-        var next = iterator.next()
-        while data.length(next) == 0 do next = iterator.next()
-        if keep then store(block, next)
-        focusBlock = block
-        focusIndex = Prim
-        next
-      else current.also:
-        length = position.n1
-    
-  protected inline def backward(): Unit =
-    val block = focusBlock.previous
-    val offset = block - first
-    current = buffer(offset)
-    done -= data.length(current)
-    focusBlock = block
-    focusIndex = Prim
+    def complete(state: Input): Markdown[Markdown.Ast.Node] = state match
+      case Input.Inline(state) =>
+        safely(state.read[InlineMd]).or:
+          throw InterpolationError(m"the markdown could not be parsed")
 
-  inline def cue(mark: Mark): Unit =
-    while mark.block.n0 < focusBlock.n0 do backward()
-    while mark.block.n0 > focusBlock.n0 do forward()
-    focusIndex = mark.index
-    
-  inline def next(): Boolean =
-    if focusIndex.next.n0 >= data.length(current) then forward()
-    else focusIndex = focusIndex.next
-    !finished
-  
-  inline def more: Boolean = !finished
-  inline def mark(using erased Cursor.Held): Mark = Mark(focusBlock, focusIndex)
-  inline def datum: data.Operand = data.address(current, focusIndex)
-  inline def position: Ordinal = (done + focusIndex.n0).z
-  
-  inline def hold[result](inline action: (erased Cursor.Held) ?=> result): result =
-    keep = true
-    first = focusBlock
-    store(focusBlock, current)
-    action(using !![Cursor.Held]).also { keep = false }
+      case Input.Block(state)  =>
+        safely(state.read[InlineMd]).or:
+          throw InterpolationError(m"the markdown could not be parsed")
 
-  inline def grab(start: Mark, end: Mark)(target: data.Target): Unit =
-    val last = end.block - first
-    var offset = start.block - first
-    
-    if start.block == end.block then data.grab(buffer(offset), start.index, end.index)(target)
-    else
-      var focus = buffer(offset)
-      data.grab(focus, start.index, data.length(focus).u)(target)
-      
-      while
-        offset += 1
-        offset < last
-      do
-        focus = buffer(offset)
-        data.grab(focus, Prim, data.length(focus).u)(target)
-      
-      data.grab(buffer(offset), Prim, end.index)(target)
+    def initial: Input = Input.Inline(t"")
+    def skip(state: Input): Input = state
+
+    def insert(state: Input, value: Input): Input = value match
+      case Input.Block(content)  => state match
+        case Input.Block(state)    => Input.Block(t"$state\n$content\n")
+        case Input.Inline(state)   => Input.Block(t"$state\n$content")
+
+      case Input.Inline(content) => state match
+        case Input.Block(state)    => Input.Block(t"$state\n$content\n")
+        case Input.Inline(state)   => Input.Inline(t"$state$content")
+
+    def parseInline(text: Text): Optional[Input.Inline] = safely:
+      text.read[InlineMd]
+      Input.Inline(text)
+
+    def parseBlock(text: Text): Optional[Input.Block] = safely:
+      text.read[Md]
+      Input.Block(text)
+
+    def parse(state: Input, next: Text): Input = state match
+      case Input.Inline(state) =>
+        parseInline(t"$state$next").or(parseBlock(t"$state$next")).or:
+          throw InterpolationError(m"the markdown could not be parsed")
+
+      case Input.Block(state) =>
+        safely:
+          t"$state$next".read[Md]
+          Input.Block(t"$state$next")
+
+        . or:
+            throw InterpolationError(m"the markdown could not be parsed")
