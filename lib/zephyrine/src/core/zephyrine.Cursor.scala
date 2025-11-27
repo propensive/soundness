a4ac8ae24ae7bee246a4f73ad211ffc19a61a2ea
Jon Pretty <jon.pretty@propensive.com>
2025-11-27 14:32:34 +0100

    Improvements to `Cursor`

--- lib/zephyrine/src/core/zephyrine.Cursor.scala
+++ lib/zephyrine/src/core/zephyrine.Cursor.scala
@@ -43,6 +43,7 @@ import denominative.*
 import fulminate.*
 import prepositional.*
 import rudiments.*
+import vacuous.*
 
 object Cursor:
   opaque type Mark = Long
@@ -50,12 +51,12 @@ object Cursor:
 
   object Mark:
     final val Initial: Mark = -1
-    
+
     def apply(block: Ordinal, position: Ordinal): Mark =
       (block.n0.toLong << 32) | (position.n0.toLong & 0xffffffffL)
-      
+
     given ordered: Ordering[Mark] = Ordering.Long
-  
+
   extension (mark: Mark)
     inline def block: Ordinal = (mark >> 32 & 0xffffffff).toInt.z
     inline def index: Ordinal = mark.toInt.z
@@ -65,12 +66,12 @@ object Cursor:
 
   inline def apply[data](iterator: Iterator[data])(using addressable0: data is Addressable)
   : Cursor[data] { val addressable: addressable0.type } =
-  
+
       if iterator.hasNext then
         val initial = iterator.next()
         new Cursor[data](initial, addressable0.length(initial), iterator, addressable0)
       else new Cursor[data](addressable0.empty, 0, Iterator.empty, addressable0)
-      
+
 export Cursor.Mark
 
 class Cursor[data](initial:                 data,
@@ -87,7 +88,7 @@ class Cursor[data](initial:                 data,
   private var keep: Boolean = false
   private var extent: Int = extent0
   private var done: Int = 0
-  
+
   protected inline def store(ordinal: Ordinal, value: data): Unit =
     val index = ordinal - first
     if buffer.length <= index then buffer.append(value) else buffer(index) = value
@@ -104,7 +105,7 @@ class Cursor[data](initial:                 data,
     val block: Ordinal = focusBlock.next
     val offset: Int = block - first
     done += addressable.length(current)
-    
+
     current =
       if buffer.length > offset then
         focusBlock = block
@@ -114,16 +115,16 @@ class Cursor[data](initial:                 data,
         var next = load()
         while addressable.length(next) == 0 do
           next = load()
-          
+
         if keep then store(block, next)
         focusBlock = block
         focus = Prim
         next
-        
+
       else current.also:
         focus = focus.next
         length = position.n1
-    
+
   protected inline def backward(): Unit =
     val block = focusBlock.previous
     val offset = block - first
@@ -136,7 +137,7 @@ class Cursor[data](initial:                 data,
     while mark.block.n0 < focusBlock.n0 do backward()
     while mark.block.n0 > focusBlock.n0 do forward()
     focus = mark.index
-    
+
   inline def next(): Boolean =
     if focus.next.n0 >= addressable.length(current) then forward()
     else focus = focus.next
@@ -144,11 +145,22 @@ class Cursor[data](initial:                 data,
 
   inline def more: Boolean = !finished
   inline def mark(using held: Cursor.Held): Mark^{held} = Mark(focusBlock, focus)
-  inline def datum: addressable.Operand = addressable.address(current, focus)
+
+  inline def datum(using Unsafe): addressable.Operand = addressable.address(current, focus)
+
+  inline def lay[result](inline otherwise: => result)(inline lambda: addressable.Operand => result)
+  : result =
+
+      if !finished then lambda(addressable.address(current, focus)) else otherwise
+
+
+  inline def process[result](inline lambda: addressable.Operand => result): Unit =
+    if !finished then lambda(addressable.address(current, focus))
+
   inline def position: Ordinal = (done + focus.n0).z
   inline def finished: Boolean = position.n0 == length - 1
   inline def available = extent - position.n0
-  
+
   inline def hold[result](inline action: Cursor.Held ?=> result): result =
     val keep0 = keep
     keep = true
@@ -164,7 +176,7 @@ class Cursor[data](initial:                 data,
   inline def clone(start: Mark, end: Mark)(target: addressable.Target): Unit =
     val last = end.block - first
     var offset = start.block - first
-    
+
     if start.block == end.block
     then
       if end.index.previous.n0 > start.index.n0
@@ -172,13 +184,13 @@ class Cursor[data](initial:                 data,
     else
       var focus = buffer(offset)
       addressable.clone(focus, start.index, addressable.length(focus).u)(target)
-      
+
       while
         offset += 1
         offset < last
       do
         focus = buffer(offset)
         addressable.clone(focus, Prim, addressable.length(focus).u)(target)
-      
+
       if end.index != Prim
       then addressable.clone (buffer(offset), Prim, end.index.previous)(target)
