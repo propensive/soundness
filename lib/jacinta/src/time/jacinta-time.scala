706f711913edbd611db7004f8859d6c7f5c9a384
Jon Pretty <jon.pretty@propensive.com>
2026-01-20 10:55:09 +0100

    Support serialization and deserialization of durations as milliseconds

--- lib/jacinta/src/time/jacinta-time.scala
+++ lib/jacinta/src/time/jacinta-time.scala
@@ -10,6 +10,12 @@ package jsonEncodables:
   given encodeInstantsAsUnixEpochMilliseconds: Instant is Encodable in Json =
     instant => Json(instant.long)
 
+  given encodeDurationsAsMilliseconds: Duration is Encodable in Json =
+    duration => Json(duration.value.toLong)
+
 package jsonDecodables:
   given decodeInstantsAsUnixEpochMilliseconds: Tactic[JsonError] => Instant is Decodable in Json =
     json => Instant.of(json.as[Long])
+
+  given decodeDurationsAsMilliseconds: Tactic[JsonError] => Duration is Decodable in Json =
+    json => Duration.of(json.as[Long])
--- lib/jacinta/src/time/soundness+jacinta-time.scala
+++ lib/jacinta/src/time/soundness+jacinta-time.scala
@@ -2,6 +2,8 @@ package soundness
 
 package jsonEncodables:
   export jacinta.jsonEncodables.encodeInstantsAsUnixEpochMilliseconds
+  export jacinta.jsonEncodables.encodeDurationsAsMilliseconds
 
 package jsonDecodables:
   export jacinta.jsonDecodables.decodeInstantsAsUnixEpochMilliseconds
+  export jacinta.jsonDecodables.decodeDurationsAsMilliseconds
