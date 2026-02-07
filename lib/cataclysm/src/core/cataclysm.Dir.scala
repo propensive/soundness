21e4240ecde35d9095552ab04ac55719dd4a8554
Jon Pretty <jon.pretty@propensive.com>
2026-02-07 18:00:29 +0100

    Update headers to 0.54.0 (#844)

--- lib/cataclysm/src/core/cataclysm.Dir.scala
+++ lib/cataclysm/src/core/cataclysm.Dir.scala
@@ -11,7 +11,7 @@
 ┃   ╭───╯   ││   ╰─╯   ││   ╰─╯   ││   │ │   ││   ╰─╯   ││   │ │   ││   ╰────╮╭───╯   │╭───╯   │   ┃
 ┃   ╰───────╯╰─────────╯╰────╌╰───╯╰───╯ ╰───╯╰────╌╰───╯╰───╯ ╰───╯╰────────╯╰───────╯╰───────╯   ┃
 ┃                                                                                                  ┃
-┃    Soundness, version 0.53.0.                                                                    ┃
+┃    Soundness, version 0.54.0.                                                                    ┃
 ┃    © Copyright 2021-25 Jon Pretty, Propensive OÜ.                                                ┃
 ┃                                                                                                  ┃
 ┃    The primary distribution site is:                                                             ┃
