0d048f9173c7756350970ee12f3eb6a165e5b3c2
Jon Pretty <jon.pretty@propensive.com>
2026-04-24 13:45:14 +0200

    Live reporting of test progress

--- lib/probably/src/core/probably.Runner.scala
+++ lib/probably/src/core/probably.Runner.scala
@@ -32,14 +32,20 @@
                                                                                                   */
 package probably
 
+import escapade.*
+import iridescence.*
 import rudiments.*
+import turbulence.*
 import vacuous.*
+
+import stdioSources.virtualMachine.ansi
+import webColors.CadetBlue
 
 object Runner:
   private[probably] val harnessThreadLocal: ThreadLocal[Option[Harness]] = ThreadLocal()
 
 class Runner[report]()(using reporter: Reporter[report]):
-  private var active: Set[TestId] = Set()
+  private var active: List[TestId] = Nil
 
   def skip(id: TestId): Boolean = false
 
@@ -48,9 +54,21 @@ class Runner[report]()(using reporter: Reporter[report]):
   def maybeRun[result](test: Test[result]): Optional[Trial[result]] =
     if skip(test.id) then Unset else run[result](test)
 
+  def redraw(size: Int): Unit =
+    if size > 0 then Out.print(e"\e[${size}A\r\e[2K")
+
+    active.reverse.each: test =>
+      Out.println(e"> ${CadetBlue}(${test.id})${" "*(test.depth*2)}${test.name}\e[K")
+
+    Out.print(e"\e[J")
+
+
   def run[result](test: Test[result]): Trial[result] =
-    _root_.java.lang.System.out.nn.println(s"running: ${test.id.name}")
-    synchronized { active += test.id }
+    synchronized:
+      val size = active.size
+      active ::= test.id
+      redraw(size)
+
     val context = Harness()
     Runner.harnessThreadLocal.set(Some(context))
     val ns0 = System.nanoTime
@@ -59,8 +77,12 @@ class Runner[report]()(using reporter: Reporter[report]):
       val ns0: Long = System.nanoTime
       val result: result = test.action(context)
       val ns: Long = System.nanoTime - ns0
+
       Trial.Returns(result, ns, context.captured.to(Map)).also:
-        synchronized { active -= test.id }
+        synchronized:
+          val size = active.size
+          active = active.filter(_ != test.id)
+          redraw(size)
 
     catch case error: Exception =>
       val ns: Long = System.nanoTime - ns0
@@ -70,18 +92,33 @@ class Runner[report]()(using reporter: Reporter[report]):
         throw error
 
       Trial.Throws(lazyException, ns, context.captured.to(Map)).also:
-        synchronized { active -= test.id }
+        synchronized:
+          val size = active.size
+          active = active.filter(_ != test.id)
+          redraw(size)
 
     finally
       Runner.harnessThreadLocal.set(None)
 
   def suite(suite: Testable, block: Testable ?=> Unit): Unit =
     if !skip(suite.id) then
+      synchronized:
+        val size = active.size
+        active ::= suite.id
+        redraw(size)
+
       reporter.declare(report, suite)
       block(using suite)
 
+      synchronized:
+        val size = active.size
+        active = active.filter(_ != suite.id)
+        redraw(size)
+
   def terminate(error: Throwable): Unit = synchronized:
-    reporter.fail(report, error, active)
+    reporter.fail(report, error, active.to(Set))
     reporter.complete(report)
 
-  def complete(): Unit = reporter.complete(report)
+  def complete(): Unit =
+    redraw(0)
+    reporter.complete(report)
