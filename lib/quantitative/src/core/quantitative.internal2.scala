28533aac0f58af222100eff0e0619a495cbfa3a1
Jon Pretty <jon.pretty@propensive.com>
2026-04-29 18:17:55 +0200

    Move `Protoquantity` trait inside `object internal2`
    Rename `Quantity2` to `Protoquantity` and nest it inside a new
    `object internal2` (sibling to `object internal`). Reference is
    qualified as `quantitative.internal2.Protoquantity` because
    `anticipation.internal2` is also imported and shadows the unqualified
    name.

--- lib/quantitative/src/core/quantitative.internal2.scala
+++ lib/quantitative/src/core/quantitative.internal2.scala
@@ -38,25 +38,26 @@ import prepositional.*
 import proscenium.*
 import symbolism.*
 
-trait Quantity2:
-  extension [units <: Measure](quantity: Quantity[units])
-    transparent inline def in[units2[power <: Nat] <: Units[power, ?]]: Any =
-      ${quantitative.internal.norm[units, units2]('quantity)}
+object internal2:
+  trait Protoquantity:
+    extension [units <: Measure](quantity: Quantity[units])
+      transparent inline def in[units2[power <: Nat] <: Units[power, ?]]: Any =
+        ${quantitative.internal.norm[units, units2]('quantity)}
 
-    transparent inline def invert: Any = Quantity[Measure](1.0)/quantity
-
-
-    inline def normalize[units2 <: Measure](using normalizable: units is Normalizable to units2)
-    :   Quantity[units2] =
-
-      normalizable.normalize(quantity)
+      transparent inline def invert: Any = Quantity[Measure](1.0)/quantity
 
 
-    inline def sqrt(using root: Quantity[units] is Rootable[2]): root.Result = root.root(quantity)
-    inline def cbrt(using root: Quantity[units] is Rootable[3]): root.Result = root.root(quantity)
-    inline def units: Map[Text, Int] = ${quantitative.internal.collectUnits[units]}
+      inline def normalize[units2 <: Measure](using normalizable: units is Normalizable to units2)
+      :   Quantity[units2] =
 
-    inline def express(using Decimalizer): Text =
-      t"${quantity.value} ${Quantity.expressUnits(units)}"
+        normalizable.normalize(quantity)
 
-    inline def dimension: Text = ${quantitative.internal.describe[units]}
+
+      inline def sqrt(using root: Quantity[units] is Rootable[2]): root.Result = root.root(quantity)
+      inline def cbrt(using root: Quantity[units] is Rootable[3]): root.Result = root.root(quantity)
+      inline def units: Map[Text, Int] = ${quantitative.internal.collectUnits[units]}
+
+      inline def express(using Decimalizer): Text =
+        t"${quantity.value} ${Quantity.expressUnits(units)}"
+
+      inline def dimension: Text = ${quantitative.internal.describe[units]}
