33b4c8f807308e814841b15e2a89355276e6a5a4
Jon Pretty <jon.pretty@propensive.com>
2026-07-25 09:23:16 +0200

    Re-drain anthology dex path lists and bitumen test streams

--- lib/anthology/src/dex/anthology_dex.scala
+++ lib/anthology/src/dex/anthology_dex.scala
@@ -101,18 +101,18 @@ object dexLinkages:
       // manifest of a hosting APK or the command line of `dalvikvm`, so entry points—like a
       // library JAR's—do not apply.
 
-      val roots: List[jnf.Path] =
+      val roots: scala.collection.immutable.List[jnf.Path] =
         jnf.Paths.get(compilation.out.encode.s).nn ::
           compilation.classpath.entries.stdlib.flatMap:
-            case ClasspathEntry.Directory(directory) => List(jnf.Paths.get(directory.s).nn)
-            case ClasspathEntry.Jar(jar)             => List(jnf.Paths.get(jar.s).nn)
-            case _                                   => Nil
+            case ClasspathEntry.Directory(directory) => scala.collection.immutable.List(jnf.Paths.get(directory.s).nn)
+            case ClasspathEntry.Jar(jar)             => scala.collection.immutable.List(jnf.Paths.get(jar.s).nn)
+            case _                                   => scala.collection.immutable.Nil
 
-      val (archives, directories) = roots.stdlib.partition(jnf.Files.isRegularFile(_))
+      val (archives, directories) = roots.partition(jnf.Files.isRegularFile(_))
 
       // D8 accepts archives wholesale but not directories, whose classfiles are enumerated
       // individually; `module-info` classfiles are not program code and are excluded.
-      val classfiles: List[jnf.Path] = directories.flatMap: directory =>
+      val classfiles: scala.collection.immutable.List[jnf.Path] = directories.flatMap: directory =>
         val walk = jnf.Files.walk(directory).nn
 
         try walk.iterator.nn.asScala.toList.filter: path =>
@@ -134,7 +134,7 @@ object dexLinkages:
         jnf.Files.createDirectories(outPath)
 
         val builder = D8Command.builder(handler).nn
-        builder.addProgramFiles((archives ++ classfiles.stdlib).asJava)
+        builder.addProgramFiles((archives ++ classfiles).asJava)
         builder.setMinApiLevel(form.minApi)
         builder.setMode(form.mode)
         builder.setOutput(outPath.resolve("main.dex.jar").nn, OutputMode.DexIndexed)
